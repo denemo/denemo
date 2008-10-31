7aeef39adf8cec116df9332cdd1d62126e6c2889
Jeremiah Benham <jjbenham@chicagoguitar.com>
2008-10-31 18:10:08 -0500

    added jack files

---
+++ src/jackmidi.c
@@ -0,0 +1,82 @@
+#include <jack/jack.h>
+#include <jack/midiport.h>
+#include <glib.h>
+
+#define NOTE_OFF                0x80
+#define NOTE_ON                 0x90
+#define SYS_EXCLUSIVE_MESSAGE1  0xF0
+
+#define INPUT_PORT_NAME         "midi_in"
+#define PROGRAM_NAME            "denemo"
+
+
+
+
+jack_client_t   *jack_client = NULL;
+jack_port_t     *input_port;
+
+void
+process_midi_input(jack_nframes_t nframes)
+{
+  int read, events, i, channel;
+  void           *port_buffer;
+  jack_midi_event_t event;
+  int             last_frame_time;
+  static int      time_of_first_event = -1;
+
+  last_frame_time = jack_last_frame_time(jack_client);
+  port_buffer = jack_port_get_buffer(input_port, nframes);
+  events = jack_midi_get_event_count(port_buffer);
+  for (i = 0; i < events; i++) {
+    read = jack_midi_event_get(&event, port_buffer, i);
+    if (event.buffer[0] = (SYS_EXCLUSIVE_MESSAGE1 & NOTE_ON))
+      printf("\nmidi note on = %d\n", event.buffer[1]);
+    if (event.buffer[0] = (SYS_EXCLUSIVE_MESSAGE1 & NOTE_OFF))
+      printf("\nmidi note off = %d\n", event.buffer[1]);
+  }
+}
+ 
+static int
+process_callback(jack_nframes_t nframes, void *notused)
+{
+  process_midi_input(nframes);
+  return 0;
+}
+
+void
+init_jack(void){
+  int err;
+  jack_client = jack_client_open(PROGRAM_NAME, JackNullOption, NULL);
+  if (jack_client == NULL) {
+    g_critical("Could not connect to the JACK server; run jackd first?");
+    //exit(EX_UNAVAILABLE);
+  }
+
+  err = jack_set_process_callback(jack_client, process_callback, 0);
+  if (err) {
+    g_critical("Could not register JACK process callback.");
+    //exit(EX_UNAVAILABLE);
+  }
+
+  input_port = jack_port_register(jack_client, INPUT_PORT_NAME, JACK_DEFAULT_MIDI_TYPE,
+	                  JackPortIsInput, 0);
+
+  if (input_port == NULL) {
+    g_critical("Could not register JACK input port.");
+    //exit(EX_UNAVAILABLE);
+  }
+
+  if (jack_activate(jack_client)) {
+    g_critical("Cannot activate JACK client.");
+    //exit(EX_UNAVAILABLE);
+  }
+}
+
+void 
+jack_stop(){
+  jack_deactivate(jack_client);
+}
+
+void jackmidi(){
+  init_jack();
+}
