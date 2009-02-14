f0b36a987282a2ba6b93f8f16f6229539570ab66
Jeremiah Benham <jjbenham@chicagoguitar.com>
2009-02-13 22:10:03 -0600

    added WITH_LASH switch

--- src/jackmidi.h
+++ src/jackmidi.h
@@ -6,13 +6,16 @@
  */
 #ifndef JACKMIDI_H
 #define JACKMIDI_H
-#include<lash/lash.h>
-
 int init_jack(void);
 int jackmidi(void);
 void  jackstop(void);
 void jack_midi_playback_control(gboolean start);
 int jack_kill_timer(void);
+#ifdef WITH_LASH
+#include<lash/lash.h>
 void start_init_lash(lash_client_t *client);
+#endif
+
+
 
 #endif //JACKMIDI_H
