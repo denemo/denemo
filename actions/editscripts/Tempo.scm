600c86d7c20e1e4289810fd48f5d3fe2f5cd0476
Richard Shann <rshann@debianrts.home>
2009-02-22 19:06:37 +0000

    Better name for the tag.

--- actions/editscripts/Tempo.scm
+++ actions/editscripts/Tempo.scm
@@ -1,11 +1,10 @@
 ;;; tweak position of Tempo indication, d-x and d-y are set by dragging in printview area.
-(display "Editing position of tempo marking")
 (let ((oldstr "")(start "")(end ""))
-  (set! oldstr (d-DirectiveGet-chord-prefix "Text"))
+  (set! oldstr (d-DirectiveGet-chord-prefix "TextScript"))
   (if (equal? oldstr "")
       (set! oldstr #f))
   (set! start "\\once \\override TextScript  #'extra-offset = #'(")
   (set! end ")")
-  (d-DirectivePut-chord-prefix "Text" (ChangeOffset oldstr start end))
+  (d-DirectivePut-chord-prefix "TextScript" (ChangeOffset oldstr start end))
   (d-RefreshDisplay))
 
