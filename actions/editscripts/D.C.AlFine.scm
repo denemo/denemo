ca218e396054774052c853cc30198b7788c38eea
Richard Shann <rshann@debianrts.home>
2009-02-24 09:34:14 +0000

    Add edit for D.C. al fine position, upgrade Tempo editing.

---
+++ actions/editscripts/D.C.AlFine.scm
@@ -0,0 +1,5 @@
+;;; tweak values of D.C. Al fineindication, d-x and d-y are set by dragging in printview area.
+
+;;just position at present...
+(ExtraOffset "TextScript")
+(d-RefreshDisplay)
--- actions/editscripts/Tempo.scm
+++ actions/editscripts/Tempo.scm
@@ -1,10 +1,4 @@
 ;;; tweak position of Tempo indication, d-x and d-y are set by dragging in printview area.
-(let ((oldstr "")(start "")(end ""))
-  (set! oldstr (d-DirectiveGet-chord-prefix "TextScript"))
-  (if (equal? oldstr "")
-      (set! oldstr #f))
-  (set! start "\\once \\override TextScript  #'extra-offset = #'(")
-  (set! end ")")
-  (d-DirectivePut-chord-prefix "TextScript" (ChangeOffset oldstr start end))
-  (d-RefreshDisplay))
+(ExtraOffset "TextScript")
+(d-RefreshDisplay)
 
