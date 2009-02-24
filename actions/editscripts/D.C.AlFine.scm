0369d81a7461213dc912bfd7c539f983167e2016
Richard Shann <rshann@debianrts.home>
2009-02-24 19:23:40 +0000

    D.C al fine shows use of edit options.

--- actions/editscripts/D.C.AlFine.scm
+++ actions/editscripts/D.C.AlFine.scm
@@ -1,5 +1,13 @@
 ;;; tweak values of D.C. Al fineindication, d-x and d-y are set by dragging in printview area.
 
-;;just position at present...
-(ExtraOffset "TextScript")
-(d-RefreshDisplay)
+(let ((choice #f))
+  (begin
+    (set! choice (d-GetOption "Edit position\0Edit text\0"))
+    (cond
+     ((boolean? choice)
+      (d-WarningDialog "Operation cancelled"))
+     ((equal? choice "Edit text")
+      (d-WarningDialog "Not yet implemented"))	
+     ((equal? choice "Edit position")
+      (ExtraOffset "TextScript")))
+    (d-RefreshDisplay)))
