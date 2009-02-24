d24670f59ab96ccfce6e73d10a46832c479485eb
Richard Shann <rshann@debianrts.home>
2009-02-24 20:36:18 +0000

    Allow editing of the text of D.C.AlFine command.

--- actions/editscripts/D.C.AlFine.scm
+++ actions/editscripts/D.C.AlFine.scm
@@ -1,13 +1,19 @@
-;;; tweak values of D.C. Al fineindication, d-x and d-y are set by dragging in printview area.
+;;; tweak values of D.C. Al fine indication, d-x and d-y are set by dragging in printview area.
 
-(let ((choice #f))
-  (begin
-    (set! choice (d-GetOption "Edit position\0Edit text\0"))
-    (cond
-     ((boolean? choice)
-      (d-WarningDialog "Operation cancelled"))
-     ((equal? choice "Edit text")
-      (d-WarningDialog "Not yet implemented"))	
-     ((equal? choice "Edit position")
-      (ExtraOffset "TextScript")))
-    (d-RefreshDisplay)))
+(let ((choice #f) (edit-text #f) (newtext ""))
+      (begin
+	(set! edit-text (lambda ()
+			  (begin
+			    (set! newtext (d-GetUserInput "Replacing Text" "Give new text" "D.C.AlFine" ))
+			    (d-DirectivePut-chord-postfix   "D.C.AlFine"  (string-append "^\\markup \\italic \\bold {" newtext "}"))
+			    (d-DirectivePut-chord-display  "D.C.AlFine" newtext)
+			    )))
+	  (set! choice (d-GetOption "Edit position\0Edit text\0"))
+	  (cond
+	   ((boolean? choice)
+	    (d-WarningDialog "Operation cancelled"))
+	   ((equal? choice "Edit text")
+	    (edit-text))	
+	   ((equal? choice "Edit position")
+	    (ExtraOffset "TextScript")))
+	  (d-RefreshDisplay)))
