236e67d99eac9a4469cc1cbbbc51e9134933fca0
Richard Shann <rshann@debianrts.home>
2009-05-20 17:55:35 +0100

    Extend editing of tempo indications, add Allegro Assai.

--- actions/editscripts/Tempo.scm
+++ actions/editscripts/Tempo.scm
@@ -1,4 +1,17 @@
-;;; tweak position of Tempo indication
-(ExtraOffset "TextScript")
-(d-RefreshDisplay)
+;;; tweak Tempo indication
 
+(let ((choice #f))
+  (begin
+    (set! choice (d-GetOption (string-append cue-OffsetPositionAll stop cue-SetPadding stop cue-SetRelativeFontSize stop cue-Advanced stop)))
+    (cond
+     ((boolean? choice)
+      (d-WarningDialog "Operation cancelled"))
+     ((equal? choice  cue-Advanced)
+      (d-DirectiveTextEdit-chord "Tempo"))
+     ((equal? choice  cue-OffsetPositionAll)
+      (ExtraOffset "TextScript"))
+     ((equal? choice cue-SetRelativeFontSize)
+      (SetRelativeFontSize "TextScript"))
+     ((equal? choice cue-SetPadding)
+      (SetPadding "TextScript")))))
+(d-RefreshDisplay)
