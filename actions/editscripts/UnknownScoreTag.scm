538fc59f9379264b5cb816d3719bbd81058a88f3
Richard Shann <rshann@debianrts.home>
2009-07-11 19:18:05 +0100

    upgrade scripts

--- actions/editscripts/UnknownScoreTag.scm
+++ actions/editscripts/UnknownScoreTag.scm
@@ -1,11 +1,13 @@
-;;; offer a delete
+;;; offer a delete or text edit
 
 (let ((choice #f))
   (begin
-    (set! choice (d-GetOption (string-append cue-Delete stop)))
+    (set! choice (d-GetOption (string-append cue-Delete stop cue-Advanced stop)))
     (cond
      ((boolean? choice)
       (d-WarningDialog "Operation cancelled"))
+     ((equal? choice  cue-Advanced)
+      (d-DirectiveTextEdit-score "UnknownScoreTag"))
      ((equal? choice  cue-Delete)
       (d-DirectiveDelete-score "UnknownScoreTag")))))
 (d-RefreshDisplay)
