f363c6851d982dedcfa43afe71cb66d7d9a22139
Richard Shann <rshann@debianrts.home>
2009-07-29 09:46:54 +0100

    Dynamics controlling midi, ff and mf done.

--- actions/menus/ObjectMenu/Markings/TextMarks/Dynamic Markings/init.scm
+++ actions/menus/ObjectMenu/Markings/TextMarks/Dynamic Markings/init.scm
@@ -1,7 +1,12 @@
- (define (Dynamics::Put graphic lily)
+ ;;;;;;;;;;; dynamics init.scm
+ (define* (Dynamics::Put graphic lily   #:optional (midi-vol #f))
 (d-DirectivePut-chord-graphic "Dynamic"  graphic)
 (d-DirectivePut-chord-gx "Dynamic"  -10)
 (d-DirectivePut-chord-gy "Dynamic"  30)
 (d-DirectivePut-chord-postfix "Dynamic"  lily)
 (d-DirectivePut-chord-minpixels  "Dynamic" 20)
+(if (string? midi-vol)
+(begin
+  (d-DirectivePut-chord-override "Dynamic" (logior DENEMO_OVERRIDE_VOLUME DENEMO_OVERRIDE_STEP))
+  (d-DirectivePut-chord-midibytes "Dynamic" midi-vol)))
 (d-RefreshDisplay))
