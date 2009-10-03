b923d5c17e75e1bf87d9452973ef31fe79c0bd90
Richard Shann <rshann@debianrts.home>
2009-10-03 11:44:50 +0100

    add Forte and Mezzo Piano

--- actions/menus/ObjectMenu/Markings/TextMarks/Dynamic Markings/init.scm
+++ actions/menus/ObjectMenu/Markings/TextMarks/Dynamic Markings/init.scm
@@ -2,7 +2,7 @@
  (define* (Dynamics::Put graphic lily   #:optional (midi-vol #f))
 (d-DirectivePut-chord-graphic "Dynamic"  graphic)
 (d-DirectivePut-chord-gx "Dynamic"  -10)
-(d-DirectivePut-chord-gy "Dynamic"  30)
+(d-DirectivePut-chord-gy "Dynamic"  50)
 (d-DirectivePut-chord-postfix "Dynamic"  lily)
 (d-DirectivePut-chord-minpixels  "Dynamic" 20)
 (if (string? midi-vol)
