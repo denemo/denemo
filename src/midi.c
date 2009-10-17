c304eb1b4cd701bb0190b3e57d371e2d4dda6aeb
Jeremiah Benham <jjbenham@chicagoguitar.com>
2009-10-17 08:09:03 -0500

    Fluidsynth immediate playback and scripted note play

--- src/midi.c
+++ src/midi.c
@@ -149,7 +149,8 @@ void play_midikey(gint key, double duration, double volume, gint channel){
   jack_playpitch(key, NOTEDURATION);
 
 #else
-  playpitch(midi2hz(key), duration, volume, channel);
+   fluid_playpitch(key, 1000 /*duration*/);
+  //playpitch(midi2hz(key), duration, volume, channel);
 #endif
 }
 
