b1019bdbe670dc6e2c2d5978306770317674e59e
Jeremiah Benham <jjbenham@chicagoguitar.com>
2009-11-17 11:38:55 -0600

    jackmidi.h updated variables

--- src/jackmidi.h
+++ src/jackmidi.h
@@ -15,8 +15,8 @@ void jack_playpitch(gint key, gint duration);
 void jack_output_midi_event(unsigned char *buffer);
 int jack_kill_timer(void);
 int create_jack_midi_port(int client_number);
-int rename_jack_midi_port(int port_number, char *port_name);
-int remove_jack_midi_port(int port_number);
-void remove_all_jack_midi_ports(gint index); 
+int rename_jack_midi_port(int client_number, int port_number, char *port_name);
+int remove_jack_midi_port(int client_number);
+void remove_all_jack_midi_ports(gint client_number); 
 void jack_start_restart (void);
 #endif //JACKMIDI_H
