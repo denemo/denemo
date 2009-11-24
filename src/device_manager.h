26d2107a58a50803ac09361fbe7151b977ae8f4a
Jeremiah Benham <jjbenham@chicagoguitar.com>
2009-11-23 22:34:36 -0600

    updated device manager as seperate file

---
+++ src/device_manager.h
@@ -0,0 +1,7 @@
+GtkWidget *DeviceManager ();
+void device_manager_create_device();
+void device_manager_remove_device();
+void device_manager_create_port(gpointer client_number);
+void device_manager_remove_port(gpointer client_number);
+
+
