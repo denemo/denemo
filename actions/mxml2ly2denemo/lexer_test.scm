22a4541baceff2534704a9892b4ef800c3cc98fd
Nils Gey <denemo@nilsgey.de>
2010-07-16 12:48:26 +0200

    SILex connected to LALR-scm, not working

--- actions/mxml2ly2denemo/lexer_test.scm
+++ actions/mxml2ly2denemo/lexer_test.scm
@@ -1,3 +1,18 @@
+;; MusicXML to Lilypond to Denemo
+;; A Lexer / Parser to import ly-converted mxml files into Denemo
+;; By Richard Shann and Nils Gey, July / August 2010
+;; Usage of SILex and LALR-scm 
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;TOC
+;; Libs
+;; init input port
+;; load and init lexer
+;; lalr-parser definition
+;; execute the parser
+;; cleaning up (input port...)
+;;;;;
+
 ;; Libs
 (load "lalr.scm")
 (load "silex.scm")
@@ -10,13 +25,29 @@
 (load "mxml2ly2denemo.l.scm")
 (lexer-init 'port (current-input-port)) 
 
-;; Quick test
-(let loop ()
-  (let ((c (lexer)))
-	(if (not (eqv? c '*eof*))
-		(begin (display c)(newline)(loop))
-		#f)))
 
 
-;; Close the input port
+;; Parser Definition
+(define mxml2ly2denemo-parser
+  (lalr-parser
+
+   ;; --- token definitions
+   (INTEGER )
+
+   
+   (e (number)	 : $1)
+   (number (INTEGER) : #t)	
+
+))
+
+; Just to get this out of my way... I don't wanted to make errors anyway! (real function later)
+(define (displayerror one two)
+		(display one)
+		(display two)(newline)
+)
+(display (lexer))(newline)
+(mxml2ly2denemo-parser lexer displayerror)
+
+
+;; Close input port
 (close 0) 
