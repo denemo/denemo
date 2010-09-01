b4df215f39f67c569113e586e2c2d10eb492549a
Nils Gey <denemo@nilsgey.de>
2010-09-01 03:42:56 +0200

    Let ShiftUp/Down use the new ANS-7 functions which results in much less and cleaner code

--- actions/menus/MainMenu/EditMenu/Shift/init.scm
+++ actions/menus/MainMenu/EditMenu/Shift/init.scm
@@ -1,35 +1,17 @@
 (define (ShiftUp)
-(define newList #f)
-
-(if (d-GetNotes)
-(begin
-(set! newList (string-tokenize(d-GetNotes)) )
-
- (let transformChordList ((i 0) )
-   (if (<= i (-(length newList )1)) 
-      (begin
-        (set! newList (replace-nth newList i (ANS-7::CalculateDiatonicStep (list-ref newList i)  ))) ; #t as optional argument for Calculate... means step down instead of up.
-        (transformChordList (+ i 1)))))
-
-   (d-ChangeChordNotes (string-join newList))
- )
-#f ; if its not a note/chord
-))
+  (if (d-GetNotes)
+    (ANS-7::ChangeChordNotes 
+  	(map ANS-7::CalculateDiatonicStepUp (ANS-7::GetChordNotes)) ; Get all notes on cursor position and create a list with shifted-up values
+  	)
+    #f ; not a note/chord
+  )
+)
 
 (define (ShiftDown)
-(define newList #f)
-
 (if (d-GetNotes)
-(begin
-(set! newList (string-tokenize(d-GetNotes)) )
-
- (let transformChordList ((i 0) )
-   (if (<= i (-(length newList )1)) 
-      (begin
-        (set! newList (replace-nth newList i (ANS-7::CalculateDiatonicStep (list-ref newList i) #t ))) ; #t as optional argument for Calculate... means step down instead of up.
-        (transformChordList (+ i 1)))))
-
-   (d-ChangeChordNotes (string-join newList))
- )
-#f ; if its not a note/chord
-))
+    (ANS-7::ChangeChordNotes 
+  	(map ANS-7::CalculateDiatonicStepDown (ANS-7::GetChordNotes)) ; Get all notes on cursor position and create a list with shifted-up values
+  	)
+    #f ; not a note/chord
+  )
+)
