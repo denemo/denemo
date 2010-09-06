d15781b5f13fbdea8397f529901e8e8ff0002277
Nils Gey <denemo@nilsgey.de>
2010-09-06 20:54:41 +0200

    Move and Rename the three notationMagick and their underlying functions to notationmagick.scm. Rule #1: no direct use in ans-7 except one function to convert from ly to ans-7

--- actions/notationmagick.scm
+++ actions/notationmagick.scm
@@ -5,9 +5,36 @@
 ;; A global var, used by several NotationMagick functions to remember the last value used 
 (define NotationMagick::NOTATIONSTRING "c' e' g'") 
 
+
+;;;; Lilypond wrappers for ans-7 functions
+;;;;;;;;;;;;;;;;;;;;;;
+
+;Random Diatonic: Converter for Lilypond syntax  
+(define*  (NotationMagick::RandomDiatonicLy #:optional (from "c,,,") (to "b''''"))
+	(ANS-7::RandomDiatonic (ANS-7::Ly2Ans from) (ANS-7::Ly2Ans to))
+)
+
+;Random Chromatic: Converter for Lilypond syntax  
+(define*  (NotationMagick::RandomChromaticLy #:optional (from "c,,,") (to "b''''"))
+	(ANS-7::RandomChromatic (ANS-7::Ly2Ans from) (ANS-7::Ly2Ans to))
+)
+
+;Shuffled List Insert for Lilypond syntax
+(define (NotationMagick::InsertListRandomlyLy lylist)
+	(ANS-7::InsertListRandomly (map ANS-7::Ly2Ans lylist))
+)
+
+;Insert a random member of a list for Lilypond syntax
+(define (NotationMagick::InsertMemberRandomlyLy lylist)
+	(ANS-7::InsertMemberRandomly (map ANS-7::Ly2Ans lylist))
+)
+
 ;; Ask for a new NOTATIONSTRING with an input-window, return that and remember the value. 
 (define (NotationMagick::AskNewNotationstring)
 (define new (d-GetUserInput "Enter Lilypond notes" "Please insert a list of lilypond notes, separated by comma." NotationMagick::NOTATIONSTRING))
 (set! NotationMagick::NOTATIONSTRING new)
 NotationMagick::NOTATIONSTRING
 )
+
+
+
