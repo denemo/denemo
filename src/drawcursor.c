d066be2303508626ca5b25a6da40ea6a679d3993
Richard Shann <rshann@debianrts.home>
2010-09-16 17:38:53 +0100

    Final touches to cursor

--- src/drawcursor.c
+++ src/drawcursor.c
@@ -57,34 +57,36 @@ draw_cursor (cairo_t *cr, DenemoScore * si,
     cairo_rectangle( cr, xx, height + y - CURSOR_MINUS, CURSOR_WIDTH, CURSOR_HEIGHT );
   cairo_fill( cr );
 
- {
+  {
     gdouble length = 20/si->zoom;
     gdouble insert_pos = CURSOR_WIDTH*0.8;
     if(!si->cursor_appending) {
-	insert_pos = -last_gap/4;
+      insert_pos = -last_gap/4;
     }
     else
       if(si->cursoroffend)
 	insert_pos = -CURSOR_WIDTH;
- 
-    setcairocolor( cr, bluegc );
-    cairo_set_line_width (cr, 4);
-    cairo_move_to( cr, xx+insert_pos, y + 4);
-    cairo_rel_line_to( cr, 0, STAFF_HEIGHT - 8);
-    cairo_stroke( cr );
-    setcairocolor( cr, paintgc );
     static gboolean on;
     on = !on;
     // g_print("on is %d %d\n", on,  Denemo.prefs.cursor_highlight);
-    if(on && Denemo.prefs.cursor_highlight) {
-      cairo_set_line_width (cr, 6.0/si->zoom);
-      cairo_set_source_rgba (cr, 0, 1, 0, 0.40);    
-      cairo_arc(cr, xx + CURSOR_WIDTH/2, height + y, length, 0, 2 * M_PI);
+    if( (!Denemo.prefs.cursor_highlight) || (on && Denemo.prefs.cursor_highlight)) {
+      setcairocolor( cr, bluegc );
+      cairo_set_line_width (cr, 4);
+      cairo_move_to( cr, xx+insert_pos, y + 4);
+      cairo_rel_line_to( cr, 0, STAFF_HEIGHT - 8);
       cairo_stroke( cr );
+      setcairocolor( cr, paintgc );
+      
+      if (Denemo.gui->view == DENEMO_PAGE_VIEW)  {
+	cairo_set_line_width (cr, 6.0/si->zoom);
+	cairo_set_source_rgba (cr, 0, 1, 0, 0.40);    
+	cairo_arc(cr, xx + CURSOR_WIDTH/2, height + y, length, 0, 2 * M_PI);
+	cairo_stroke( cr );
+      }
     }
+  }
+    cairo_restore( cr );
+    
+    /* Now draw ledgers if necessary and we're done */
+    draw_ledgers (cr, height, height, xx, y, CURSOR_WIDTH);
  }
-  cairo_restore( cr );
-
-  /* Now draw ledgers if necessary and we're done */
-  draw_ledgers (cr, height, height, xx, y, CURSOR_WIDTH);
-}
