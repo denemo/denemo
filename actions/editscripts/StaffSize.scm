93b8939358b8784aa985ffca673318fe0b654854
Richard Shann <rshann@debianrts.home>
2010-09-22 20:40:21 +0100

    Allow general edit of Smaller Staff instuction

--- actions/editscripts/StaffSize.scm
+++ actions/editscripts/StaffSize.scm
@@ -2,10 +2,12 @@
 
 (let ((choice #f))
   (begin
-    (set! choice (d-GetOption (string-append cue-Delete stop)))
+    (set! choice (d-GetOption (string-append cue-Delete stop cue-Advanced stop)))
     (cond
      ((boolean? choice)
       (d-WarningDialog "Operation cancelled"))
+     ((equal? choice  cue-Advanced)
+      (d-DirectiveTextEdit-note "StringNum"))
      ((equal? choice  cue-Delete)
       (d-DirectiveDelete-staff "StaffSize")))))
 (d-RefreshDisplay)
