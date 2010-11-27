1f48bbb83e236ca765d52a7b31c0ebaf806b39f3
Nils Gey <denemo@nilsgey.de>
2010-11-27 17:18:51 +0100

    Fix augment and diminish

--- actions/denemo.scm
+++ actions/denemo.scm
@@ -1440,9 +1440,112 @@
 ;;;;;;;;;; End of duration-conversion
 
 
+;;;;;;;;;;Duration Conversions between Denemo, Lilypond and Tick syntax.
+;; A table of common values
+; 6 = 256 = 8
+;12 = 128 = 7
+;24 = 64 = 6
+;48 = 32 = 5
+;96 = 16 = 4
+;192 = 8 = 3
+;384 =4 = 2
+;768 = 2 = 1
+;1536 = 1 = 0
+;3072 = 0.5  = -1  ; Breve. 0.5 and -1 are not existend.  Lilypond and Denemo use the string breve instead.
+
+; Guile returns a value with .0, which should be exact but internally it's inexact. So we need this little back and forth conversion hack.
+(define (duration::inexact->exact return)
+  (inexact->exact (string->number (number->string return)))
+)
+
+;Some functions, like Upbeat, know only a single tick-value. They need to guess the baseNote.
+(define (duration::GuessBaseNoteInTicks ticks)
+; first guess the basic note duration.  2*x > ticks < 1*x  is always true in our circumstance 
+  (cond
+	 ( (and (>= ticks 6) (< ticks 12)) 6 )       ;1/256
+ 	 ( (and (>= ticks 12) (< ticks 24))  12 )   ;1/128
+ 	 ( (and (>= ticks 24) (< ticks 48)) 24)   ;1/64
+ 	 ( (and (>= ticks 48) (< ticks 96)) 48 )   ;1/32
+ 	 ( (and (>= ticks 96) (< ticks 192))  96)   ;sixteen 1/16
+ 	 ( (and (>= ticks 192) (< ticks 384)) 192 ) ; eight 1/8
+ 	 ( (and (>= ticks 384) (< ticks 768))  384 ) ; quarter 1/4
+ 	 ( (and (>= ticks 768) (< ticks 1536))  768) ; half 1/2
+ 	 ( (and (>= ticks 1536) (< ticks 3072))  1536 ) ; whole 1
+ 	 ( (and (>= ticks 3072) (< ticks 6144))  3072 ) ; breve 2*1
+ 	 ( (and (>= ticks 6144) (< ticks 12288))  6144) ; longa 4*1
+ 	 ( (and (>= ticks 12288) (< ticks 24576))  12288 )  ; maxima 8*1
+	(else #f)
+))
+
+; Calculate a new Duration in Ticks with a basic note value and a number of augmentation-dots
+(define (duration::CalculateTicksWithDots baseTicks numberOfDots)
+	; x = basic note value
+	; n = number of dots
+	; 2x - x/2^n 
+	(-  (* 2 baseTicks) (/ baseTicks (expt 2 numberOfDots)))
+)
+
+; Calculate how many dots a tick value has. Needs base duration, too.
+(define (duration::CalculateDotsFromTicks ticks base)
+; x = base , y = ticks. result is the number of dots
+; log(x/(2*x-y))  / log(2)
+ (define return (/  (log (/ base (- (* base 2) ticks)))   (log 2) ))
+ (duration::inexact->exact return)
+)
+
+; Get Number of Dots from a Lilypond string like "2.". Its so basic it will work on Denemo notes, too.
+(define (duration::GetNumberOfDotsInLilypond input)
+ (length (cdr (string-split input #\.)))
+)
+
+; For the sake of completenes. Denemo and Lilypond dot-syntax is just the same, only the number itself is different.
+(define (duration::GetNumberOfDotsInDenemo input)
+	(duration::GetNumberOfDotsInLilypond input)
+)
+
+(define (duration::denemo->lilypond number)
+	(define return (expt 2 number))
+	return
+)
+
+(define (duration::lilypond->denemo number)
+	(define return (/ (log number) (log 2))	)
+	 (duration::inexact->exact return)
+)
+
+(define (duration::denemo->ticks number) ; increases with negative integers
+	(define return (* (expt 2 (- 8 number)) 6))
+	return
+)
+
+(define (duration::lilypond->ticks number) ; increases with 0.5, 0.25 etc.
+	(define return (* (expt 2 (- 8 (/ (log number) (log 2)))) 6))
+	 (duration::inexact->exact return)
+)
+
+; Ticks->Denemo wants a number but returns a string because of dots
+(define* (duration::ticks->denemo number #:optional (basenumber number))
+ (define numberOfDots  (duration::CalculateDotsFromTicks number basenumber))
+;n = -(log(y/3)-9*log(2))/log(2) 
+ (define return (- (/ (- (log (/ basenumber 3)) (* 9 (log 2))) (log 2))))
+ (set! return (duration::inexact->exact return))
+ (string-append (number->string return) (string-concatenate (make-list numberOfDots "."))) 
+)
+
+;Ticks->Lilypond wants a number but returns a string because of dots
+(define* (duration::ticks->lilypond number #:optional (basenumber number))
+ ;Equation in readable form: http://www.numberempire.com/equationsolver.php?function=y+%3D+6*2^%288-n%29&var=n&answers=
+ (define numberOfDots  (duration::CalculateDotsFromTicks number basenumber))
+ (define return  (expt 2 (- (/ (- (log (/ basenumber 3)) (* 9 (log 2))) (log 2)))))
+ (set! return (duration::inexact->exact return))
+ (string-append (number->string return) (string-concatenate (make-list numberOfDots "."))) 
+)
+;;;;;;;;;; End of duration-conversion
+
+
 ;;;;;;;; Applied duration scripts
 (define (duration::GetNumberOfDotsInTicks)
-	(duration::CalculateDotsFromTicks (d-GetDurationInTicks) (d-GetBaseDurationInTicks)))
+	(duration::CalculateDotsFromTicks (d-GetDurationInTicks) (abs (d-GetBaseDurationInTicks))))
 
 (define (duration::GetSelectionDurationInTicks) 
   (d-PushPosition)
@@ -1461,7 +1564,7 @@
 
 (define* (duration::ChangeNoteDurationInTicks ticks #:optional (dots 0))
 ; First change the base-duration. The d-Change commands also delete any dots.
-  (define (changeBase) (case ticks
+  (define (changeBase number) (case number
    ;((12288) (d-ChangeMaxima))  ;Maxima
    ((6144)	(d-ChangeLonga)) ; Longa
    ((3072)	(d-ChangeBreve)) ; Breve
@@ -1479,7 +1582,7 @@
  
 ; Second add dots
   ; d-ChangeN work on appending position, but Breve and Longa not. But d-AddDot works on appending, too. So we must rule Appending out, else it will add dots without changing the duration for breve and longa.
-  (if (and (string-ci=? (d-GetType) "CHORD") (integer? dots)(changeBase))
+  (if (and (string-ci=? (d-GetType) "CHORD") (integer? dots)(changeBase ticks))
   (let loop ((i 0))
 	(if (= dots i)
 	#t
@@ -1489,5 +1592,8 @@
 	)
   ))
 )
+
+
+;;;;;;;;;;;;;;
 
 
