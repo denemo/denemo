a687076afba8e3712202c038b8b5e65665572de3
Nils Gey <denemo@nilsgey.de>
2010-12-30 14:53:23 +0100

    Add four functions to create music objects - musobj - which hold various information

--- actions/denemo.scm
+++ actions/denemo.scm
@@ -1702,4 +1702,84 @@
 ;;;;;;;
 
 (define (d-GetStartTick)
-	(- (d-GetEndTick) (d-GetDurationInTicks)))	
+	(- (d-GetEndTick) (d-GetDurationInTicks)))
+	
+; from http://icem.folkwang-hochschule.de/~finnendahl/cm_kurse/doc/t-y-scheme/t-y-scheme-Z-H-7.html
+; needed for define-macro defstruct
+(define list-position
+  (lambda (o l)
+    (let loop ((i 0) (l l))
+      (if (null? l) #f
+          (if (eqv? (car l) o) i
+              (loop (+ i 1) (cdr l)))))))
+
+; from http://www.ccs.neu.edu/home/dorai/t-y-scheme/t-y-scheme-Z-H-11.html#node_sec_9.2
+(define-macro defstruct
+  (lambda (s . ff)
+    (let ((s-s (symbol->string s)) (n (length ff)))
+      (let* ((n+1 (+ n 1))
+             (vv (make-vector n+1)))
+        (let loop ((i 1) (ff ff))
+          (if (<= i n)
+            (let ((f (car ff)))
+              (vector-set! vv i 
+                (if (pair? f) (cadr f) '(if #f #f)))
+              (loop (+ i 1) (cdr ff)))))
+        (let ((ff (map (lambda (f) (if (pair? f) (car f) f))
+                       ff)))
+          `(begin
+             (define ,(string->symbol 
+                       (string-append "make-" s-s))
+               (lambda fvfv
+                 (let ((st (make-vector ,n+1)) (ff ',ff))
+                   (vector-set! st 0 ',s)
+                   ,@(let loop ((i 1) (r '()))
+                       (if (>= i n+1) r
+                           (loop (+ i 1)
+                                 (cons `(vector-set! st ,i 
+                                          ,(vector-ref vv i))
+                                       r))))
+                   (let loop ((fvfv fvfv))
+                     (if (not (null? fvfv))
+                         (begin
+                           (vector-set! st 
+                               (+ (list-position (car fvfv) ff)
+                                  1)
+                             (cadr fvfv))
+                           (loop (cddr fvfv)))))
+                   st)))
+             ,@(let loop ((i 1) (procs '()))
+                 (if (>= i n+1) procs
+                     (loop (+ i 1)
+                           (let ((f (symbol->string
+                                     (list-ref ff (- i 1)))))
+                             (cons
+                              `(define ,(string->symbol 
+                                         (string-append
+                                          s-s "." f))
+                                 (lambda (x) (vector-ref x ,i)))
+                              (cons
+                               `(define ,(string->symbol
+                                          (string-append 
+                                           "set!" s-s "." f))
+                                  (lambda (x v) 
+                                    (vector-set! x ,i v)))
+                               procs))))))
+             (define ,(string->symbol (string-append s-s "?"))
+               (lambda (x)
+                 (and (vector? x)
+                      (eqv? (vector-ref x 0) ',s))))))))))
+                      
+; Create a music-object that holds various information. This is the smallest, single object 
+(defstruct musobj pitch measure start duration end)
+(define (createMusObj) 
+	(make-musobj 'pitch (ANS::GetChordNotes)
+				 'measure (d-GetMeasureNumber)
+				 'start (d-GetStartTick)
+				 'duration (d-GetDurationInTicks)
+				 'end (d-GetEndTick)
+							))							
+;Examples:
+;(define testob (createMusObj))							
+;(set!musobj.duration testob 256)
+;(display (musobj.start testob))
