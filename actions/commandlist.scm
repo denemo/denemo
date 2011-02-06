b1d86072a3a4833ff01bc0bf1bf933c9035906da
Nils Gey <denemo@nilsgey.de>
2011-02-06 13:23:54 +0100

    First command list: select durations

--- actions/commandlist.scm
+++ actions/commandlist.scm
@@ -10,15 +10,14 @@ A set of functions to provide scrolling up and down through a list of commands. 
  Syntax: CommandList::UPPERCASE is a list of pairs: (key command/procname)
  Each key only once. The key must be something that can be retrieved by a Denemo d-Get... function. It can be any data type that works with (equal? ...)
  Each command should be mutaly exclusive: d-Set0 revokes d-Set2 and commands about a certain lilypond directive should take care of overwriting/deleting the old version first.
- Example, which copies d-MoveCursorUp in a more complex way :) But its clear and easy to understand. 
-	(define CommandList::CURSOR-Y (list
+ Example, which copies d-MoveCursorUp in a more complex way :) Used as CursorUp this will do pentatonic steps. 
+	(define CommandList::PENTATONIC (list
 		(cons "c" d-MoveToC)
 		(cons "d" d-MoveToD)
 		(cons "e" d-MoveToE)
-		(cons "f" d-MoveToF)
 		(cons "g" d-MoveToG)
-		(cons "a" d-MoveToA)
-		(cons "b" d-MoveToB)))
+		(cons "a" d-MoveToA)))
+
 		
 	(CommandListScrollUp CommandList::CURSOR-Y (d-GetCursorNote))	
 !#
@@ -45,6 +44,19 @@ A set of functions to provide scrolling up and down through a list of commands. 
 		(cdr (CommandList:list-ref commandlist (1- (list-index (lambda (x) (equal? (car x) current)) commandlist))))))
 
 ;Definitions of lists
-;;TODO: Set0 Set1 ... duration
 ;;TODO: Scroll through different barlines. Problem: You still have to enter at least one. Maybe a higher level script can take care of inserting one.
 ;;;In the end: Write a wrapper function for single objects (no selections!!) that gets the type of an object and scrolls afterwards.
+
+(define CommandList::PREVAILINGDURATION (list
+	(cons -3072 d-SetBreve)
+	(cons -6144 d-SetLonga)
+	(cons 0 d-Set0)
+	(cons 1 d-Set1)
+	(cons 2 d-Set2)
+	(cons 3 d-Set3)
+	(cons 4 d-Set4)
+	(cons 5 d-Set5)
+	(cons 6 d-Set6)
+	(cons 7 d-Set7)))
+		
+	
