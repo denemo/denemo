b5898b32182512ccbb78364a46d5ef98f162b6ab
rshann <richard.shann@virgin.net>
2011-06-18 17:19:03 +0100

    Make the amount of intersystem stretching settable, and limit it by default in Simple and Arranger profiles

--- actions/Arranger.scm
+++ actions/Arranger.scm
@@ -1,3 +1,4 @@
 (if (and (= 1 (d-GetMovement)) (not (d-Directive-header? "ScoreTitle")))
 (SetHeaderField "title" "Untitled"))
 (d-DirectiveDelete-scoreheader "ScoreTagline")
+(d-LimitInterSystemSpace 1.2)
--- actions/Simple.scm
+++ actions/Simple.scm
@@ -25,3 +25,5 @@
 
 (if (and (= 1 (d-GetMovement)) (not (d-Directive-header? "ScoreTitle")))
 (SetHeaderField "title" "Untitled"))
+
+(d-LimitInterSystemSpace 1.2)
