7a33da534219b125c0b9f11f6e5f9003a572aca0
Jeremiah Benham <jjbenham@chicagoguitar.com>
2011-10-25 22:39:40 -0500

    replace gdk_pixbuf_get_from_drawable

--- src/screenshot.c
+++ src/screenshot.c
@@ -299,7 +299,7 @@ screenshot_get_pixbuf (GdkWindow    *window,
       width  = rectangle->width;
       height = rectangle->height;
       if(width>0 && height>0)
-        screenshot = gdk_pixbuf_get_from_drawable (NULL, root, NULL,
+        screenshot = gdk_pixbuf_get_from_window (NULL, root, NULL,
                                               x_orig, y_orig, 0, 0,
                                               width, height);
    }
--- src/view.c
+++ src/view.c
@@ -7053,7 +7053,7 @@ GdkBitmap *bitmap = gdk_pixmap_create_from_data(NULL, Denemo.gui->xbm, width, he
 #endif
 
 
-    GdkPixbuf *pixbuf1 = gdk_pixbuf_get_from_drawable (NULL,  bitmap, NULL, 0,0,0,0, width, height);
+    GdkPixbuf *pixbuf1 = gdk_pixbuf_get_from_window (NULL,  bitmap, NULL, 0,0,0,0, width, height);
 
     GdkPixbuf *pixbuf = gdk_pixbuf_add_alpha (pixbuf1, TRUE, 0,0,0);// 255, 255, 255);
 
