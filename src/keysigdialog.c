b7cf4ab4f641cbf377e119c9a2330907ca810476
rshann <richard.shann@virgin.net>
2011-12-01 11:42:33 +0000

    Fix hang with keysig change

--- src/keysigdialog.c
+++ src/keysigdialog.c
@@ -136,6 +136,7 @@ insert_keysig (keysig_data *kdata)
   DenemoScore *si = Denemo.gui->si;
   measurenode *curmeasure;
   gint tokey, mode;
+  DenemoObject *newkey = NULL;
   tokey = mode = 0;
 
   gint isminor =
@@ -158,7 +159,7 @@ insert_keysig (keysig_data *kdata)
 				       si->currentmeasurenum - 1);
 	      if(curmeasure) {
 		curmeasure->data = g_list_append ((objnode *) curmeasure->data,
-						  dnm_newkeyobj ((tokey - mode),
+						  newkey = dnm_newkeyobj ((tokey - mode),
 								 isminor, mode));
 		if (curmeasure == si->currentmeasure)
 		  si->currentobject =
@@ -170,12 +171,13 @@ insert_keysig (keysig_data *kdata)
       else
 	{
 
-	  object_insert (Denemo.gui, dnm_newkeyobj (tokey - mode, isminor, mode));
+	  object_insert (Denemo.gui, newkey = dnm_newkeyobj (tokey - mode, isminor, mode));
 	  showwhichaccidentalswholestaff ((DenemoStaff *) si->currentstaff->
 					  data);
 	}
       si->cursor_appending = FALSE;
-    adjust_tonal_center( ((keysig*)((DenemoObject*)si->currentobject->data)->object)->accs);
+      if(newkey)
+	adjust_tonal_center( ((keysig*)(newkey->object))->accs);
     }				/* End if */
 
   g_free(kdata);
