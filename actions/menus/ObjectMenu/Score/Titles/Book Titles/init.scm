be180ea20c8d7aac97cb00db818667721a115772
rshann <richard.shann@virgin.net>
2011-12-04 11:17:05 +0000

    Improved book titles - no more searching for that copyright char, buttons for everything

---
+++ actions/menus/ObjectMenu/Score/Titles/Book Titles/init.scm
@@ -0,0 +1,16 @@
+(define (BookTitles::Do field lilyfield initial help)
+	(define tag (string-append "Book" field))
+	(d-LilyPondInclude "book-titling.ily")
+	(let ((chapter (d-DirectiveGet-scoreheader-display tag)))
+  (if (not chapter)
+    (set! chapter initial))
+  (set! chapter (d-GetUserInput initial help chapter))
+  (if chapter
+   (begin
+     (d-SetSaved #f)
+     (if (string-null? chapter)
+  	(d-DirectiveDelete-scoreheader tag)
+      	(begin 
+      		(d-DirectivePut-scoreheader-display tag  chapter)
+      		(d-DirectivePut-scoreheader-override tag  (logior DENEMO_OVERRIDE_TAGEDIT DENEMO_OVERRIDE_GRAPHIC))
+      		(d-DirectivePut-scoreheader-postfix tag (string-append lilyfield " = \"" chapter "\"\n"))))))))
