d6ff856af083bef96ec155e890e516ffc2ba68e3
Jeremiah Benham <jjbenham@chicagoguitar.com>
2011-12-19 10:29:42 -0600

    added test/AllDenemoDirectiveOutputs.scm

---
+++ test/AllDenemoDirectiveOutputs.scm
@@ -0,0 +1,45 @@
+(d-DirectivePut-chord-prefix "test" "%{chord prefix%}")
+(d-DirectivePut-chord-postfix "test" "%{chord postfix%}")
+(d-DirectivePut-chord-postfix "testwithaffix" "%{chord postfix with affix override set%}")
+(d-DirectivePut-chord-prefix "testwithaffix" "%{chord prefix with affix override set%}")
+(d-DirectivePut-chord-override "testwithaffix" DENEMO_OVERRIDE_AFFIX)
+
+(d-DirectivePut-note-prefix "test" "%{note prefix%}")
+(d-DirectivePut-note-postfix "test" "%{note postfix%}")
+
+(d-DirectivePut-staff-prefix "test" "%{staff prefix%}")
+(d-DirectivePut-staff-postfix "test" "%{staff postfix%}")
+
+(d-DirectivePut-voice-prefix "test" "%{voice prefix%}")
+(d-DirectivePut-voice-postfix "test" "%{voice postfix%}")
+
+(d-DirectivePut-score-prefix "test" "%{score prefix%}")
+(d-DirectivePut-score-postfix "test" "%{score postfix%}")
+
+(d-DirectivePut-clef-prefix "test" "%{clef prefix%}")
+(d-DirectivePut-clef-postfix "test" "%{clef postfix%}")
+
+(d-DirectivePut-scoreheader-prefix "test" "%{scoreheader prefix%}\n")
+(d-DirectivePut-scoreheader-postfix "test" "%{scoreheader postfix%}\n")
+
+(d-DirectivePut-movementcontrol-prefix "test" "%{movementcontrol prefix%}\n")
+(d-DirectivePut-movementcontrol-postfix "test" "%{movementcontrol postfix%}\n")
+
+
+;;(d-DirectivePut-movementcontrol-override "test" DENEMO_OVERRIDE_LILYPOND)
+
+(d-DirectivePut-header-prefix "test" "%{header prefix%}\n")
+(d-DirectivePut-header-postfix "test" "%{header postfix%}\n")
+
+(d-DirectivePut-paper-prefix "test" "%{paper prefix%}\n")
+(d-DirectivePut-paper-postfix "test" "%{paper postfix%}\n")
+
+(d-DirectivePut-layout-prefix "test" "%{layout prefix%}\n")
+(d-DirectivePut-layout-postfix "test" "%{layout postfix%}\n")
+
+
+
+(d-DirectivePut-standalone-prefix "test" "%{standalone prefix%}")
+(d-DirectivePut-standalone-postfix "test" "%{standalone postfix%}")
+
+(d-RefreshDisplay)
