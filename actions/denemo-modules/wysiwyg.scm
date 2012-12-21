62fa1e0593fe204f4975f74f01ce5ffd0a559b52
rshann <richard.shann@virgin.net>
2012-12-21 17:34:34 +0000

    Only offer to chop the beaming down when it is possible

--- actions/denemo-modules/wysiwyg.scm
+++ actions/denemo-modules/wysiwyg.scm
@@ -281,11 +281,12 @@
 									(if (> base-duration 3)
 										(begin
 											(set! menu (cons (cons (cons (_"One Beam Right (Off/On)") (_"Put just one beam to the right or undo a previous invocation of this command")) d-BeamRightOne) menu))
-											(set! menu (cons (cons (cons (_"One Beam Left (Off/On)") (_"Put just one beam to the left or undo a previous invocation of this command")) d-BeamLeftOne) menu))))
-									(if (> base-duration 2)
-									  (begin
+											(set! menu (cons (cons (cons (_"One Beam Left (Off/On)") (_"Put just one beam to the left or undo a previous invocation of this command")) d-BeamLeftOne) menu))
+											(set! menu (cons (cons (cons (_"Chop to One Beam") (_"Reduce the beaming between this and the next note to just one beam")) chop-beam) menu))
 											
-									   (set! menu (cons (cons (cons (_"Chop to One Beam") (_"Reduce the beaming between this and the next note to just one beam")) chop-beam) menu))
+											))
+									(if (> base-duration 2)
+									  (begin 
 									   (set! menu (cons (cons (cons (_"No Beam (Off/On)") (_"Leave note/chord un-beamed or undo a previous invocation of this command")) d-NoBeam) menu))
 										 (set! menu (cons (cons (cons (_"Change beam angle/position") (_"Allows you to drag the ends of the beam")) GetBeamPositions) menu))))
 			
