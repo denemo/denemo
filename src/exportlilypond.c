152528399cf21f6f0a7c4400f1cd03f7f0b45d89
rshann <richard.shann@virgin.net>
2012-12-31 16:04:23 +0000

    Fix wrong tag name for continuous typesetting

--- src/exportlilypond.c
+++ src/exportlilypond.c
@@ -2281,7 +2281,7 @@ static void output_score_to_buffer (DenemoGUI *gui, gboolean all_movements, gcha
 			scoreblock_tag = "standard scoreblock";
 
 	insert_scoreblock_section(gui, scoreblock_tag, sb);
-	gtk_text_buffer_get_iter_at_mark(gui->textbuffer, &iter, gtk_text_buffer_get_mark(gui->textbuffer, "standard scoreblock"));
+	gtk_text_buffer_get_iter_at_mark(gui->textbuffer, &iter, gtk_text_buffer_get_mark(gui->textbuffer, scoreblock_tag));
 		/* FIXME make the text editable if the custom scoreblock has been reduced to text */
 	//something like	insert_editable( &sb->lilypond, (sb->lilypond)->str, &iter, gui, 0,0,0,0,0,0,0,0);
 	//but where is the write-back to sb->lilypond (which must be the only copy of course).
