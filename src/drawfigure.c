76cf73544a49795f54dda51205375ab1aa898540
rshann <richard.shann@virgin.net>
2013-03-18 11:15:17 +0000

    Draw figured bass in display nicely

--- src/drawfigure.c
+++ src/drawfigure.c
@@ -9,43 +9,7 @@
 #include "utils.h"
 #include <denemo/denemo.h>
 #include <string.h>
-static gboolean  substitute_accs(gchar *str) {
-  gint accs=FALSE;
-  for(;*str;str++) {   
-	if(*str=='_') {
-	  if(*(str+1)) {	  
-	    if(*(str+1)=='+') {
-	      *str++='#';
-	      *str=' ';	      
-	    } else
-	      if(*(str+1)=='-') {
-		*str++='b';
-		*str=' ';
-	
-	      } else
-		if(*(str+1)=='|')
-		  *str++=' ';	   
-	  } else 
-	    *str=' ';
-	} else
-	  if(g_ascii_isdigit(*str)) {
-	     if(*(str+1)=='+') {
-	       str++;
-	       *str='#';
-	       accs = TRUE;
-	     } else
-	       if(*(str+1)=='-') {
-	       str++;
-	       *str='b';
-	       accs = TRUE;
-	     }
-	  }
-	// else
-	// if(*str=='~')
-	//   *str=' '; 	  
-  }
-  return accs;
-}
+
 /**
  * Draw figured bass on the score
  *
@@ -58,28 +22,43 @@ draw_figure (cairo_t *cr,
   chord *ch;
   if (theobj->type == CHORD)
     {
+			GString *gstr = g_string_new("");
       ch = (chord *) theobj->object;     
       gint ystep=0, xstep=0;
       gint accs=FALSE;
       gchar *orig = ((GString *) (ch->figure))->str;
-      text = g_strdup(orig);
-      gchar *str = strtok (text, " ");
-      while(str) {
-	
-	if(*str=='|') {	 
-	  ystep = 0;
-	  xstep += 25 + (accs?10:0);
-	  accs = FALSE;
-	} else
-	  if(*(orig + (str-text) + strlen(str))==' ' || *(orig + (str-text) + strlen(str))==0 ) {
-	    if(substitute_accs(str))
-	      accs = TRUE;
-	  drawnormaltext_cr( cr, str, xx+xstep, y + STAFF_HEIGHT + 10 + ystep );
-	  ystep+=16;
-	}
-	str = strtok (NULL, " ");
-      }	
-	g_free(text);
+      gchar *str = orig;
+      while(*str) {
+				if(*str=='+') {
+					accs = TRUE, g_string_append(gstr, "♯");
+				} else if(*str=='-') {
+					accs = TRUE, g_string_append(gstr, "♭");
+				}	else if(*str=='!') {
+					accs = TRUE, g_string_append(gstr, "♮");
+				}	else if(*str=='_') {
+							/*do nothing*/; 
+				}	else	if(*str=='|') {	
+					drawlargetext_cr( cr, gstr->str, xx+xstep, y + STAFF_HEIGHT + 20 + ystep );
+					ystep = 0;
+					xstep += 25 + (accs?10:0);
+					g_string_assign(gstr, "");
+					accs = FALSE;
+				} else 	if(*str==' ') {
+						if(gstr->len) {	
+							drawlargetext_cr( cr, gstr->str, xx+xstep, y + STAFF_HEIGHT + 20 + ystep );
+							ystep+=20;
+							g_string_assign(gstr, "");
+						}
+				} else {
+					g_string_append_c(gstr, *str);
+				}
+				str++;
+				if(*str==0) {
+					drawlargetext_cr( cr, gstr->str, xx+xstep, y + STAFF_HEIGHT + 20 + ystep );
+					break;
+				}	
+      }
+		g_string_free(gstr, TRUE);
     }
   //g_debug ("%s\n", text);
 }
