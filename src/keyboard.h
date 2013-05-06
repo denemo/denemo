b8434fc16c161c090fa555e94fd4f26c9847a5e7
Éloi Rivard <azmeuk@gmail.com>
2013-05-07 00:48:25 +0200

    keyboard.c refactoring createtion of keymapio.c that takes xml parts from keyboard.c

--- src/keyboard.h
+++ src/keyboard.h
@@ -14,18 +14,24 @@
 #define KEYBOARDH
 
 #include <denemo/denemo.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
-#include "utils.h"
-#include "prefops.h"
 
-
-gint load_xml_keymap (gchar * filename, gboolean interactive);
-gint save_xml_keymap (gchar * filename);
-gint save_xml_keybindings (gchar * filename);
-gint load_xml_keybindings (gchar * filename);
 void set_visibility_for_action (GtkAction * action, gboolean visible);
-gint parse_paths (gchar * filename, DenemoGUI * gui);
-gint save_script_as_xml (gchar * filename, gchar * myname, gchar * myscheme, gchar * mylabel, gchar * mytooltip, gchar * after);
 void add_ui (gchar * menupath, gchar * after, gchar * name);
+void create_command(gchar* name,
+                    gchar* label,
+                    gchar* scheme,
+                    gchar* tooltip,
+                    gboolean hidden,
+                    gchar* after,
+                    gchar* menupath,
+                    gchar* fallback,
+                    gint merge,
+                    GList* menupaths);
+void create_binding(gchar* tmp, keymap * the_keymap, gint command_number);
+void show_action_of_name (gchar * name);
+void show_action_of_name (gchar * name);
+gchar* translate_binding_dnm_to_gtk (const gchar * dnm_binding);
+gchar* translate_binding_gtk_to_dnm (const gchar * gtk_binding);
+gchar* extract_menupath (gchar * filename);
+
 #endif //KEYBOARDH
