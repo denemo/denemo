03617780298d11ea0873b83f4b9397377cc3cda9
rshann <richard.shann@virgin.net>
2013-05-22 22:10:15 +0100

    use less intense colors for MIDI

--- src/draw.c
+++ src/draw.c
@@ -1358,9 +1358,9 @@ draw_callback (cairo_t * cr)
   else if (gtk_widget_has_focus (Denemo.scorearea))
     {
       if (Denemo.gui->input_source == INPUTMIDI && (Denemo.keyboard_state == GDK_LOCK_MASK || Denemo.keyboard_state == GDK_SHIFT_MASK))      //listening to MIDI-in
-        cairo_set_source_rgb (cr, 0.6, 0.6, 1.0);
+        cairo_set_source_rgb (cr, 0.9, 0.85, 1.0);
       else if (Denemo.gui->input_source == INPUTMIDI && Denemo.keyboard_state == GDK_CONTROL_MASK)      //checking pitches
-        cairo_set_source_rgb (cr, 0.6, 1.0, 0.6);
+        cairo_set_source_rgb (cr, 0.85, 1.0, 0.9);
       else
         cairo_set_source_rgb (cr, ((0xFF0000 & Denemo.color) >> 16) / 255.0, ((0xFF00 & Denemo.color) >> 8) / 255.0, ((0xFF & Denemo.color)) / 255.0);
     }
