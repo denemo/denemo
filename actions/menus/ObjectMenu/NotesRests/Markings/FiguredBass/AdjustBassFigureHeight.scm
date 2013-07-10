7ce9627773865c761db37c1411cdfdbd8c50c730
rshann <richard.shann@virgin.net>
2013-07-10 19:41:29 +0100

    Let figured bass height adjustments recall the value previously set.

--- actions/menus/ObjectMenu/NotesRests/Markings/FiguredBass/AdjustBassFigureHeight.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/FiguredBass/AdjustBassFigureHeight.scm
@@ -1,15 +1,18 @@
 ;;;AdjustBassFigureHeight
-(let ((tag "AdjustBassFigureHeight"))
-	(define offset #f)
+(let ((tag "AdjustBassFigureHeight") (offset #f))
 	(if (not (equal? AdjustBassFigureHeight::params "edit"))
 		(set! offset AdjustBassFigureHeight::params))
 	(if (not offset)
-		(set! offset (d-GetUserInput (_ "Figured Bass Height") (_ "Give height adjustment (unit = staff space): ") "1.0")))
+		(begin
+			(set! offset (d-DirectiveGet-note-display tag))
+			(if (not offset)
+				(set! offset 7.0))
+			(set! offset (d-GetUserInput (_ "Figured Bass Height") (_ "Give height adjustment (unit = staff space): ") offset))))
 	(if (and (string? offset) (string->number offset))
 		(begin
 			(d-DirectivePut-note-prefix tag (string-append "\\once \\override Staff.BassFigureAlignmentPositioning #'Y-offset = #'" offset " "))
 			(d-DirectivePut-note-override tag DENEMO_ALT_OVERRIDE)
-			(d-DirectivePut-note-display tag "^")
+			(d-DirectivePut-note-display tag offset)
 			(d-DirectivePut-note-ty tag -10))	
 		(d-DirectiveDelete-note tag))	
 	(d-RefreshDisplay)
