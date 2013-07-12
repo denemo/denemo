d07c09620fdb066b8f13690ec75bbae80aef3c28
rshann <richard.shann@virgin.net>
2013-07-12 20:22:30 +0100

    Get Grace fix working for multiple grace notes and beamed ones too

--- actions/menus/ObjectMenu/NotesRests/GraceNotes/InstallGraceNoteHints.scm
+++ actions/menus/ObjectMenu/NotesRests/GraceNotes/InstallGraceNoteHints.scm
@@ -7,28 +7,41 @@
       (if (d-NextObjectInMeasure)
           (loop))))
           
+
   (define (get-grace)
-    (d-SetMark) ;;;FIXME single grace notes only
-    (d-Copy))
-    
-  (define (insert-grace-rest)
-          (d-PrevObjectInMeasure)
-          (d-Paste)
-          (d-MoveCursorLeft) ;;;FIXME single grace notes only
-          (d-StagedDelete)
-          (d-SetNonprinting))
-          
-  (define (ensure-grace start-tick) 
+    (define str "(d-InsertBlankWholeNote)(d-ToggleGrace)")
+    (let ((duration (d-GetNoteBaseDuration)) )
+     (set! str (string-append str "(d-Change" (number->string duration) ")(d-MoveCursorRight)")))
+     (let loop ()
+        (if (d-NextChordInMeasure)
+          (if (d-IsGrace)
+            (begin
+              (set! str (string-append str "(d-InsertBlankWholeNote)(d-ToggleGrace)" "(d-Change" (number->string (d-GetNoteBaseDuration)) ")(d-MoveCursorRight)"))
+              (loop)))))
+      str)
+      
+  (define (no-grace-at-tick start-tick)
+    (define ret #t)
+    (d-PushPosition)
     (let loop ()
+        (if (= start-tick (d-GetStartTick))
+          (if (d-IsGrace)
+            (set! ret #f)
+            (if (d-NextObjectInMeasure)
+              (loop)))))
+    (d-PopPosition)
+    (if (not (Music?))
+      (d-NextObjectInMeasure))
+  ret)
+          
+  (define (ensure-grace start-tick grace)
+    (let loop () 
       (if (and (> start-tick (d-GetStartTick)) (d-NextObjectInMeasure))
         (loop)
-        (let inner-loop () 
+        (begin
           (if (= (d-GetStartTick) start-tick)
-            (if (not (d-IsGrace))
-              (if (d-NextObjectInMeasure)
-                (inner-loop)))
-            (if (> (d-GetStartTick) start-tick)
-              (insert-grace-rest)))))))
+            (if (no-grace-at-tick start-tick)
+                  (eval-string grace)))))))
 
 
   (define (dangerous-grace?) 
@@ -44,15 +57,16 @@
   (define (fix-measure)
     (set! last-object 'beginning)
     (if (dangerous-grace?)
-      (let ((start-tick (d-GetStartTick)))
-        (get-grace)
+      (let ((start-tick (d-GetStartTick)) (grace (get-grace)))
+        
         (d-PushPosition)
         (while (MoveUpStaffOrVoice))
-        (ensure-grace start-tick)
+        (while (d-PrevObjectInMeasure)) ;;if it doesn't go up a staff we may not be at the start.
+        (ensure-grace start-tick grace)
         (let loop ()
           (if (MoveDownStaffOrVoice)
             (begin
-              (ensure-grace start-tick)
+              (ensure-grace start-tick grace)
               (loop))))
         (d-PopPosition))))
 
