bd52e2092b748ca3f1e47c301a4260c63581e1e6
rshann <richard.shann@virgin.net>
2013-07-18 18:10:12 +0100

    Add Sustain Pedal commands

---
+++ actions/menus/ObjectMenu/Directives/SustainOff.scm
@@ -0,0 +1,16 @@
+;;;SustainOff
+(let ((tag "SustainOff"))
+ (if (d-Directive-standalone? tag)
+	(d-DirectiveDelete-standalone tag)
+	(begin
+		(if (d-MoveCursorLeft)
+			(if (d-Directive-standalone? tag)
+			  (d-DirectiveDelete-standalone tag)
+				(d-MoveCursorRight)))
+	(StandAloneDirectiveProto (cons "SustainOff" "\\sustainOff") #f "\n!P\nDenemo\n48")
+	(d-DirectivePut-standalone-gy tag -44)
+	(d-DirectivePut-standalone-grob tag tag)
+	(d-MoveCursorRight)))
+(d-RefreshDisplay)
+(d-SetSaved #f))
+		
