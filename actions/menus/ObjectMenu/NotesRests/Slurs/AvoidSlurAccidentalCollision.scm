0a6c9a9e3b87a80c109ed277cf3a74ae22f9a5ef
rshann <richard.shann@virgin.net>
2013-07-24 16:17:21 +0100

    Refine Slur avoidance command

--- actions/menus/ObjectMenu/NotesRests/Slurs/AvoidSlurAccidentalCollision.scm
+++ actions/menus/ObjectMenu/NotesRests/Slurs/AvoidSlurAccidentalCollision.scm
@@ -6,8 +6,11 @@
  		(d-DirectiveDelete-chord  tag)
  		(d-InfoDialog (_ "Slur/Accidental avoidance removed")))
  	(begin
-		(d-DirectivePut-chord-prefix tag "\\once \\override Slur #'details #'edge-attraction-factor = #1 ")
-		(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
-		(d-DirectivePut-chord-display tag "(X")))
+ 		(if (d-IsSlurStart)
+ 			(begin
+				(d-DirectivePut-chord-prefix tag "\\once \\override Slur #'details #'edge-attraction-factor = #1 ")
+				(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
+				(d-DirectivePut-chord-display tag "(X"))
+			(d-InfoDialog (_ "Use only on a slur start to make the slur avoid accidentals on following notes")))))
 (d-RefreshDisplay)
 (d-SetSaved #f))
