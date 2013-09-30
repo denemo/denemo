9299c4809cc8e9bfb82f703ebb3833086140ec92
Richard Shann <richard.shann@virgin.net>
2013-09-30 11:49:09 +0100

    Add a command to export the music of a staff as raw LilyPond

---
+++ actions/menus/MainMenu/FileMenu/Export/QuickLilyPondPart.scm
@@ -0,0 +1,16 @@
+;;;QuickLilyPondPart
+(d-PushPosition)
+(d-GoToBeginning)
+(let ((name  (d-Open "query=filename")))
+	(if name
+		(let* ((filename  (string-append (substring name 0 (- (string-length name) 7)) "-" (number->string (d-GetStaff)) ".ly"))   
+				(port (open-file filename "w")))
+			(let loop ()
+				(if port
+					(begin 
+						(format port "~A " (d-GetLilyPond))
+						(if (d-MoveCursorRight)
+							(loop)
+							(close-port port))))))
+		(d-WarningDialog (_ "The score has not been saved, so no name or location for the part is defined"))))
+(d-PopPosition)	
