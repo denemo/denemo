ace647e877b7a71c83ab71412d03097b562ede79
Richard Shann <richard.shann@virgin.net>
2013-11-09 17:30:52 +0000

    Give feedback when starting a tied note

--- actions/menus/ObjectMenu/NotesRests/TiedNotes/InsertTiedNoteDuration.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/InsertTiedNoteDuration.scm
@@ -2,7 +2,9 @@
 ; Toggles a tie and moves right for insert with duration key. Gives audible feedback
 (d-ToggleTie)
 (if (d-IsTied)
-	(d-PendingMidi 75)
-	(d-PlayMidiNote 35 255 9 100))
+	(begin
+		(d-PlayMidiNote 59 255 9 10)
+		(d-PendingMidi 71))
+	(d-PlayMidiNote 41 255 9 10))
 (if (not (d-IsAppending))
 	(d-MoveCursorRight))
