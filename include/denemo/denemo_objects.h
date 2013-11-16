bed6a28e467fb130a898ff78a131e17330a70ba4
Richard Shann <richard.shann@virgin.net>
2013-11-16 12:51:45 +0000

    Remove unused field

--- include/denemo/denemo_objects.h
+++ include/denemo/denemo_objects.h
@@ -139,7 +139,6 @@ typedef struct chord
   gboolean is_grace;  /**< Flag to show whether note is a grace note */
   gboolean struck_through; /**< Flag for a struck through stem*/
   gint stemy;		/**< Stores the y position of the end of the stem */
-  gboolean is_highlighted; /**< If set true changes the notes color */
   GString *lyric; /**< Pointer to the lyrics applied to that chord */
   gboolean is_syllable; /**< Is the lyric a syllable? */
   gboolean center_lyric; /**< Should the lyrics be centered or 
