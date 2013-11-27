257a4d9630656b2df2ab7042caba6c31bd02938c
Richard Shann <richard.shann@virgin.net>
2013-11-27 17:52:08 +0000

    Move silencing to the create click staff routine. Put in tempo markings

--- actions/menus/MainMenu/FileMenu/OpenMenu/GuidedMidiImport.scm
+++ actions/menus/MainMenu/FileMenu/OpenMenu/GuidedMidiImport.scm
@@ -1,8 +1,6 @@
 ;;;GuidedMidiImport
 (if  (d-New)
 (begin
-	(d-MasterVolume 0)
 	(d-ImportMidi "guided=true")
 	(d-CreateClickStaffForMidi)
-	(d-LoadMidiTrack)
-	(d-MasterVolume )))
+	(d-LoadMidiTrack)))
--- actions/menus/ObjectMenu/StaffMenu/InsertStaff/CreateClickStaffForMidi.scm
+++ actions/menus/ObjectMenu/StaffMenu/InsertStaff/CreateClickStaffForMidi.scm
@@ -1,4 +1,4 @@
-;;;CreateClickTrackForMidi
+;;;CreateClickStaffForMidi
 (let ()
  (define (writeBar numerator denominator)
     (let loop ((count numerator))
@@ -7,16 +7,25 @@
 	    (eval-string (string-append "(d-" (number->string (duration::lilypond->denemo denominator)) ")"))
 	    (loop (- count 1))))))
 	
- (define (writeAllBars duration tempo)	
+ (define (writeAllBars duration tempo old_tempo)	
 	(if (and duration tempo)
 		(let ((numer (list-ref tempo 1))  (denom (list-ref tempo 2)) (spqn (list-ref tempo 3)) )
 			(define seconds_per_bar (* spqn (* numer (* (/ 4 denom)))))
 			(define bars (round (/ duration seconds_per_bar)))
 			(define thetimesig (string-append (number->string numer) "/" (number->string denom)))
+			
 			(if (not (None?))
 							(d-AddMeasure))
 			(if (not (equal? (d-GetPrevailingTimesig) thetimesig))
 				(d-InsertTimeSig thetimesig))
+			(if (or (not old_tempo) (not (equal? (list-ref tempo 3) (list-ref old_tempo 3))))
+				(let ((tag "MetronomeMark")(bpm (* 60 (/ 1 (list-ref tempo 3)))))
+					(d-DirectivePut-standalone tag)
+				    (d-DirectivePut-standalone-override tag (logior DENEMO_OVERRIDE_TAGEDIT DENEMO_OVERRIDE_TEMPO DENEMO_OVERRIDE_STEP))
+					(d-DirectivePut-standalone-midibytes tag (number->string bpm))
+					(d-DirectivePut-standalone-display tag (number->string bpm))
+					(d-DirectivePut-standalone-minpixels tag 30)	
+				))
 			(let loop ((count bars))
 				(if (positive? count)
 					(begin					
@@ -27,9 +36,11 @@
 	(define old-time 0)
 	(define tempo #f)    
 	(define old_highlight (d-HighlightCursor #f))
+	(define old_volume (d-MasterVolume))
 	(define next-tempo #f)
-	
-	;;; the procedure	
+	(define old_tempo #f)
+	;;; the procedure
+	(d-MasterVolume 0)	
 	(let loop ((count 0))
 		(set! tempo (d-GetRecordedMidiTempo count))
 		(set! next-tempo #f)
@@ -44,8 +55,9 @@
 						(set! duration (- (d-GetRecordedMidiDuration) old-time)))))		
 			(begin
 				(set! duration (d-GetRecordedMidiDuration))))
-		(writeAllBars duration tempo)
+		(writeAllBars duration tempo old_tempo)
+		(set! old_tempo tempo)
 		(if next-tempo				
 			(loop (1+ count))))
+	(d-MasterVolume old_volume)		
 	(d-HighlightCursor old_highlight))
-
