f42f6dfcd30767e8375d70d4f25095848068de3e
Richard Shann <richard.shann@virgin.net>
2013-11-30 17:06:40 +0000

    Do not ask to select track if there is only one

--- actions/menus/ObjectMenu/StaffMenu/InsertStaff/MIDI/LoadMidiTrack.scm
+++ actions/menus/ObjectMenu/StaffMenu/InsertStaff/MIDI/LoadMidiTrack.scm
@@ -2,22 +2,25 @@
 (let ((n "1")(max (d-GetImportedMidiTracks)))
   (if max
      (begin
-	(if LoadMidiTrack::params
-		(set! n LoadMidiTrack::params)
-		(begin
-			(set! n (d-GetUserInput (_ "MIDI Track Selection") 
-			(string-append (_ "Select track to import") "1 - " (number->string max) ":")
-			
-			 "1"))))
-	(if n
-		(begin
-		(set! n (string->number n))
-		(if (and (<= n max) (> n 0))
-		   (begin
-			(while (d-MoveToStaffUp))
-			(d-NewStructuredStaff)
-			(d-GetImportedMidiTrack n)
-			(d-AdvanceMarkedMidi 0))
-		  (d-WarningDialog (_ "Out of range"))))
-		  (d-InfoDialog (_ "Cancelled"))))
-         (d-WarningDialog (_ "No MIDI file loaded"))))
+		(if LoadMidiTrack::params
+			(set! n LoadMidiTrack::params)
+			(begin
+				(if (= max 1)
+					(set! n "1")
+					(begin
+						(set! n (d-GetUserInput (_ "MIDI Track Selection") 
+						(string-append (_ "Select track to import") " 1 ... " (number->string max) ":")
+										"1"))))))
+			(if n
+				(begin
+				(set! n (string->number n))
+				(if (and (<= n max) (> n 0))
+				   (begin
+					(while (d-MoveToStaffUp))
+					(d-NewStructuredStaff)
+					(d-GetImportedMidiTrack n)
+					(d-AdvanceMarkedMidi 0))
+				  (d-WarningDialog (_ "Out of range"))))
+				  (d-InfoDialog (_ "Cancelled"))))
+		         (d-WarningDialog (_ "No MIDI file loaded"))))
+		
