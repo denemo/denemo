ca216a17db2d7d6689dc8aea0d7087875457b597
Richard Shann <richard.shann@virgin.net>
2013-12-12 08:47:24 +0000

    position cursor at end of bar after recording MIDI

--- actions/menus/MainMenu/PlaybackMenu/RecordAndConvert.scm
+++ actions/menus/MainMenu/PlaybackMenu/RecordAndConvert.scm
@@ -1,21 +1,21 @@
 ;;RecordAndConvert
-	(define-once RecordAndConvert::position #f)
-	(define DenemoClickTrack (_ "Click"))
-	(let ((position (GetPosition)))
-		(set! RecordAndConvert::position position)
-		(while (d-MoveToStaffUp))
-		(if  (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
-			(begin
-				(d-InitialTimeSig)
-				(d-CreateClickStaffForMidi)
-				(d-GoToPosition #f  (+ 1 (list-ref position 1)) (list-ref position 2)  (list-ref position 3)))
-			(begin
-				(d-GoToPosition #f (list-ref position 1) (list-ref position 2)  (list-ref position 3))))	
-		(d-MoveToMeasureLeft)
-		(d-MidiRecord "(d-ComputeMidiNoteDurations)
-						(d-FirstNoteOnset)
-						(while (d-MoveToStaffUp))
-						(d-CursorToMarkedMidiNotePosition)
-						(d-MoveToStaffDown)"))
-						
+    (define-once RecordAndConvert::position #f)
+    (define DenemoClickTrack (_ "Click"))
+    (let ((position (GetPosition)))
+        (set! RecordAndConvert::position position)
+        (while (d-MoveToStaffUp))
+        (if  (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
+            (begin
+                (d-InitialTimeSig)
+                (d-CreateClickStaffForMidi)
+                (d-GoToPosition #f  (+ 1 (list-ref position 1)) (list-ref position 2)  (list-ref position 3)))
+            (begin
+                (d-GoToPosition #f (list-ref position 1) (list-ref position 2)  (list-ref position 3))))    
+        (d-MoveToMeasureLeft)
+        (d-MidiRecord "(d-ComputeMidiNoteDurations)
+                        (d-FirstNoteOnset)
+                        (while (d-MoveToStaffUp))
+                        (d-CursorToMarkedMidiNotePosition)
+                        (d-MoveToStaffDown)(while (d-NextObjectInMeasure))(d-MoveCursorRight)"))
+                        
 
