527ef0f99ba99d93c89afe98b296022c72d7edb0
Richard Shann <richard.shann@virgin.net>
2013-12-16 10:23:50 +0000

    Rename command to match action

---
+++ actions/menus/MainMenu/PlaybackMenu/PlayAndRecordMidiIn.scm
@@ -0,0 +1,28 @@
+;;PlayAndRecordMidiIn
+    (define-once PlayAndRecordMidiIn::position #f)
+    (define DenemoClickTrack (_ "Click"))
+    (let ((position (GetPosition)))
+        (define success #f)
+        (set! PlayAndRecordMidiIn::position position)
+        (while (d-MoveToStaffUp))
+        (if  (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
+            (begin
+                (d-InitialTimeSig)
+                (d-CreateClickStaffForMidi)
+                (set! success  (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
+                (if success
+                    (d-GoToPosition #f  (+ 1 (list-ref position 1)) (list-ref position 2)  (list-ref position 3))))
+            (begin
+                (set! success #t)
+                (d-GoToPosition #f (list-ref position 1) (list-ref position 2)  (list-ref position 3))))  
+        (if success  
+            (begin
+                (d-MoveToMeasureLeft)
+                (d-MidiRecord "(d-ComputeMidiNoteDurations)
+                            (d-FirstNoteOnset)
+                            (while (d-MoveToStaffUp))
+                            (d-CursorToMarkedMidiNotePosition)
+                            (d-MoveToStaffDown)
+                            (if (not (None?)) (begin (while (d-NextObjectInMeasure))(d-MoveCursorRight)))"))))
+                        
+
