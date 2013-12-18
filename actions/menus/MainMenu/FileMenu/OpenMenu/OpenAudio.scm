bfee42b5223b88c20b946be349bf45f0f9554488
Richard Shann <richard.shann@virgin.net>
2013-12-18 14:51:21 +0000

    Improve open audio click track

--- actions/menus/MainMenu/FileMenu/OpenMenu/OpenAudio.scm
+++ actions/menus/MainMenu/FileMenu/OpenMenu/OpenAudio.scm
@@ -1,26 +1,38 @@
 ;;;;;;;;;;;;;;OpenAudio
-(d-PushPosition)
+(define DenemoClickTrack "Click")
+
+
+
 (let ((seconds (d-OpenSourceAudioFile)))
-	(if (and seconds (EmptyMeasure?) (not (d-MoveToMeasureRight)) (not (d-MoveToStaffDown)))
-		(let ( (timesig (d-InitialTimeSig "query=timesigname")) (numerator #f)(denominator #f))
-			(set! numerator (string->number (car (string-split   timesig #\/))))
-			(set! denominator (string->number (cadr (string-split  timesig #\/))))
-			
-			(let loop ((count numerator))
-	 			(if (> count 0)
-	 				(begin
-	 					(eval-string (string-append "(d-" (number->string (duration::lilypond->denemo denominator)) ")")) 
-	 					(d-SetNonprinting)
-	 					(d-CursorUp)
-	 					(loop (- count 1)))))
-	 					
-	 		(d-RecreateTimebase)
-	 					
-	 		(let loop ((count (/ seconds (d-GetMidiOffTime)))) 
-				(if (> count 0)
-					(begin 
-						(d-AddDuplicateMeasure)
-						(loop (- count 1)))))
-			(d-SetPlaybackInterval 0.0 seconds))))
-						
-(d-PopPosition)	
+    (define old_volume (d-MasterVolume)) 
+    (if (and seconds (EmptyMeasure?) (not (d-MoveToMeasureRight)) (not (d-MoveToStaffDown)))
+        (let ( (timesig (d-InitialTimeSig "query=timesigname")) (numerator #f)(denominator #f))
+            (set! numerator (string->number (car (string-split   timesig #\/))))
+            (set! denominator (string->number (cadr (string-split  timesig #\/))))
+           (d-MasterVolume 0) 
+            (d-CursorToNote "c'")
+            (d-NonPrintingStaff)
+            (d-StaffProperties (string-append "denemo_name=" DenemoClickTrack))            
+            (let loop ((count numerator))
+                (if (> count 0)
+                    (begin
+                        (eval-string (string-append "(d-" (number->string (duration::lilypond->denemo denominator)) ")")) 
+                        (d-SetNonprinting)
+                        (d-CursorUp)
+                        (loop (- count 1)))))
+                        
+            (d-RecreateTimebase)
+ 
+                    
+            (let loop ((count (/ seconds (d-GetMidiOffTime)))) 
+                (if (> count 0)
+                    (begin 
+                        (d-AddDuplicateMeasure)
+                        (loop (- count 1)))))
+            (d-MuteStaff "unmute")      
+            (d-MoveToBeginning)
+            (d-MasterVolume old_volume)         
+                        
+            (d-SetPlaybackInterval 0.0 seconds)
+                        
+            (d-NewStaffAfter))))
