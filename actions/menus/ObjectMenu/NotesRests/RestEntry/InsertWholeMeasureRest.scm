2af6b8f02e38372c2510962af7cd952fe7e88582
Richard Shann <richard.shann@virgin.net>
2014-01-06 13:50:31 +0000

    Make whole measure rest commands behave sanely when invoked in measures containing notes

--- actions/menus/ObjectMenu/NotesRests/RestEntry/InsertWholeMeasureRest.scm
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/InsertWholeMeasureRest.scm
@@ -1,13 +1,17 @@
 ;;;;InsertWholeMeasureRest
- (if (or (None?)  (zero? (d-GetDurationInTicks)))
- 	(begin
- 		(d-WholeMeasureRest))
- 	(begin	
- 		(if (d-MoveToMeasureRight)
- 			(d-WholeMeasureRest)
- 			(begin
-          			 (d-AppendMeasureAllStaffs)	
-          			 (d-MoveCursorRight)
-	  			  (d-WholeMeasureRest)))))
-(d-MoveCursorRight)
-(d-Set2)
+(if (or (None?)  (zero? (d-GetDurationInTicks)))
+    (begin
+        (d-WholeMeasureRest)
+        (d-MoveCursorRight)
+        (d-Set2))
+    (begin  
+        (if (d-MoveToMeasureRight)
+             (if (or (None?)  (zero? (d-GetDurationInTicks)))
+                (d-WholeMeasureRest)
+                (d-MoveToMeasureLeft))
+            (begin
+                (d-AppendMeasureAllStaffs)  
+                (d-MoveCursorRight)
+                (d-WholeMeasureRest)
+                (d-Set2)
+                (d-MoveCursorRight)))))
