0384b1fb089d30e3e682001701297fdf2d2d0d5d
Richard Shann <richard.shann@virgin.net>
2014-01-15 10:51:01 +0000

    mark arranger as new for tracing it

--- actions/Arranger.scm
+++ actions/Arranger.scm
@@ -9,18 +9,18 @@
     (let ((ok #f))
       (d-InfoDialog (_ "Score Check: Error in this measure"))
       (set! ok (RadioBoxMenu (cons (_ "Print Anyway") "y") (cons (_ "Cancel") "n")))
-			(if (equal? ok "n")
-				(begin
-					(d-PopPushPosition);;goes to the position on the top of the stack, replacing the top of the stack with the position before it went
-					(d-PopPosition);;goes to the error position that was pushed
-					(exit)))))
-	(d-PopPosition))
+            (if (equal? ok "n")
+                (begin
+                    (d-PopPushPosition);;goes to the position on the top of the stack, replacing the top of the stack with the position before it went
+                    (d-PopPosition);;goes to the error position that was pushed
+                    (exit)))))
+    (d-PopPosition))
 (d-LimitInterSystemSpace 1.2)
 ;;(d-NormalBeamEndings)
 ;(d-PointAndClick)
 
 (if DenemoPref_newbie
-	(d-MouseInsertion))
+    (d-MouseInsertion))
 
-(display "Arranger Profile\n")
+(display "New Arranger Profile\n")
 (d-SetSaved #t)
