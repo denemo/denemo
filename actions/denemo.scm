c267c1430ee5ba17a84bd1d8ed75d2066fa97039
Richard Shann <richard.shann@virgin.net>
2014-01-30 17:54:42 +0000

    Ensure font is specified for link symbol

--- actions/denemo.scm
+++ actions/denemo.scm
@@ -823,7 +823,7 @@
   (d-Directive-standalone  "DenemoLink")
   (d-DirectivePut-standalone-postfix "DenemoLink" (string-append "%{" filepos "%}"))
   (d-DirectivePut-standalone-minpixels "DenemoLink" 30)
-  (d-DirectivePut-standalone-graphic "DenemoLink" "\n⬁\n\n36")
+  (d-DirectivePut-standalone-graphic "DenemoLink" "\n⬁\ndenemo\n36")
   (d-DirectivePut-standalone-gy "DenemoLink" 10)
   (d-MoveCursorRight)
   (d-RefreshDisplay))
