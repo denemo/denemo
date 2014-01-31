7e72f3d598262eeedea624799b843bab5b8e471a
Richard Shann <richard.shann@virgin.net>
2014-01-31 09:48:28 +0000

    Fix table of contents for 2.18

--- actions/menus/ObjectMenu/Score/Titles/BookTitles/TocTitle.scm
+++ actions/menus/ObjectMenu/Score/Titles/BookTitles/TocTitle.scm
@@ -2,27 +2,27 @@
 (d-LilyPondInclude "book-titling.ily")
 (d-PushPosition)
 (while (d-PreviousMovement))
-(let ((tag "TableOfContents") (title "Contents") (pb 3) (mc  " \\markuplines \\table-of-contents\n"))
+(let ((tag "TableOfContents") (title "Contents") (pb 3) (mc  " \\markuplist \\table-of-contents\n"))
 (if (d-Directive-paper? tag)
-	(set! title (d-DirectiveGet-paper-display tag)))
+    (set! title (d-DirectiveGet-paper-display tag)))
 (set! title (d-GetUserInput (_ "Table of Contents Title") (_ "Give title for the table of contents\nBlank to delete") title))
 (set! pb (d-GetUserInput (_ "Table of Contents Page Break Control") (_ "Give 0 - No page breaks\n1 - Page Break before Table of Contents\n2 - Page Break after\n3 - Both") pb))
 
 (if pb
-	(cond ((equal? pb "1")
-			(set! mc (string-append "\n\\pageBreak\n" mc)))
-		    ((equal? pb "2") 
-		    	(set! mc (string-append mc "\n\\pageBreak\n")))
-		    ((equal? pb "3") 
-		    	(set! mc (string-append  "\n\\pageBreak\n" mc "\\pageBreak\n")))))
+    (cond ((equal? pb "1")
+            (set! mc (string-append "\n\\pageBreak\n" mc)))
+            ((equal? pb "2") 
+                (set! mc (string-append mc "\n\\pageBreak\n")))
+            ((equal? pb "3") 
+                (set! mc (string-append  "\n\\pageBreak\n" mc "\\pageBreak\n")))))
 
 (if (string-null? title)
-	(begin
-		(d-DirectiveDelete-paper tag)
-		(d-DirectiveDelete-movementcontrol tag))
-	(begin
-		(d-DirectivePut-paper-postfix tag (string-append "\ntocTitle = \"" title "\"\n"))
-		(d-DirectivePut-paper-display tag title)
-		(d-DirectivePut-movementcontrol-prefix tag mc)))
+    (begin
+        (d-DirectiveDelete-paper tag)
+        (d-DirectiveDelete-movementcontrol tag))
+    (begin
+        (d-DirectivePut-paper-postfix tag (string-append "\ntocTitle = \"" title "\"\n"))
+        (d-DirectivePut-paper-display tag title)
+        (d-DirectivePut-movementcontrol-prefix tag mc)))
 (d-SetSaved #f))
 (d-PopPosition)
