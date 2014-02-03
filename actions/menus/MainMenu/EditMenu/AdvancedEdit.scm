bde3d705492a6f58124bc71dc6a7d923f260b9df
Richard Shann <richard.shann@virgin.net>
2014-02-03 12:42:42 +0000

    Add advanced edit command for standalones

---
+++ actions/menus/MainMenu/EditMenu/AdvancedEdit.scm
@@ -0,0 +1,5 @@
+;;;AdvancedEdit
+(if (d-Directive-standalone?)
+	(if  (not (d-DirectiveTextEdit-standalone))
+		(d-DeleteObject))
+	(d-InfoDialog (_ "To edit a directive attached to the object at the cursor\nright click and choose edit directives")))
