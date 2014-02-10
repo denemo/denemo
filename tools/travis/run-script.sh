97daf726c37ce1eb3bc17f2a683b9ec12002ff82
Éloi Rivard <azmeuk@gmail.com>
2014-02-10 11:52:03 +0100

    Ensure that repository is clean after tests

--- tools/travis/run-script.sh
+++ tools/travis/run-script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+check_status()
+{
+  STATUS=`git status -s`
+  if [ "$STATUS"x = ""x ]; then
+     #Success
+     return 0;
+  else
+     #Failure
+     echo "ERROR: Directory is not clean. Please clean it up during the tests, or update .gitignore file"
+     git status
+     return 1;
+  fi
+}
+
 if [ "$TEST"x = "coverage"x ]; then
   CONFIGURE_FLAGS="--enable-gcov"
 fi
@@ -28,8 +42,13 @@ fi
 
 if [ "$TEST"x = "integration"x ]; then
   make distcheck
+  rm -rf denemo-*.tar.gz
 fi
 
 if [ "$TEST"x = "coverage"x ]; then
   make check
 fi
+
+#Should be the last command ran, it checks that the repository is clean after tests are ran.
+check_status
+
