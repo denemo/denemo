d5d7123db2bbb2a9b3b27ff48065b744fca2031d
Richard Shann <richard.shann@virgin.net>
2014-03-05 10:29:54 +0000

    Give longer notice, and avoid translating markup

--- actions/menus/ObjectMenu/NotesRests/RestEntry/WholeMeasureRest.scm
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/WholeMeasureRest.scm
@@ -4,7 +4,7 @@
     (set! timesig (d-InsertTimeSig "query=timesigname")) 
     (if (not (d-Directive-chord? DenemoWholeMeasureRestTag))
         (d-InsertWholeRest)
-        (Help::TimedNotice  (_ "<span font_desc=\"16\" foreground=\"blue\">Duration of whole measure rest has been re-calculated</span>")))
+        (Help::TimedNotice (string-append "<span font_desc=\"16\" foreground=\"blue\">" (_ "Duration of whole measure rest has been re-calculated") "</span>") 5000))
     (d-SetDurationInTicks (* 1536 (GetPrevailingTimeSig #t)))
     (d-DirectivePut-chord-graphic DenemoWholeMeasureRestTag "\n\x20")
     (d-DirectivePut-chord-gx DenemoWholeMeasureRestTag 60)
