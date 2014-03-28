d855fce89c4791fca33b4d1c208049d26eed5c4d
Richard Shann <richard.shann@virgin.net>
2014-03-28 14:54:44 +0000

    Make DynamicText accept a parameter

--- actions/menus/ObjectMenu/Directives/Markings/DynamicText.scm
+++ actions/menus/ObjectMenu/Directives/Markings/DynamicText.scm
@@ -9,76 +9,85 @@
    ("sf" "" "sf") ("sff" "" "sff") ("sp" "" "sp" ) ("spp" "" "spp") ("sfz" "" "sfz") ("rfz" "" "rfz") ("Custom" "" "")))
   (define (firstmenu choices thelist)
       (if (and (not (null? thelist))
-	  (not (equal? (car (car thelist)) (_ "More"))))
-	(begin
-	    (set! choices (string-append choices (car (car thelist)) stop))
-	    (firstmenu choices (cdr thelist)))
-	(set! choice (d-GetOption (string-append choices (_ "More") stop)))))
+      (not (equal? (car (car thelist)) (_ "More"))))
+    (begin
+        (set! choices (string-append choices (car (car thelist)) stop))
+        (firstmenu choices (cdr thelist)))
+    (set! choice (d-GetOption (string-append choices (_ "More") stop)))))
    
   (if (d-Directive-standalone? tag)
     (set! replace #t))
    (d-PushPosition)
    (if (d-MoveCursorLeft)
-    	(if (d-Directive-standalone? tag)
-    		(begin
-    			(d-PopPosition)
-    			(d-MoveCursorLeft)
-    			(set! replace #t))
-    		(begin
-    			(d-PopPosition))))
-    		
-
-  (if DenemoDynamicList
-      (set! DynamicList DenemoDynamicList)
-      (set! DynamicList DefaultDynamicList))
-
-
-  (firstmenu "" DynamicList)
+        (if (d-Directive-standalone? tag)
+            (begin
+                (d-PopPosition)
+                (d-MoveCursorLeft)
+                (set! replace #t))
+            (begin
+                (d-PopPosition))))
+            
+(if DynamicText::params
+    (begin
+        (set! choice DynamicText::params)
+        (set! DynamicList DefaultDynamicList))
+    (begin
+          (if DenemoDynamicList
+              (set! DynamicList DenemoDynamicList)
+              (set! DynamicList DefaultDynamicList))
+          (firstmenu "" DynamicList)))
+          
   (let loop ()
     (if (and choice (equal? choice (_ "More")) )
       (begin
-	(if (not (equal? DenemoDynamicList DefaultDynamicList)) ;;we used a custom list, switch to using the Default list and try again
-	  (begin
-	    (set! DenemoDynamicList DefaultDynamicList)
-	    (set! DynamicList DefaultDynamicList)
-	    (firstmenu "" DynamicList)
-	    (loop))))))
+    (if (not (equal? DenemoDynamicList DefaultDynamicList)) ;;we used a custom list, switch to using the Default list and try again
+      (begin
+        (set! DenemoDynamicList DefaultDynamicList)
+        (set! DynamicList DefaultDynamicList)
+        (firstmenu "" DynamicList)
+        (loop))))))
 
  (if (and choice (equal? choice (_ "More")) )
-      (begin	    
-	(set! DynamicList DefaultDynamicList)
-	(set! choice (d-GetOption (string-append "ppppp" stop "pppp" stop  "ffff" stop "fp" stop "sf" stop  "sff" stop "sp"  stop "spp" stop "sfz" stop "rfz" stop "Custom" stop)))))
+      (begin        
+    (set! DynamicList DefaultDynamicList)
+    (set! choice (d-GetOption (string-append "ppppp" stop "pppp" stop  "ffff" stop "fp" stop "sf" stop  "sff" stop "sp"  stop "spp" stop "sfz" stop "rfz" stop "Custom" stop)))))
 
 
   (if choice
     (begin
- 	(set! LilyString (string-append " \\" choice  ))
-	(set! X (assoc choice DynamicList) )
-	(set! level (car (cdr X)) )
-	(set! Graphic (car (cdr (cdr X) ) ) ) 
-	(if (equal? choice "Custom" ) (begin 
-   		(set! choice (d-GetUserInput (_ "Custom dynamic") (_ "Enter dynamic text:") "" ) )
-   		(if choice
-        		(set! LilyString (string-append  " $(make-dynamic-script (markup #:normal-text #:bold #:italic \"" choice "\")) " )))))
-    (if (equal? level "") 
-      (begin
-				(set! level (d-GetUserInput (_ "Dynamic setting") (_ "Enter loudness level (0-127):") "63" ) ) 
-				(let ( (a 0)) 
-					(set! a (string->number level) )	
-					(if (or (boolean? a) (> a 127) (< a 0) )(set! level #f) ))))
+    (set! LilyString (string-append " \\" choice  ))
+    (set! X (assoc choice DynamicList) )
+    (if X
+        (begin
+            (set! level (car (cdr X)) )
+            ;(set! Graphic (car (cdr (cdr X) ) ) ) 
+            (if (equal? choice "Custom" ) (begin 
+                (set! choice (d-GetUserInput (_ "Custom dynamic") (_ "Enter dynamic text:") "" ) )
+                (if choice
+                        (set! LilyString (string-append  " $(make-dynamic-script (markup #:normal-text #:bold #:italic \"" choice "\")) " )))))
+            (if (equal? level "") 
+              (begin
+                        (set! level (d-GetUserInput (_ "Dynamic setting") (_ "Enter loudness level (0-127):") "63" ) ) 
+                        (let ( (a 0)) 
+                            (set! a (string->number level) )    
+                            (if (or (boolean? a) (> a 127) (< a 0) )(set! level #f) )))))
+        (begin
+            (set! level "127")
+            (set! LilyString (string-append  " $(make-dynamic-script (markup #:normal-text #:bold #:italic \"" choice "\")) " ))))))
+        
     (if choice
       (begin 
-    	(if (not replace) (d-DirectivePut-standalone tag))
-    	(d-DirectivePut-standalone-prefix tag  "<>")	
-			(d-DirectivePut-standalone-postfix tag  LilyString)	   
-			(d-DirectivePut-standalone-graphic  tag (string-append "\n" choice "\nSerif\n24\n1\n1"))
-			(d-DirectivePut-standalone-gy tag 40)
-			(d-DirectivePut-standalone-gx tag 12)
-			(if level
+        (if (not replace) (d-DirectivePut-standalone tag))
+        (d-DirectivePut-standalone-prefix tag  "<>")    
+            (d-DirectivePut-standalone-postfix tag  LilyString)    
+            (d-DirectivePut-standalone-graphic  tag (string-append "\n" choice "\nSerif\n24\n1\n1"))
+            (d-DirectivePut-standalone-gy tag 40)
+            (d-DirectivePut-standalone-gx tag 12)
+            (if level
            (begin
-						(d-DirectivePut-standalone-override tag (logior DENEMO_OVERRIDE_STEP DENEMO_OVERRIDE_VOLUME))
-						(d-DirectivePut-standalone-midibytes tag level)))
-			(d-DirectivePut-standalone-minpixels tag 10)
-			(d-SetSaved #f)
-			(d-MoveCursorRight))))))
+                        (d-DirectivePut-standalone-override tag (logior DENEMO_OVERRIDE_STEP DENEMO_OVERRIDE_VOLUME))
+                        (d-DirectivePut-standalone-midibytes tag level)))
+            (d-DirectivePut-standalone-minpixels tag 10)
+            (d-SetSaved #f)
+            (d-MoveCursorRight))))
 (d-RefreshDisplay)
