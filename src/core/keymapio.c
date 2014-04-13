7259a14679cd09741dc7f2d2262c4e4820aebf77
Éloi Rivard <azmeuk@gmail.com>
2014-04-13 21:01:40 +0200

    Unused variable.

--- src/core/keymapio.c
+++ src/core/keymapio.c
@@ -728,7 +728,6 @@ save_command_data (gchar * filename, gchar * myscheme)
 gchar *
 load_command_data (gint idx)
 {
-  GAction* action = lookup_action_from_idx(Denemo.map, idx);
   gchar *basename = lookup_name_from_idx (Denemo.map, idx);
   gchar *filename = g_strconcat (basename, SCM_EXT, NULL);
   gchar* path = NULL;
