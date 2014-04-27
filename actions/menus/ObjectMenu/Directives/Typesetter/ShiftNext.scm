c8badc11a8c233b1d4ba76fedc86317784ae762c
Richard Shann <richard.shann@virgin.net>
2014-04-27 10:17:09 +0100

    Improve feedback for Shift Next command

--- actions/menus/ObjectMenu/Directives/Typesetter/ShiftNext.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/ShiftNext.scm
@@ -1,8 +1,10 @@
 ;;ShiftNext
-(let (( lilytype (GetTypeAsLilypond))  
-	( lilycontext  (GetContextAsLilypond))
-	( X (d-GetUserInput (_ "X-Y shift") (_ "Give horizontal shift required") "1.5"))
-	( Y  (d-GetUserInput (_ "X-Y shift") (_ "Give vertical shift required") "0.0")))
-(if (and lilytype X Y)
-  (StandAloneDirectiveProto (cons "ShiftNext" (string-append  "\\once \\override " lilycontext "." lilytype " #'extra-offset = #'(" X " . " Y")"  )))
-  #f))
+(let (( lilytype (GetTypeAsLilypond)))
+	(if lilytype
+		(let (( lilycontext  (GetContextAsLilypond))
+			( X (d-GetUserInput (_ "X-Y shift") (_ "Give horizontal shift required") "1.5"))
+			( Y  (d-GetUserInput (_ "X-Y shift") (_ "Give vertical shift required") "0.0")))
+				(if (and X Y)
+				  (StandAloneDirectiveProto (cons "ShiftNext" (string-append  "\\once \\override " lilycontext "." lilytype " #'extra-offset = #'(" X " . " Y")"  )))
+				  #f))
+		(d-WarningDialog (_ "This type of object cannot be shifted by this method"))))
