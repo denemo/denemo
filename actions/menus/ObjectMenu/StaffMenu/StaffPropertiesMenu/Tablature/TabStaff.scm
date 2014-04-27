8a75360317c51b0715b10caa4094e08a2a7ed77b
Richard Shann <richard.shann@virgin.net>
2014-04-27 12:56:01 +0100

    improve coding

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/TabStaff.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/TabStaff.scm
@@ -1,3 +1,6 @@
-(ToggleDirective "staff" "postfix" "TabStaff" " \\new TabStaff <<\n"  DENEMO_OVERRIDE_LILYPOND)
-(ToggleDirective "voice" "postfix" "TabStaff" "\n" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
-(ToggleDirective "clef" "postfix" "TabStaff" "{} \n"  (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+;;;TabStaff
+(let ((tag "TabStaff"))
+	(ToggleDirective "staff" "postfix" tag " \\new TabStaff <<\n"  DENEMO_OVERRIDE_LILYPOND)
+	(ToggleDirective "voice" "postfix" tag "\n" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+	(ToggleDirective "clef" "postfix" tag "{} \n"  (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND)))
+
