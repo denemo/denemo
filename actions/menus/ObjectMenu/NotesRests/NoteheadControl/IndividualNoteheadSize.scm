a7fc6b98c19a802fdef73c5a557995a13c4f5735
Richard Shann <richard.shann@virgin.net>
2014-05-06 20:29:46 +0100

    Allow individual noteheads to be made smaller or larger

---
+++ actions/menus/ObjectMenu/NotesRests/NoteheadControl/IndividualNoteheadSize.scm
@@ -0,0 +1,15 @@
+;;;IndividualNoteheadSize
+(let ((tag "NoteHeadSize") (current #f)(size #f)(data #f))
+    (if (d-Directive-note? tag)
+        (begin
+            (set! data (d-DirectiveGet-note-data tag))))
+    
+    (if data
+        (set! current data)
+        (set! current "-0.5"))
+    (set! size (d-GetUserInput (_ "Notehead Font Magnification") (_  "Give size required: ") current))
+    (if size
+        (begin
+            (d-DirectivePut-note-prefix tag (string-append " \\tweak  font-size #" size " "))
+            (d-DirectivePut-note-data tag size)
+            (d-SetSaved #f))))
