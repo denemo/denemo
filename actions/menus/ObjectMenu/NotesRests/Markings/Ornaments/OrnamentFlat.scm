2da996d70f3a7bfd69e1785855f0f9b623577e88
Richard Shann <richard.shann@virgin.net>
2014-05-08 12:44:42 +0100

    Allow wysiwyg on accidentals applied to ornaments

--- actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/OrnamentFlat.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/OrnamentFlat.scm
@@ -1,2 +1,19 @@
 ;;;OrnamentFlat
-(ChordAnnotation "OrnamentFlat" "^\\markup { \\tiny \\flat }" #f LG-Flat)
+(disp "called with " OrnamentFlat::params " as param\n\n")
+(let ((tag "OrnamentFlat")  (params OrnamentFlat::params)  (markup "^\\markup { \\tiny \\flat }" ))
+	(if (list?  params)
+		(let ((offsetx #f) (offsety #f)(padding #f))
+			(if (eq? (car (list-ref params 0)) 'offsetx)
+				(begin
+					(set! offsetx (cdr (list-ref params 0)))
+					(set! offsety (cdr (list-ref params 1)))
+					 (set! markup 
+			   		(string-append "-\\tweak #'X-offset #'" offsetx "  -\\tweak #'Y-offset #'" offsety " "  markup)))
+			   	(begin
+			   		(set! padding (cdr (list-ref params 0)))
+			   		 (set! markup 
+			   		(string-append "-\\tweak padding # "  padding " " markup))))
+			   (d-DirectiveDelete-chord tag)))	  
+	(ChordAnnotation "OrnamentFlat"  markup #f LG-Flat))
+
+	
--- actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/OrnamentSharp.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/OrnamentSharp.scm
@@ -1,2 +1,18 @@
 ;;;OrnamentSharp
-(ChordAnnotation "OrnamentSharp" "^\\markup { \\tiny \\sharp }" #f LG-Sharp)
+(let ((tag "OrnamentSharp")  (params OrnamentSharp::params)  (markup "^\\markup { \\tiny \\sharp }" ))
+	(if (list?  params)
+		(let ((offsetx #f) (offsety #f)(padding #f))
+			(if (eq? (car (list-ref params 0)) 'offsetx)
+				(begin
+					(set! offsetx (cdr (list-ref params 0)))
+					(set! offsety (cdr (list-ref params 1)))
+					 (set! markup 
+			   		(string-append "-\\tweak #'X-offset #'" offsetx "  -\\tweak #'Y-offset #'" offsety " "  markup)))
+			   	(begin
+			   		(set! padding (cdr (list-ref params 0)))
+			   		 (set! markup 
+			   		(string-append "-\\tweak padding # "  padding " " markup))))
+			   (d-DirectiveDelete-chord tag)))	  
+	(ChordAnnotation "OrnamentSharp"  markup #f LG-Sharp))
+
+	
