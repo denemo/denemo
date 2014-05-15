70d712794c7c39a534390b50e9dc656846ef4e46
Richard Shann <richard.shann@virgin.net>
2014-05-15 13:58:19 +0100

    Place ignore markers at start of bar

--- actions/menus/ObjectMenu/MeasureMenu/IgnoreMeasureDurationError.scm
+++ actions/menus/ObjectMenu/MeasureMenu/IgnoreMeasureDurationError.scm
@@ -1,7 +1,12 @@
 ;;;IgnoreMeasureDurationError
 (if IgnoreMeasureDurationError::params
-    (d-InfoDialog (_ "This object tells Denemo not to signal an error when the current measure is incomplete or overfull"))
+    (d-InfoDialog (_ "This Directive tells Denemo not to signal an error if there are too many notes or rests after this point (or too few) for a complete measure."))
     (let ((tag "!"))
+        (d-PushPosition)
+        (while (d-PrevObjectInMeasure))
         (if (d-Directive-standalone? tag)
             (d-DirectiveDelete-standalone tag)
-            (d-DirectivePut-standalone-minpixels tag 10))))
+            (d-DirectivePut-standalone-minpixels tag 10))
+        (d-PopPosition)
+        (d-SetSaved #f)
+        (d-RefreshDisplay)))
