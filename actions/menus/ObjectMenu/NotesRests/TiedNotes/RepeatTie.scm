9bdc2eeb01574b87c09c6d7621ca53b6684ac8c6
Richard Shann <richard.shann@virgin.net>
2014-05-17 19:55:50 +0100

    Get the repeat tie glyph working

--- actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
@@ -1,13 +1,11 @@
 ;;RepeatTie
 (let ((tag "RepeatTie"))
-	(if (Note?)
-	   (begin
-		(if (d-Directive-chord? tag)
-			(d-DirectiveDelete-chord tag)	
-			(begin
-				(d-DirectivePut-chord-postfix tag "\\repeatTie")
-				(d-DirectivePut-chord-graphic tag "\n⌣
-				Denemo
-				30")))
-		(d-RefreshDisplay)
-		(d-SetSaved #f))))
+    (if (Note?)
+       (begin
+        (if (d-Directive-chord? tag)
+            (d-DirectiveDelete-chord tag)   
+            (begin
+                (d-DirectivePut-chord-postfix tag "\\repeatTie")
+                (d-DirectivePut-chord-graphic tag "\n⌣\nDenemo\n30")))
+        (d-RefreshDisplay)
+        (d-SetSaved #f))))
