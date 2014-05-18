3c567e3e68ea47358551dad1a94b6978d8681a50
Richard Shann <richard.shann@virgin.net>
2014-05-18 10:16:09 +0100

    Repeat tie more friendly

--- actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
@@ -1,11 +1,15 @@
 ;;RepeatTie
 (let ((tag "RepeatTie"))
+   (if (Appending?)
+   	(d-MoveCursorLeft))
     (if (Note?)
        (begin
-        (if (d-Directive-chord? tag)
-            (d-DirectiveDelete-chord tag)   
-            (begin
-                (d-DirectivePut-chord-postfix tag "\\repeatTie")
-                (d-DirectivePut-chord-graphic tag "\n⌣\nDenemo\n30")))
-        (d-RefreshDisplay)
-        (d-SetSaved #f))))
+		(if (d-Directive-chord? tag)
+		    (d-DirectiveDelete-chord tag)   
+		    (begin
+		        (d-DirectivePut-chord-postfix tag "\\repeatTie")
+		        (d-DirectivePut-chord-graphic tag "\n⌣\nDenemo\n30")))
+		(d-RefreshDisplay)
+		(d-SetSaved #f))
+        (d-InfoDialog (_ "This command only applies to notes or chords."))))
+
