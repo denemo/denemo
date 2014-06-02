61057f1b47816253e487db549778537d06f21765
Richard Shann <richard.shann@virgin.net>
2014-06-02 12:41:56 +0100

    Improve graphic

--- actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/RepeatTie.scm
@@ -8,7 +8,8 @@
 		    (d-DirectiveDelete-chord tag)   
 		    (begin
 		        (d-DirectivePut-chord-postfix tag "\\repeatTie")
-		        (d-DirectivePut-chord-graphic tag "\n⌣\nDenemo\n30")))
+		        (d-DirectivePut-chord-gy tag -20)
+		        (d-DirectivePut-chord-graphic tag "\n⌣𝅘𝅥\nDenemo\n18")))
 		(d-RefreshDisplay)
 		(d-SetSaved #f))
         (d-InfoDialog (_ "This command only applies to notes or chords."))))
