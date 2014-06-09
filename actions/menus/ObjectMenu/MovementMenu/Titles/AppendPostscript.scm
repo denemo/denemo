4dff81415c6f1922f1814d577d01a47a7c447d29
Richard Shann <richard.shann@virgin.net>
2014-06-09 11:06:19 +0100

    Allow editing of graphics added to movements

--- actions/menus/ObjectMenu/MovementMenu/Titles/AppendPostscript.scm
+++ actions/menus/ObjectMenu/MovementMenu/Titles/AppendPostscript.scm
@@ -1,29 +1,61 @@
 ;;AppendPostscript
-(let ((tag "AppendPostscript")(filename #f)(scale #f)(space-above #f))
-    (if (d-Directive-movementcontrol? tag)
-        (let ((data (eval-string (d-DirectiveGet-movementcontrol-data tag))))
-            (set! filename  (list-ref data 0))
-            (set! scale (list-ref data 1))
-            (set! space-above (list-ref data 2))))
+(let ((tag "AppendPostscript")(filename #f)(width #f)(space-above #f)(space-left #f)(params AppendPostscript::params))
+    (define (scale val)
+       (number->string (/ (* 16 (string->number val)) (string->number (d-ScoreProperties "query=fontsize")))))
+    (if (list? params)
+        (begin
+                (set! filename  (list-ref params 0))
+                (set! width (list-ref params 1))
+                (set! space-above (list-ref params 2))
+                (set! space-left (list-ref params 3)))
+        (if (d-Directive-movementcontrol? tag)
+            (let ((data (eval-string (d-DirectiveGet-movementcontrol-data tag))))
+                (set! filename  (list-ref data 0))
+                (set! width (list-ref data 1))
+                (set! space-above (list-ref data 2))
+                (set! space-left (list-ref data 3)))))
     (if (not filename)
-        (begin
-            (set! filename "...../drawing.eps")
-            (set! scale "100")
-            (set! space-above "14")))
+        (let ((name (d-GetFilename))) (disp "Got " name " ok\n\n" DENEMO_HOME_DIR "\n\n")
+            (if name
+                (set! filename (string-append (d-PathFromFilename name) "//" "drawing.eps"))
+                (set! filename (string-append DENEMO_HOME_DIR "//" "drawing.eps")))
+            (set! width "100")
+            (set! space-above "14")
+            (set! space-left "0")))
             
-            
-    (set! filename (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give full .eps filename:") filename))
-    (set! scale (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give width required:")  scale))
-    (set! space-above (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space above required:") space-above))
-    (if (and filename scale space-above)
-        (begin
-                    (d-DirectivePut-movementcontrol-postfix tag
-                        (string-append "\\markup {\\vspace #" space-above " \\epsfile #X #" scale " #\"" filename "\"}"))
-                   ;;; (d-DirectivePut-movementcontrol-override tag DENEMO_OVERRIDE_AFFIX)
-                     (d-DirectivePut-movementcontrol-data tag (string-append "(list \"" filename "\" \"" scale "\" \"" space-above "\")")))
-                (begin
-                    (if (equal? (_ "y") (d-GetUserInput  (_ "Encapsulated Postscript File") (_ "Delete appended postscript?") (_ "n")))
+   (if (not (list? params))
+    (cond
+        ((equal? params "edit")
+            (begin
+                (d-EditGraphics filename)))
+        ((equal? params 'refresh))
+        (else
+                (set! filename (d-ChooseFile (_ "Encapsulated Postscript File") (d-PathFromFilename filename) (list "*.eps" "*.EPS")))
+                (if filename
                     (begin
-                        (d-DirectiveDelete-movementcontrol tag)
-                        (d-InfoDialog "Appended Postscript Deleted"))))))
+                        (set! width (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give width required:")  width))
+                        (set! space-above (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space above required:") space-above))
+                        (set! space-left (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space to the left required:") space-left))
+                        (if (RadioBoxMenu (cons (string-append (_ "Edit the file ") filename) #t) (cons (_ "Use the file unedited") #f))
+                            (begin
+                             (d-EditGraphics filename)
+                             (d-WarningDialog (_ "Wait for your vector graphics editor to start.
+It will open an SVG file of the same name, if available,
+but be sure to save as encapsulated postscript (eps).
+You will need to refresh the print view to see your changes.
+When saving your eps it is good to save as SVG file format as well, 
+as this will give better editing later.  
+Quit your graphics editor before quitting this dialog
+to return to work in Denemo.")))))))))
+
+    (if (and filename width space-above space-left)
+        (begin
+                (d-DirectivePut-movementcontrol-postfix tag
+                        (string-append "\\markup { \\hspace #" (scale space-left) " \\vspace #" (scale space-above) " \\with-url #'\"scheme:(d-AppendPostscript 'refresh)\" \\epsfile #X #" (scale width) " #\"" filename "\" }"))
+                (d-DirectivePut-movementcontrol-data tag (string-append "(list \"" (scheme-escape filename) "\" \"" width "\" \"" space-above "\" \"" space-left "\")")))
+        (begin
+                    (if (equal? (_ "y") (d-GetUserInput  (_ "Encapsulated Postscript File") (_ "Delete prepended postscript?") (_ "n")))
+                        (begin
+                            (d-DirectiveDelete-movementcontrol tag)
+                            (d-InfoDialog (_ "Prepended Postscript Deleted")))))))
 (d-SetSaved #f)
--- actions/menus/ObjectMenu/MovementMenu/Titles/PrependPostscript.scm
+++ actions/menus/ObjectMenu/MovementMenu/Titles/PrependPostscript.scm
@@ -1,26 +1,57 @@
 ;;PrependPostscript
-(let ((tag "PrependPostscript")(filename #f)(scale #f)(space-below #f))
-    (if (d-Directive-movementcontrol? tag)
-        (let ((data (eval-string (d-DirectiveGet-movementcontrol-data tag))))
-            (set! filename  (list-ref data 0))
-            (set! scale (list-ref data 1))
-            (set! space-below (list-ref data 2))))
+(let ((tag "PrependPostscript")(filename #f)(width #f)(space-below #f)(space-left #f)(params PrependPostscript::params))
+    (if (list? params)
+        (begin
+                (set! filename  (list-ref params 0))
+                (set! width (list-ref params 1))
+                (set! space-below (list-ref params 2))
+                (set! space-left (list-ref params 3)))
+        (if (d-Directive-movementcontrol? tag)
+            (let ((data (eval-string (d-DirectiveGet-movementcontrol-data tag))))
+                (set! filename  (list-ref data 0))
+                (set! width (list-ref data 1))
+                (set! space-below (list-ref data 2))
+                (set! space-left (list-ref data 3)))))
     (if (not filename)
-        (begin
-            (set! filename "...../drawing.eps")
-            (set! scale "100")
-            (set! space-below "14")))
+        (let ((name (d-GetFilename)))
+            (if name
+                (set! filename (string-append (d-PathFromFilename name) "//" "drawing.eps"))
+                (set! filename (string-append DENEMO_HOME_DIR "//" "drawing.eps")))
+            (set! width "100")
+            (set! space-below "14")
+            (set! space-left "0")))
             
-            
-    (set! filename (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give full .eps filename:") filename))
-    (set! scale (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give width required:")  scale))
-    (set! space-below (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space below required:") space-below))
-    (if (and filename scale space-below)
+   (if (not (list? params))
+    (cond
+        ((equal? params "edit")
+            (begin
+                (d-EditGraphics filename)))
+        ((equal? params 'refresh))
+        (else
+                (set! filename (d-ChooseFile (_ "Encapsulated Postscript File") (d-PathFromFilename filename) (list "*.eps" "*.EPS")))
+                (if filename
+                    (begin
+                        (set! width (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give width required:")  width))
+                        (set! space-below (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space below required:") space-below))
+                        (set! space-left (d-GetUserInput (_ "Encapsulated Postscript File") (_ "Give space to the left required:") space-left))
+                        (if (RadioBoxMenu (cons (string-append (_ "Edit the file ") filename) #t) (cons (_ "Use the file unedited") #f))
+                            (begin
+                            (d-EditGraphics filename)
+                             (d-WarningDialog (_ "Wait for your vector graphics editor to start.
+It will open an SVG file of the same name, if available,
+but be sure to save as encapsulated postscript (eps).
+You will need to refresh the print view to see your changes.
+When saving your eps it is good to save as SVG file format as well, 
+as this will give better editing later.  
+Quit your graphics editor before quitting this dialog
+to return to work in Denemo.")))))))))
+
+    (if (and filename width space-below space-left)
         (begin
-                    (d-DirectivePut-movementcontrol-prefix tag
-                        (string-append "\\markup {\\epsfile #X #" scale " #\"" filename "\" \\vspace #" space-below " }"))
-                     (d-DirectivePut-movementcontrol-data tag (string-append "(list \"" filename "\" \"" scale "\" \"" space-below "\")")))
-                (begin
+                (d-DirectivePut-movementcontrol-prefix tag
+                        (string-append "\\markup {\\hspace #" space-left " \\with-url #'\"scheme:(d-AppendPostscript 'refresh)\" \\epsfile #X #" width " #\"" filename "\" \\vspace #" space-below " }"))
+                (d-DirectivePut-movementcontrol-data tag (string-append "(list \"" (scheme-escape filename) "\" \"" width "\" \"" space-below "\" \"" space-left "\")")))
+        (begin
                     (if (equal? (_ "y") (d-GetUserInput  (_ "Encapsulated Postscript File") (_ "Delete prepended postscript?") (_ "n")))
                     (begin
                         (d-DirectiveDelete-movementcontrol tag)
