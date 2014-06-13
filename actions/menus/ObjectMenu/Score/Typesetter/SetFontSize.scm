75513c171dafb19504ba3f0fa32bb603faa307de
Richard Shann <richard.shann@virgin.net>
2014-06-13 09:57:10 +0100

    Refresh title page size after score resize

--- actions/menus/ObjectMenu/Score/Typesetter/SetFontSize.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/SetFontSize.scm
@@ -4,5 +4,6 @@
 		(set! newsize  (d-GetUserInput "Overall Score Sizing"  "Give font size to use" size)))
 (if (and newsize (not (equal? size newsize)))					       
 	(begin
-		(d-ScoreProperties (string-append "fontsize="   newsize))		
+		(d-ScoreProperties (string-append "fontsize="   newsize))	
+		(d-RefreshDynamicDirectives)	
 		(d-SetSaved #f))))
