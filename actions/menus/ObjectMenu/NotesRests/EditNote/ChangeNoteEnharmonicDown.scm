6720f0bd07c3f6a06e4dfdc88171059de3689dfc
Richard Shann <richard.shann@virgin.net>
2014-06-26 11:02:14 +0100

    Allow cursor off note when changing enharmonically single notes

--- actions/menus/ObjectMenu/NotesRests/EditNote/ChangeNoteEnharmonicDown.scm
+++ actions/menus/ObjectMenu/NotesRests/EditNote/ChangeNoteEnharmonicDown.scm
@@ -1,16 +1,19 @@
 ;ChangeNoteEnharmonic Down
 ;; This script switches through enharmonic variants of the same in the direction of lower diatonic base notes
+
 (if (Note?)
-(let ((target (d-GetNoteAtCursor)))
-    (define old (ANS::GetChordNotes))
-    (define (proc ansNote thetarget)
-        (if (eq? thetarget ansNote)
-            (cond 
-                ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (- ansNote 40 ))) (- ansNote 40 )) ; special rule for irregular notes
-                ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (- ansNote 30 ))) (- ansNote 30)) ; all other notes 
-                (else ansNote))
-            ansNote)) ; else just stay on the same note
-    ;body
-    (if target
-        (set! target (ANS::Ly2Ans (string->symbol target))))
-    (ANS::ChangeChordNotes (map (lambda (x) (proc x target)) old))))
+(if (not (Chord?))
+	(d-ChangeEnharmonicDown)
+	(let ((target (d-GetNoteAtCursor)))
+	    (define old (ANS::GetChordNotes))
+	    (define (proc ansNote thetarget)
+		(if (eq? thetarget ansNote)
+		    (cond 
+		        ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (- ansNote 40 ))) (- ansNote 40 )) ; special rule for irregular notes
+		        ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (- ansNote 30 ))) (- ansNote 30)) ; all other notes 
+		        (else ansNote))
+		    ansNote)) ; else just stay on the same note
+	    ;body
+	    (if target
+		(set! target (ANS::Ly2Ans (string->symbol target))))
+	    (ANS::ChangeChordNotes (map (lambda (x) (proc x target)) old)))))
--- actions/menus/ObjectMenu/NotesRests/EditNote/ChangeNoteEnharmonicUp.scm
+++ actions/menus/ObjectMenu/NotesRests/EditNote/ChangeNoteEnharmonicUp.scm
@@ -1,16 +1,18 @@
 ;ChangeNoteEnharmonicUp
 ;; This script switches through enharmonic variants of the same in the direction of higher diatonic base notes
 (if (Note?)
-(let ((target (d-GetNoteAtCursor)))
-    (define old (ANS::GetChordNotes))
-    (define (proc ansNote thetarget)
-        (if (eq? thetarget ansNote)
-            (cond 
-                ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (+ ansNote 40 ))) (+ ansNote 40 )) ; special rule for irregular notes
-                ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (+ ansNote 30 ))) (+ ansNote 30)) ; all other notes 
-                (else ansNote))
-            ansNote)) ; else just stay on the same note
-    ;body
-    (if target
-        (set! target (ANS::Ly2Ans (string->symbol target))))
-    (ANS::ChangeChordNotes (map (lambda (x) (proc x target)) old))))
+(if (not (Chord?))
+	(d-ChangeEnharmonicUp)
+	(let ((target (d-GetNoteAtCursor)))
+	    (define old (ANS::GetChordNotes))
+	    (define (proc ansNote thetarget)
+		(if (eq? thetarget ansNote)
+		    (cond 
+		        ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (+ ansNote 40 ))) (+ ansNote 40 )) ; special rule for irregular notes
+		        ((= (ANS::GetHalfToneDistanceFromC ansNote) (ANS::GetHalfToneDistanceFromC (+ ansNote 30 ))) (+ ansNote 30)) ; all other notes 
+		        (else ansNote))
+		    ansNote)) ; else just stay on the same note
+	    ;body
+	    (if target
+		(set! target (ANS::Ly2Ans (string->symbol target))))
+	    (ANS::ChangeChordNotes (map (lambda (x) (proc x target)) old)))))
