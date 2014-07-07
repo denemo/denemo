e46174a12fdcae93fb99ed219aebbfde25174c0e
Richard Shann <richard.shann@virgin.net>
2014-07-07 18:59:49 +0100

    prevent display confusion on polyphonic rests

--- actions/menus/ObjectMenu/NotesRests/RestEntry/MoveRest.scm
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/MoveRest.scm
@@ -1,13 +1,20 @@
 ;;;MoveRest
 (if (d-Directive-chord? "PolyphonicRest")
-	(begin
-		(d-DirectiveDelete-chord "PolyphonicRest")
-		(d-StagedDelete)))
+    (begin
+        (d-DirectiveDelete-chord "PolyphonicRest")
+        (d-StagedDelete)))
 (if (Rest?)
-  (let ((which  (duration::lilypond->denemo (string->number (d-GetNoteDuration)))))
+  (let* ((duration (string->number (d-GetNoteDuration)))(which (duration::lilypond->denemo duration )))
     (d-AddNoteToChord)
+    (if (> duration 4)
+        (begin
+            (d-DirectivePut-standalone-minpixels "BeamingDisplayFix" 10)
+            (d-MoveCursorRight)
+            (d-DirectivePut-standalone-minpixels  "BeamingDisplayFix" 10)
+            (d-MoveCursorLeft)
+            (d-MoveCursorLeft)
+            (d-RefreshDisplay)))
     (d-DirectivePut-chord-postfix "PolyphonicRest" "\\rest")
     (d-DirectivePut-chord-graphic  "PolyphonicRest" (vector-ref Rests which))
-    (d-DirectivePut-chord-override "PolyphonicRest" (logior DENEMO_OVERRIDE_VOLUME DENEMO_OVERRIDE_GRAPHIC DENEMO_ALT_OVERRIDE))
-  ))
+    (d-DirectivePut-chord-override "PolyphonicRest" (logior DENEMO_OVERRIDE_VOLUME DENEMO_OVERRIDE_GRAPHIC DENEMO_ALT_OVERRIDE))))
 (d-SetSaved #f)
