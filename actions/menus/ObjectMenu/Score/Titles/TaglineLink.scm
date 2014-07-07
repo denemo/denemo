8a799684259d545b0e54e7a684d63f94c6e12e68
Richard Shann <richard.shann@virgin.net>
2014-07-07 10:14:46 +0100

    retain information for editing tagline with link

--- actions/menus/ObjectMenu/Score/Titles/TaglineLink.scm
+++ actions/menus/ObjectMenu/Score/Titles/TaglineLink.scm
@@ -1,16 +1,28 @@
 ;;;TaglineLink
- (let ((tag "TaglineLink"))
- (define link (d-GetUserInput (_ "Tagline with Link") (_ "Give URL to link to") "http://www.denemo.org"))
- (define text (d-GetUserInput (_ "Tagline with Link") (_ "Give text for tagline") (_ "Source file at http://denemo.org")))
- (if link
- 	(begin
-		 (if (not text) 
- 		(set! text "Link"))
- 		(if (string-null? text)
- 			(begin
- 				(d-DirectiveDelete-scoreheader tag))
- 			(begin
-				 (d-DirectivePut-scoreheader-override tag (logior DENEMO_OVERRIDE_TAGEDIT DENEMO_OVERRIDE_GRAPHIC))
-  				(d-DirectivePut-scoreheader-display tag (DenemoAbbreviatedString text))
-  				(d-DirectivePut-scoreheader-postfix tag (string-append "tagline = \\markup { \\with-url #\"" link "\" \"" text "\"}\n"))))		
-		(d-SetSaved #f))))
+ (let* ((tag "TaglineLink")(link (d-DirectiveGet-scoreheader-data tag))(text #f))
+    (if link
+        (begin
+            (set! link (eval-string link))
+            (set! text (car link))
+            (set! link (cdr link)))
+        (begin
+            (set! text (_ "Source file at http://denemo.org"))
+            (set! link "http://www.denemo.org")))
+            
+    (set! link (d-GetUserInput (_ "Tagline with Link") (_ "Give URL to link to") link))
+    (set! text (d-GetUserInput (_ "Tagline with Link") (_ "Give text for tagline") text))
+    (if link
+        (begin
+         (if (not text) 
+            (set! text "Link"))
+         (if (string-null? text)
+            (begin
+                (d-WarningDialog (_ "Tagline deleted"))
+                (d-DirectiveDelete-scoreheader tag))
+            (begin
+                (d-DirectivePut-scoreheader-override tag (logior DENEMO_OVERRIDE_TAGEDIT DENEMO_OVERRIDE_GRAPHIC))
+                (d-DirectivePut-scoreheader-display tag (DenemoAbbreviatedString text))
+                (d-DirectivePut-scoreheader-data tag (string-append "(cons \"" (scheme-escape text)
+                                                                            "\" \"" (scheme-escape link) "\")"))
+                (d-DirectivePut-scoreheader-postfix tag (string-append "tagline = \\markup { \\with-url #\"" link "\" \"" text "\"}\n"))))      
+        (d-SetSaved #f))))
