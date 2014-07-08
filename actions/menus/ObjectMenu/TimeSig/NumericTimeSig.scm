1a4ea93a2dda26017df65bf58a10068d727dce7b
Richard Shann <richard.shann@virgin.net>
2014-07-08 15:09:54 +0100

    Add numeric time sig command

---
+++ actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
@@ -0,0 +1,12 @@
+;;;NumericTimeSig
+(let ((tag "NumericTimeSig"))
+  (if (d-Directive-timesig? tag)
+	(d-DirectiveDelete-timesig tag)
+	(begin
+	  (d-DirectivePut-timesig-prefix tag "\\once \\override Staff.TimeSignature  #'style = #'numeric\n")
+	  (d-DirectivePut-timesig-graphic tag "\nN\nDenemo\n12")
+	  (d-DirectivePut-timesig-gy tag 0)
+	  (d-DirectivePut-timesig-minpixels tag 30)))
+  
+(d-SetSaved #f)
+(d-RefreshDisplay))
