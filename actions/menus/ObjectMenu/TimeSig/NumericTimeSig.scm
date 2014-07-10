27c0b7c81b1efd60ebd108fd770194f3e16dd600
Richard Shann <richard.shann@virgin.net>
2014-07-10 12:36:50 +0100

    Get syntax correct

--- actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
+++ actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
@@ -3,7 +3,7 @@
   (if (d-Directive-timesig? tag)
 	(d-DirectiveDelete-timesig tag)
 	(begin
-	  (d-DirectivePut-timesig-prefix tag "\\once \\override Staff.TimeSignature  #'style = #'numeric\n")
+	  (d-DirectivePut-timesig-prefix tag "\\numericTimeSignature\n")
 	  (d-DirectivePut-timesig-graphic tag "\nN\nDenemo\n12")
 	  (d-DirectivePut-timesig-gy tag 0)
 	  (d-DirectivePut-timesig-minpixels tag 30)))
