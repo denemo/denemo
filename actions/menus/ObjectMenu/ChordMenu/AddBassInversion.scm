30d0cddd00a302251c5af9b731a93244ecf17cdf
Richard Shann <richard.shann@virgin.net>
2014-07-10 13:48:18 +0100

    Add bass inversion command

---
+++ actions/menus/ObjectMenu/ChordMenu/AddBassInversion.scm
@@ -0,0 +1,20 @@
+;;;AddBassInversion
+(let ((tag "AddBassInversion")(note AddBassInversion::params))
+  (if (Note?)
+    (begin
+	(if (not note)
+		(begin
+			(set! note (d-DirectiveGet-note-data tag))
+			(if (not note)
+				(set! note "C"))
+			(set! note (d-GetUserInput (_ "Add Bass Note") (_ "Give bass note to add below root") note))))
+	(if (string? note)
+		(begin
+			(d-DirectivePut-note-data "tag" note)
+			(set! note (string-downcase note))
+			(d-DirectivePut-note-postfix "tag" (string-append "\\withMusicProperty bass ##t " note " "))
+			(d-DirectivePut-note-display "tag" (string-append "/" (string-upcase note)))
+			(d-DirectivePut-note-tx "tag" -7)
+			(d-DirectivePut-note-ty "tag" 30)
+			(d-SetSaved #f)
+			(d-RefreshDisplay))))))
