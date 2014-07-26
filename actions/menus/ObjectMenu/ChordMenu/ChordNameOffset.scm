117e7e3ac22d0aa03929b2ead91c4167db2d3898
Richard Shann <richard.shann@virgin.net>
2014-07-26 14:48:59 +0100

    Allow user control over chord symbol offset

--- actions/menus/ObjectMenu/ChordMenu/ChordNameOffset.scm
+++ actions/menus/ObjectMenu/ChordMenu/ChordNameOffset.scm
@@ -1,9 +1,17 @@
-;;;ChordNameOffset
-(let ((tag "ChordNameOffset"))
- (if (d-Directive-chord? tag)
- 	(d-DirectiveDelete-chord  tag)
- 	(begin
-		(d-DirectivePut-chord-prefix tag "\\tweak ChordName.extra-offset  #'(-5 . -2.0) ")
+;;ChordNameOffset
+(let* ((tag "ChordNameOffset") (x "-5")(y "-2")(data (d-DirectiveGet-chord-data tag)))
+    (if data
+        (begin
+           (set! data (eval-string data))
+            (set! x (car data))
+            (set! y (cdr data))))
+    (set! x (d-GetUserInput (_ "Chord Symbol Offset") (_ "Give horizontal displacement required") x))
+    (if x
+        (set! y (d-GetUserInput (_ "Chord Symbol Offset") (_ "Give vertical displacement required") y)))
+    (if x
+        (begin
+		(d-DirectivePut-chord-data tag (format #f "(cons ~s  ~s)" x y))
+		(d-DirectivePut-chord-prefix tag (string-append "\\tweak ChordName.extra-offset  #'(" x " . " y ") "))
 		(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
 		(d-DirectivePut-chord-display tag "<-->")))
 (d-SetSaved #f))
