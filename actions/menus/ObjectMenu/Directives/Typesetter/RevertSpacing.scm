0c61d94a3cf98f75a1fe786e707afe7837e85047
Richard Shann <richard.shann@virgin.net>
2014-07-28 16:04:09 +0100

    Clearer interface to note spacing commands

---
+++ actions/menus/ObjectMenu/Directives/Typesetter/RevertSpacing.scm
@@ -0,0 +1,10 @@
+;;;RevertSpacing
+(let ((tag "RevertSpacing"))
+    (if (d-Directive-standalone? tag)
+        (begin
+            (d-DirectiveDelete-standalone tag)
+            (d-NewSpacing #f))
+        (begin
+            (if (d-Directive-standalone? "NewSpacing")
+                (d-DirectiveDelete-standalone "NewSpacing")
+                (StandAloneDirectiveProto (cons tag "\\newSpacingSection\n\\revert Score.SpacingSpanner.spacing-increment\n")  #f "\n->\nDenemo\n48")))))
