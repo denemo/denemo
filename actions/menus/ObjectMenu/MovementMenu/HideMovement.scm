d5938b6d7dcadf52b497033c874c61f976b39d57
Richard Shann <richard.shann@virgin.net>
2014-08-01 18:26:24 +0100

    Create a working Hide Movement command

--- actions/menus/ObjectMenu/MovementMenu/HideMovement.scm
+++ actions/menus/ObjectMenu/MovementMenu/HideMovement.scm
@@ -1,6 +1,17 @@
 ;;;HideMovement
+(let ((tag "HideMovement"))
 ;delete obsolete directives
-(d-DirectiveDelete-movementcontrol "HideMovement")
-(d-DirectiveDelete-layout "HideMovement")
-(d-DirectiveDelete-header "HideMovement")
-(d-InfoDialog "Use the Score Layout view to create a typeset with movements omitted.\nUse the \"X\" button on the movements to be dropped.\nSee the View Menu, Score Layout")
+(d-DirectiveDelete-layout "HiddenMovement")
+(d-DirectiveDelete-header "HiddenMovement")
+(if HideMovement::params
+	    (d-DirectiveDelete-movementcontrol tag))
+(if (d-Directive-movementcontrol? tag)
+    (begin
+        (d-DirectiveDelete-movementcontrol tag)
+        (d-InfoDialog (_ "Movement will be typeset normally")))
+    (begin
+        (d-DirectivePut-movementcontrol-prefix tag "\\void ")
+        (d-DirectivePut-movementcontrol-display tag (_ "Non-printing Movement"))
+        (if (not HideMovement::params)
+       		 (d-InfoDialog (_ "This movement will not be typeset. You must have at least one movement that is typeset"))))))
+(d-SetSaved #f)
