6c6a5e025db25f75d74a5d136893af10b8594e53
Richard Shann <richard.shann@virgin.net>
2014-08-03 14:13:07 +0100

    Get display names correct for Grand Staff

--- actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GrandStaffEnd.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GrandStaffEnd.scm
@@ -1,1 +1,2 @@
-(AttachDirective "staff" "postfix" '("ContextGrandStaff" . "Grand Staff Start")  " >>\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)
+
+(AttachDirective "staff" "postfix" '("ContextGrandStaff" . "Grand Staff End")  " >>\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)
--- actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GrandStaffStart.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GrandStaffStart.scm
@@ -1,1 +1,1 @@
-(AttachDirective "staff" "prefix" '("ContextGrandStaff" . "Grand Staff End")  " \\new GrandStaff <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)
+(AttachDirective "staff" "prefix" '("ContextGrandStaff" . "Grand Staff Start")  " \\new GrandStaff <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)
