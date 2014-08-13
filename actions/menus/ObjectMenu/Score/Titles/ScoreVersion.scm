8950b885b78c7003922f35091c6f50009d145a11
Richard Shann <richard.shann@virgin.net>
2014-08-13 09:51:34 +0100

    Allow score version on every page

--- actions/menus/ObjectMenu/Score/Titles/ScoreVersion.scm
+++ actions/menus/ObjectMenu/Score/Titles/ScoreVersion.scm
@@ -1,19 +1,26 @@
 ;;;ScoreVersion
-(let ((tag "ScoreVersion")(current #f) (thematch #f))
-  (set! current (d-DirectiveGet-score-prefix tag ))
-  (if (boolean? current)
-      (set! current (_ "v 1.0"))
-      (begin
-	(set! thematch (string-match "\\\\markup \\\\teeny \"([^\"]*)\"" current))
-	(if (regexp-match? thematch)
-	    (set! current (match:substring thematch 1))
-	    (set! current (_ "v 1.0")))))
+(let* ((tag "ScoreVersion")(current (d-DirectiveGet-score-data tag )))
+  (if (not current)
+      (set! current (_ "v 1.0")))
   (set! current (d-GetUserInput (_ "Version") (_ "Give a version for this edition:") current))
   (if current
-  	(begin 
-  		(d-DirectivePut-score-display tag current)
-  		(d-DirectivePut-score-override tag  DENEMO_OVERRIDE_AFFIX)
-  		(d-DirectivePut-score-prefix tag  (string-append "\\markup \\teeny \"" current "\""))
-  		(d-SetSaved #f))
-  	(d-DirectiveDelete-score tag)))
-
+    (begin 
+        (d-DirectivePut-score-display tag current)
+        (d-DirectivePut-score-data tag current)
+        (case (RadioBoxMenu 
+            (cons (_ "Show on first page") 'first)
+            (cons (_ "Show on every page") 'all)
+            (cons (_ "Delete") 'delete))
+         ((first)
+            (d-DirectiveDelete-paper tag)
+            (d-DirectivePut-score-override tag  DENEMO_OVERRIDE_AFFIX)
+            (d-DirectivePut-score-prefix tag  (string-append "\\markup \\teeny \"" current "\"")))
+        ((all)
+            (d-DirectivePut-score-prefix tag "")
+            (d-DirectivePut-paper-postfix tag (string-append "\nevenHeaderMarkup = \\markup \\teeny {" current "}\n"   
+            "oddHeaderMarkup = \\markup \\teeny {" current "}\n")))
+        ((delete)
+            (d-DirectiveDelete-paper tag)
+            (d-DirectiveDelete-score tag)))         
+        (d-SetSaved #f))
+    (d-InfoDialog (_ "Cancelled"))))
