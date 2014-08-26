f9f208ebf9a2b3e8034d3813b98ea3ae93dba026
Richard Shann <richard.shann@virgin.net>
2014-08-26 12:54:11 +0100

    Add commands for switching number key shortcuts

---
+++ actions/menus/MainMenu/InputMenu/Keyboard/NumbersDefault.scm
@@ -0,0 +1,7 @@
+;;NumbersDefault
+(d-AddKeybinding "0" "0")
+(d-AddKeybinding "1" "1")
+(d-AddKeybinding "2" "2")
+(d-AddKeybinding "3" "3")
+(d-AddKeybinding "4" "4")
+(d-AddKeybinding "5" "5")
---
+++ actions/menus/MainMenu/InputMenu/Keyboard/NumbersSelectDuration.scm
@@ -0,0 +1,7 @@
+;;NumbersSelectDuration
+(d-AddKeybinding "Set0" "0")
+(d-AddKeybinding "Set1" "1")
+(d-AddKeybinding "Set2" "2")
+(d-AddKeybinding "Set3" "3")
+(d-AddKeybinding "Set4" "4")
+(d-AddKeybinding "Set5" "5")
