54ea4a0ac6aaaa8a9f847524dc3d67ddecbcb687
Richard Shann <richard.shann@virgin.net>
2014-09-02 16:34:20 +0100

    Display ossia start and stop nicely

--- actions/menus/ObjectMenu/StaffMenu/Ossia/HideOssia.scm
+++ actions/menus/ObjectMenu/StaffMenu/Ossia/HideOssia.scm
@@ -3,4 +3,6 @@
 (d-DirectivePut-standalone-postfix "StopStaff" "\\stopStaff ")
 (d-DirectivePut-standalone-display "StopStaff" (_ "Hide"))
 (d-DirectivePut-standalone-ty "StopStaff" 40)
-(d-RefreshDisplay)
+(d-DirectivePut-standalone-minpixels "StopStaff" 30)
+(d-MoveCursorRight)
+(d-RefreshDisplay)  
--- actions/menus/ObjectMenu/StaffMenu/Ossia/ShowOssia.scm
+++ actions/menus/ObjectMenu/StaffMenu/Ossia/ShowOssia.scm
@@ -3,4 +3,6 @@
 (d-DirectivePut-standalone-postfix "StartStaff" "\\startStaff ")
 (d-DirectivePut-standalone-display "StartStaff" (_ "Show"))
 (d-DirectivePut-standalone-ty "StartStaff" 40)
+(d-DirectivePut-standalone-minpixels "StartStaff" 30)
+(d-MoveCursorRight)
 (d-RefreshDisplay)
