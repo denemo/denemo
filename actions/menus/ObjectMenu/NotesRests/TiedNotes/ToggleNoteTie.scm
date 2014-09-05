ca639489c87971feb78b193894f13fa4bda7c047
Richard Shann <richard.shann@virgin.net>
2014-09-05 21:07:33 +0100

    Warn trying to attach individual ties to non-chord notes

--- actions/menus/ObjectMenu/NotesRests/TiedNotes/ToggleNoteTie.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/ToggleNoteTie.scm
@@ -1,12 +1,15 @@
 ;;ToggleNoteTie
 (let ((tag "Tie"))
-	(if (d-Directive-note? tag)
-		(d-DirectiveDelete-note tag)
-		(begin
-			(d-DirectivePut-note-postfix tag "~")
-			;;for some reason we do not have ⏝ in the font
-			(d-DirectivePut-note-graphic tag "\n~
-			Denemo
-			30")))
-	(d-RefreshDisplay)
-	(d-SetSaved #f))
+    (if (Chord?)
+        (begin
+            (if (d-Directive-note? tag)
+                (d-DirectiveDelete-note tag)
+                (begin
+                    (d-DirectivePut-note-postfix tag "~")
+                    ;;for some reason we do not have ⏝ in the font
+                    (d-DirectivePut-note-graphic tag "\n~
+                    Denemo
+                    30")))
+            (d-RefreshDisplay)
+            (d-SetSaved #f))
+        (d-WarningDialog (_ "Individual Ties only apply to notes within chords."))))
--- actions/menus/ObjectMenu/NotesRests/TiedNotes/ToggleNoteUpTie.scm
+++ actions/menus/ObjectMenu/NotesRests/TiedNotes/ToggleNoteUpTie.scm
@@ -1,13 +1,16 @@
 ;;ToggleNoteUpTie
 (let ((tag "Tie"))
-	(if (d-Directive-note? tag)
-		(d-DirectiveDelete-note tag)
-		(begin
-			(d-DirectivePut-note-postfix tag "^~ ")
-			(d-Chordize)
-			;;for some reason we do not have ⏝ in the font
-			(d-DirectivePut-note-graphic tag "\n~
-			Denemo
-			30")))
-	(d-RefreshDisplay)
-	(d-SetSaved #f))
+    (if (Chord?)
+        (begin
+            (if (d-Directive-note? tag)
+                (d-DirectiveDelete-note tag)
+                (begin
+                    (d-DirectivePut-note-postfix tag "^~ ")
+                    (d-Chordize)
+                    ;;for some reason we do not have ⏝ in the font
+                    (d-DirectivePut-note-graphic tag "\n~
+                    Denemo
+                    30")))
+            (d-RefreshDisplay)
+            (d-SetSaved #f))
+        (d-WarningDialog (_ "Individual Ties only apply to notes within chords."))))
