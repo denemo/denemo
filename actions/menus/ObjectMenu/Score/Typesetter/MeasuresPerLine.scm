63198cbf97f2171e6e97a516b366726c41a53f5f
Richard Shann <richard.shann@virgin.net>
2014-09-14 14:11:23 +0100

    Make Measures per line work on all movements, and provide a deletion mechanism for it

--- actions/menus/ObjectMenu/Score/Typesetter/MeasuresPerLine.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/MeasuresPerLine.scm
@@ -1,14 +1,23 @@
 ;;;MeasuresPerLine
 (let ((tag "MeasuresPerLine")(params  MeasuresPerLine::params)(value #f))
+    (if (d-Directive-score? tag)
+        (let ((choice (RadioBoxMenu (cons (_ "Revert to Optimal") 'delete) (cons (_ "Edit") 'edit))))
+            (case choice
+            ((delete)
+                (d-DirectiveDelete-score tag)
+                (d-SetSaved #f)
+                (set! params 'delete)))))
+    
     (if (and params (not (equal? params "edit")))
         (set! value params)
         (set! value (d-GetUserInput (_ "Measures per line") (_ "Give required measures per line: ") "4")))
-    (if (and value (string->number value))
+    (if (and (string? value) (string->number value))
         (begin
         
             (d-DirectivePut-score-display tag  (_ "Measures per line"))
-                        (d-DirectivePut-score-override tag (logior DENEMO_OVERRIDE_AFFIX))
-                        (d-DirectivePut-score-prefix tag "
+            (d-DirectivePut-score-override tag (logior DENEMO_OVERRIDE_AFFIX))
+            (d-DirectivePut-score-prefix tag (string-append "
+       
 #(define ((bars-per-line-engraver bar-list) context)
   (let* ((working-copy bar-list)
          (total (1+ (car working-copy))))
@@ -25,14 +34,12 @@
                          (set! working-copy bar-list)
                          (begin
                            (set! working-copy (cdr working-copy))))
-                           (set! total (+ total (car working-copy))))))))))))")     
-        
-            (d-DirectivePut-layout-display tag (_ "Measures per line"))
-            (d-DirectivePut-layout-postfix tag  (string-append "
-\\context {
+                           (set! total (+ total (car working-copy))))))))))))
+\\layout {
+   \\context {
       \\Score
       %use the line below to insist on your layout
       %\\override NonMusicalPaperColumn.line-break-permission = ##f
-      \\consists #(bars-per-line-engraver '( " value "))}\n"))
-      
+      \\consists #(bars-per-line-engraver '( " value "))}}                             
+                           "))
             (d-SetSaved #f))))
