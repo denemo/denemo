e5f435999b46078103b6f3b8feb0d7cf322fdf45
Richard Shann <richard.shann@virgin.net>
2014-10-08 14:44:21 +0100

    allow deletion, make sure lowest note is used, allow edit

--- actions/menus/ObjectMenu/ChordMenu/AddBassInversion.scm
+++ actions/menus/ObjectMenu/ChordMenu/AddBassInversion.scm
@@ -1,9 +1,12 @@
 ;;;AddBassInversion
 (let ((tag "AddBassInversion")(note AddBassInversion::params))
+(if (equal? note "edit")
+	(set! note #f))
   (if (Appending?)
     (d-MoveCursorLeft))
   (if (Note?)
     (begin
+    (d-CursorToNthNoteHeight 1) ;;attach to lowest note only
     (if (not note)
         (begin
             (set! note (d-DirectiveGet-note-data tag))
@@ -12,11 +15,14 @@
             (set! note (d-GetUserInput (_ "Add Bass Note") (_ "Give bass note to add below root\nUse \"es\" for flat, \"is\" for sharp") note))))
     (if (string? note)
                 (begin
-                    (d-DirectivePut-note-data tag note)
-                    (set! note (string-downcase note))
-                    (d-DirectivePut-note-postfix tag (string-append "\\withMusicProperty bass ##t " note " "))
-                    (d-DirectivePut-note-display tag (string-append "/" (string-upcase note)))
-                    (d-DirectivePut-note-tx tag -7)
-                    (d-DirectivePut-note-ty tag 30)
-                    (d-Chordize #t)
-                    (d-SetSaved #f))))))
+                	(if (string-null? note)
+                	 	(d-DirectiveDelete-note tag)
+		        	 (begin
+					    (d-DirectivePut-note-data tag note)
+					    (set! note (string-downcase note))
+					    (d-DirectivePut-note-postfix tag (string-append "\\withMusicProperty bass ##t " note " "))
+					    (d-DirectivePut-note-display tag (string-append "/" (string-upcase note)))
+					    (d-DirectivePut-note-tx tag -7)
+					    (d-DirectivePut-note-ty tag 30)
+					    (d-Chordize #t)))
+                    		(d-SetSaved #f))))))
