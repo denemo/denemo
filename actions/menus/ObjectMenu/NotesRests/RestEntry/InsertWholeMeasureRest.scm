dda32efc560b9abe46c4d242dcec09f58d1f7f1b
Richard Shann <richard.shann@virgin.net>
2014-10-10 11:53:45 +0100

    Do not change prevailing duration when inserting whole measure rests

--- actions/menus/ObjectMenu/NotesRests/RestEntry/InsertWholeMeasureRest.scm
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/InsertWholeMeasureRest.scm
@@ -1,23 +1,24 @@
 ;;;;InsertWholeMeasureRest
-(if (or (None?)  (zero? (d-GetDurationInTicks)))
-    (begin
-        (DenemoWholeMeasureRestCommand)
-        (d-MoveCursorRight)
-        (d-Set2))
-    (begin  
-        (if (d-MoveToMeasureRight)
-             (if (or (None?)  (zero? (d-GetDurationInTicks)))
-                (DenemoWholeMeasureRestCommand)
-                (begin
-                	(d-MoveToMeasureLeft)
-                	 (let loop () 
-                		(if (d-Directive-chord? DenemoWholeMeasureRestTag)
-                			(DenemoWholeMeasureRestCommand)
-                			(if (d-NextObjectInMeasure) 
-                				(loop))))))
-            (begin
-                (d-AppendMeasureAllStaffs)  
-                (d-MoveCursorRight)
-                (DenemoWholeMeasureRestCommand)
-                (d-Set2)
-                (d-MoveCursorRight)))))
+(let ((duration (d-GetPrevailingDuration)))
+	(if (or (None?)  (zero? (d-GetDurationInTicks)))
+	    (begin
+		(DenemoWholeMeasureRestCommand)
+		(d-MoveCursorRight)
+		(d-GetPrevailingDuration duration))
+	    (begin  
+		(if (d-MoveToMeasureRight)
+		     (if (or (None?)  (zero? (d-GetDurationInTicks)))
+		        (DenemoWholeMeasureRestCommand)
+		        (begin
+		        	(d-MoveToMeasureLeft)
+		        	 (let loop () 
+		        		(if (d-Directive-chord? DenemoWholeMeasureRestTag)
+		        			(DenemoWholeMeasureRestCommand)
+		        			(if (d-NextObjectInMeasure) 
+		        				(loop))))))
+		    (begin
+		        (d-AppendMeasureAllStaffs)  
+		        (d-MoveCursorRight)
+		        (DenemoWholeMeasureRestCommand)
+		        (d-GetPrevailingDuration duration)
+		        (d-MoveCursorRight))))))
