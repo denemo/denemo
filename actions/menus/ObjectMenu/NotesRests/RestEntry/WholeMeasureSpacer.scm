69ac90d2f0e634af9282b6e7e23e7434819710d6
Richard Shann <richard.shann@virgin.net>
2014-10-10 10:50:37 +0100

    Add whole measure spacer command

---
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/WholeMeasureSpacer.scm
@@ -0,0 +1,9 @@
+;;;WholeMeasureSpacer
+(while (not (Appending?))
+	(d-MoveCursorRight))
+(d-InsertWholeMeasureRest)
+d-MoveCursorLeft)
+(d-SetNonprinting)
+(d-WholeMeasureRest "edit")
+(d-MoveCursorRight)
+(d-NoBarline)
