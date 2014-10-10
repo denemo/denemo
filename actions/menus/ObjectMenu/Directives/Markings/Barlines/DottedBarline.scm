c72c5350079fa3e918a7658e541c7693ce67e738
Richard Shann <richard.shann@virgin.net>
2014-10-10 15:02:39 +0100

    Add dotted barline

---
+++ actions/menus/ObjectMenu/Directives/Markings/Barlines/DottedBarline.scm
@@ -0,0 +1,7 @@
+;;;DottedBarline
+(d-Directive-standalone "DottedBarline")
+(d-DirectivePut-standalone-postfix "DottedBarline" "\\bar \"!\" ")
+(d-DirectivePut-standalone-display "DottedBarline" "\\bar \"!\" ")
+(d-DirectivePut-standalone-minpixels "DottedBarline" 30)
+(d-SetSaved #f)(d-RefreshDisplay)
+;;;End of scheme script
