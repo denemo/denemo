1b91aa599bb93489f6051473deb25809058c4313
Richard Shann <richard.shann@virgin.net>
2014-10-26 17:33:43 +0000

    Allow ColorChordSymbol to take a color as parameter

--- actions/menus/ObjectMenu/Directives/Typesetter/ChordSymbols/ColorChordSymbol.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/ChordSymbols/ColorChordSymbol.scm
@@ -1,15 +1,16 @@
 ;;;;;;ColorChordSymbol
-(let ((tag "ColorChordSymbol")(params ColorChordSymbol::params))
+(let ((tag "ColorChordSymbol")(params ColorChordSymbol::params)(color #f))
     (if (equal? params "edit")
         (set! params #f))
-    
-    (let ((color (d-SelectColor (_ "Color for Next Chord"))))
-        (if color
-            (begin
-                (d-Directive-standalone tag)
-                (d-DirectivePut-standalone-postfix tag (format #f "\\once \\override ChordName.color = #(rgb-color ~s ~s ~s) " (list-ref color 0)  (list-ref color 1) (list-ref color 2)))
-                (d-DirectivePut-standalone-data tag (format #f "'~s" color))
-                (d-DirectivePut-standalone-display tag (_ "Color"))
-                (d-DirectivePut-standalone-minpixels tag 50)
-                (d-SetSaved #f)
-                (d-RefreshDisplay)))))
+    (set! color params)
+    (if (not color)
+        (set! color (d-SelectColor (_ "Color for Next Chord"))))
+    (if color
+        (begin
+            (d-Directive-standalone tag)
+            (d-DirectivePut-standalone-postfix tag (format #f "\\once \\override ChordName.color = #(rgb-color ~s ~s ~s) " (list-ref color 0)  (list-ref color 1) (list-ref color 2)))
+            (d-DirectivePut-standalone-data tag (format #f "'~s" color))
+            (d-DirectivePut-standalone-display tag (_ "Color"))
+            (d-DirectivePut-standalone-minpixels tag 50)
+            (d-SetSaved #f)
+            (d-RefreshDisplay))))
