c580935e579cae085cca3de59daca366b0640e7b
Richard Shann <richard.shann@virgin.net>
2014-10-27 12:51:14 +0000

    Add FretDiagram command

--- actions/menus/ObjectMenu/Directives/Markings/InsertGraphic.scm
+++ actions/menus/ObjectMenu/Directives/Markings/InsertGraphic.scm
@@ -1,10 +1,11 @@
 ;;;InsertGraphic
-(let ((tag "InsertGraphic")(params InsertGraphic::params)(file #f)(data #f)(x-offset "0")(y-offset "0")(scale "1"))
+(let ((tag "InsertGraphic")(params InsertGraphic::params)(file "")(data #f)(x-offset "0")(y-offset "0")(scale "1"))
     (if (equal? params "edit")
         (set! params #f))
     (set! data (d-DirectiveGet-standalone-data tag))
     (if data
         (begin
+            (set! data (eval-string data))
             (set! file (assq-ref data 'file))
             (set! scale (assq-ref data 'scale))
             (set! x-offset (assq-ref data 'x-offset))
@@ -16,12 +17,18 @@
             (set! scale (assq-ref params 'scale))
             (set! x-offset (assq-ref params 'x-offset))
             (set! y-offset (assq-ref params 'y-offset))))
-    (if (not file)
-        (set! file (d-GetUserInput "Graphic File" "Give File Name (eps)" "")))
+    (if (not params)
+        (set! file (d-GetUserInput "Graphic File" "Give File Name (eps)" file)))
     (if (not scale)
         (set! scale "1"))
     (if file
         (begin
+            (set! data (assq-set! data 'file file))
+            (set! data (assq-set! data 'scale scale))
+            (if x-offset
+                (set! data (assq-set! data 'x-offset x-offset)))
+            (if y-offset
+                (set! data (assq-set! data 'y-offset y-offset)))
             (d-Directive-standalone tag)
             (d-DirectivePut-standalone-data tag (format #f "'~s" data))
             (if (and x-offset y-offset)
