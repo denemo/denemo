40a6baf0772fc7e778521aea2a3a06f10291740b
Richard Shann <richard.shann@virgin.net>
2014-10-28 16:56:43 +0000

    Allow setting by parameters

--- actions/menus/ObjectMenu/ChordMenu/ChordNameOffset.scm
+++ actions/menus/ObjectMenu/ChordMenu/ChordNameOffset.scm
@@ -1,12 +1,20 @@
 ;;ChordNameOffset
-(let* ((tag "ChordNameOffset") (x "2")(y "0")(data (d-DirectiveGet-chord-data tag)))
+(let* ((tag "ChordNameOffset") (params ChordNameOffset::params)(x "2")(y "0")(data (d-DirectiveGet-chord-data tag)))
     (if data
         (begin
            (set! data (eval-string data))
             (set! x (car data))
             (set! y (cdr data))))
-    (set! x (d-GetUserInput (_ "Chord Symbol Offset") (_ "Give horizontal displacement required") x))
-    (if (and  x (string->number x))
+      (if (equal? params "edit")
+      	(set! params #f))
+     (if (pair? params)
+     	(begin
+     		(set! x (number->string (car params)))
+      		(set! y (number->string (cdr params)))
+      		(set! params 'finished)))  
+     (if (not params)  		
+    	(set! x (d-GetUserInput (_ "Chord Symbol Offset") (_ "Give horizontal displacement required") x)))
+    (if (and  (not params) x (string->number x))
         (set! y (d-GetUserInput (_ "Chord Symbol Offset") (_ "Give vertical displacement required") y)))
     (if (and  y (string->number y))
         (begin
