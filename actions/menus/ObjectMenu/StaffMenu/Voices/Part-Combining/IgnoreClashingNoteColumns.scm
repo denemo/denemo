da9f307270fc665359dc4c5d7fea9cee4fb3d183
Richard Shann <richard.shann@virgin.net>
2014-11-18 12:31:56 +0000

    Add ignore clashing note column command

---
+++ actions/menus/ObjectMenu/StaffMenu/Voices/Part-Combining/IgnoreClashingNoteColumns.scm
@@ -0,0 +1,11 @@
+;;;IgnoreClashingNoteColumns
+(let ((tag "IgnoreClashingNoteColumns"))
+	(if (d-Directive-score? tag)
+		(begin
+			(d-DirectiveDelete-score tag)
+			(d-InfoDialog (_ "Clashing Note Columns will generate a warning from the LilyPond typesetter")))
+		(begin
+			(d-DirectivePut-score-prefix tag "\\layout {\\override NoteColumn #'ignore-collision = ##t }\n")
+			(d-DirectivePut-score-display tag (_ "Ignore Clashing Columns"))
+			(d-DirectivePut-score-override tag 4)))
+	(d-SetSaved #f))
