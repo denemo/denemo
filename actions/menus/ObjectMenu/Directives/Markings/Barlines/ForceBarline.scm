c926bd47308ccc2834ceb7913c43087c5afc5865
Richard Shann <richard.shann@virgin.net>
2014-11-20 14:48:05 +0000

    Warn of misunderstanding of insert barline

--- actions/menus/ObjectMenu/Directives/Markings/Barlines/ForceBarline.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Barlines/ForceBarline.scm
@@ -1,9 +1,21 @@
-;;;ForceBarline
-(d-DirectivePut-standalone-minpixels "ForceBarline" 12)
-(d-MoveCursorLeft)
-(d-DirectivePut-standalone-postfix "ForceBarline" " \\bar \"|\"")
-(d-DirectivePut-standalone-gx  "ForceBarline"  13)
-(d-DirectivePut-standalone-gy  "ForceBarline"  0)
-(d-DirectivePut-standalone-graphic "ForceBarline" "Barline")
-(d-MoveCursorRight)
-(d-RefreshDisplay)
+;;;ForceBarline 
+(define-once ForceBarline::warned  (_ "Warning: Do not use this command to create an upbeat (or pickup) bar.\nIt does not affect which notes appear in which measures.\nIt simply draws a barline at the point in question, if no other barline will be drawn.\nUse the Measures->Anacrusis (Upbeat) command for a short measure."))
+(disp "have " ForceBarline::params "\n\n")
+
+(let ((tag "ForceBarline"))
+   (if (or (d-Directive-standalone? tag) (equal? ForceBarline::params "edit"))
+      (d-InfoDialog (_ "This object instructs the LilyPond typesetter to draw a barline here.\nThe timing is not altered, use a hidden time signature change or the short measure or anacrusis command to do that."))
+     (begin
+         (if ForceBarline::warned
+            (begin
+                (d-WarningDialog ForceBarline::warned)
+                (set! ForceBarline::warned #f)))
+        (d-DirectivePut-standalone-minpixels tag 20)
+        (d-MoveCursorLeft)
+        (d-DirectivePut-standalone-postfix tag " \\bar \"|\"")
+        (d-DirectivePut-standalone-gx  tag  13)
+        (d-DirectivePut-standalone-gy  tag  0)
+        (d-DirectivePut-standalone-graphic tag "Barline")
+        (d-MoveCursorRight)
+        (d-RefreshDisplay)
+        (d-SetSaved #f))))
