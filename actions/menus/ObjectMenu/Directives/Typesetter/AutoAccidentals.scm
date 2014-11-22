f3b7be3f762f0ba5b65c4c6b75af13bad1bc8187
Richard Shann <richard.shann@virgin.net>
2014-11-22 16:58:23 +0000

    Add further accidental styles

--- actions/menus/ObjectMenu/Directives/Typesetter/AutoAccidentals.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/AutoAccidentals.scm
@@ -1,4 +1,17 @@
-;;AutomaticAccidentals
-             (StandAloneDirectiveProto (cons "AutomaticAccidentals" "
-        	#(set-accidental-style 'modern 'Score)        	
-"))
+;;AutoAccidentals
+(let ((tag "AutoAccidentals"))
+	(if AutoAccidentals::params 
+		(d-InfoDialog (_ "This directive sets the style of typesetting for accidentals"))
+		(let ((choice (RadioBoxMenu
+				(cons (_ "Default") "default")
+				(cons (_ "Modern") "modern")
+				(cons (_ "Modern Cautionary") "modern-cautionary")
+				(cons (_ "Neo-Modern") "neo-modern")	
+				(cons (_ "Teaching") "teaching")	
+				(cons (_ "Forget") "forget"))))
+				(if choice
+					(begin
+						(d-SetSaved #f)
+        					(StandAloneDirectiveProto (cons tag (string-append "
+        	#(set-accidental-style '" choice " 'Score)        	
+"))     #t #f choice    20          ))))))
