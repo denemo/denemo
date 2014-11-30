eb02534235fd365d82d51055548359bdaf8aece5
Richard Shann <richard.shann@virgin.net>
2014-11-30 10:17:46 +0000

    Reset movement indicator when changing hidden status

--- actions/menus/ObjectMenu/MovementMenu/HideMovement.scm
+++ actions/menus/ObjectMenu/MovementMenu/HideMovement.scm
@@ -4,7 +4,7 @@
 (d-DirectiveDelete-layout "HiddenMovement")
 (d-DirectiveDelete-header "HiddenMovement")
 (if HideMovement::params
-	    (d-DirectiveDelete-movementcontrol tag))
+        (d-DirectiveDelete-movementcontrol tag))
 (if (d-Directive-movementcontrol? tag)
     (begin
         (d-DirectiveDelete-movementcontrol tag)
@@ -13,5 +13,13 @@
         (d-DirectivePut-movementcontrol-prefix tag "\\void ")
         (d-DirectivePut-movementcontrol-display tag (_ "Non-printing Movement"))
         (if (not HideMovement::params)
-       		 (d-InfoDialog (_ "This movement will not be typeset. You must have at least one movement that is typeset"))))))
+             (d-InfoDialog (_ "This movement will not be typeset. You must have at least one movement that is typeset"))))))
+;;; Reset the movement indicator so it shows red
+(if (= 1 (d-GetMovement))
+    (begin
+        (d-NextMovement)
+        (d-PreviousMovement))
+    (begin
+        (d-PreviousMovement)
+        (d-NextMovement)))
 (d-SetSaved #f)
