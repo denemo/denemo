7f22bb109e0de230ab9131329f7c39e0f8088873
Richard Shann <richard.shann@virgin.net>
2014-12-03 09:10:15 +0000

    Improve label

--- actions/menus/ObjectMenu/Score/Typesetter/HideEmptyStaffs.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/HideEmptyStaffs.scm
@@ -6,5 +6,5 @@
 	  (d-DirectivePut-layout-postfix tag 
 				     " \\context { \\Staff \\RemoveEmptyStaves } ")
           (d-DirectivePut-layout-override  tag DENEMO_OVERRIDE_GRAPHIC)
-	  (d-DirectivePut-layout-display  tag (_ "Hide Empty Staffs"))))
+	  (d-DirectivePut-layout-display  tag (_ "Empty Systems Hidden"))))
   (d-SetSaved #f))
