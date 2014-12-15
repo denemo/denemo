3064e752dca703bc03d113301392032f399924de
Richard Shann <richard.shann@virgin.net>
2014-12-15 20:33:38 +0000

    Create a delete all before cursor command

---
+++ actions/menus/ObjectMenu/StaffMenu/DeleteMeasuresBeforeCursor.scm
@@ -0,0 +1,14 @@
+;;;DeleteMeasuresBeforeCursor
+(let ((choice (RadioBoxMenu (cons (_ "Cancel") #f) (cons (_ "All Staffs - Delete all measures before cursor?") 'all) (cons (_ "This Staff/Voice only?") 'this))))
+                 (case choice
+                    ((all)
+                    	(d-SetSaved #f)
+                    	(d-EvenOutStaffLengths)
+                        (while (d-MoveToMeasureLeft)
+                            (d-DeleteMeasureAllStaffs)))
+                   ((this)
+                        (d-SetSaved #f)
+                        (while (d-MoveToMeasureLeft)
+                            (d-DeleteMeasure)))
+                    (else        
+                        (d-InfoDialog (_ "Cancelled")))))
