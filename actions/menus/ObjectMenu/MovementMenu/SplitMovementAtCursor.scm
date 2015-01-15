b696b717b33786c5daedd4c46e1fe427ebdbcf20
Richard Shann <richard.shann@virgin.net>
2015-01-15 08:43:39 +0000

    Add Split Movement command

---
+++ actions/menus/ObjectMenu/MovementMenu/SplitMovementAtCursor.scm
@@ -0,0 +1,15 @@
+;;;SplitMovementAtCursor
+(if (d-GetSaved)
+    (let ((timesig (d-GetPrevailingTimesigAsLilyPond))
+            (keysig (d-GetPrevailingKeysigAsLilyPond))) 
+        (while (d-MoveToStaffUp))
+        (d-SetMark)
+        (d-GoToEnd) ;;;extends selection
+        (while (d-StaffDown)) ;;;extends selection
+        (d-Cut)
+        (d-InsertMovementAfter)
+        (while (d-MoveToStaffUp))
+        (d-InitialTimeSig timesig)
+         (d-InitialKey keysig)
+          (d-Paste))
+     (d-WarningDialog (_ "Score is not saved")))
