749e8aef59a9e35adf017b669403a98fd6a2c33d
Richard Shann <richard.shann@virgin.net>
2015-01-25 18:01:31 +0000

    Allow leading tr to be omitted, better editing by right click

--- actions/menus/ObjectMenu/Directives/Markings/Spanning/StartTrillSpan.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/StartTrillSpan.scm
@@ -1,16 +1,23 @@
 ;;;StartTrillSpan
 (let ((tag "StartTrillSpan"))
  (if (d-Directive-standalone? tag)
-	(d-DirectiveDelete-standalone tag)
-	(begin
-		(if (d-MoveCursorLeft)
-			(if (d-Directive-standalone? tag)
-			  (d-DirectiveDelete-standalone tag)
-				(d-MoveCursorRight)))
-	(StandAloneDirectiveProto (cons "StartTrillSpan" "\\startTrillSpan") #f LG-Prall)
-	(d-DirectivePut-standalone-gx tag 10)
-	(d-DirectivePut-standalone-grob tag tag)
-	(d-MoveCursorRight)))
+    (let ((choice (RadioBoxMenu (cons (_ "Help") 'help)(cons (_ "No \"tr\" at start.") 'start) (cons (_ "Delete") 'delete))))  
+        (case choice
+            ((help)
+                (d-InfoDialog (_ "This marks the start of a trill extending over several notes. The leading \"tr\" can be omitted if desired.")))
+            ((start)
+                (d-DirectivePut-standalone-prefix tag "-\\tweak bound-details.left.text #'() "))
+            ((delete)
+                (d-DirectiveDelete-standalone tag))))
+    (begin
+        (if (d-MoveCursorLeft)
+            (if (d-Directive-standalone? tag)
+              (d-DirectiveDelete-standalone tag)
+                (d-MoveCursorRight)))
+    (StandAloneDirectiveProto (cons "StartTrillSpan" "\\startTrillSpan") #f LG-Prall)
+    (d-DirectivePut-standalone-gx tag 10)
+    (d-DirectivePut-standalone-grob tag tag)
+    (d-MoveCursorRight)))
 (d-RefreshDisplay)
 (d-SetSaved #f))
-		
+        
--- actions/menus/ObjectMenu/Directives/Markings/Spanning/StopTrillSpan.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/StopTrillSpan.scm
@@ -1,16 +1,16 @@
 ;;;StartTrillSpan
 (let ((tag "StopTrillSpan"))
  (if (d-Directive-standalone? tag)
-	(d-DirectiveDelete-standalone tag)
-	(begin
-		(if (d-MoveCursorLeft)
-			(if (d-Directive-standalone? tag)
-			  (d-DirectiveDelete-standalone tag)
-				(d-MoveCursorRight)))
-	(StandAloneDirectiveProto (cons "StopTrillSpan" "\\stopTrillSpan") #f LG-UpPrall)
-	(d-DirectivePut-standalone-gx tag 10)
-	(d-DirectivePut-standalone-grob tag tag)
-	(d-MoveCursorRight)))
+    (d-InfoDialog (_ "This marks the end of a trill spanning several notes"))
+    (begin
+        (if (d-MoveCursorLeft)
+            (if (d-Directive-standalone? tag)
+              (d-DirectiveDelete-standalone tag)
+                (d-MoveCursorRight)))
+    (StandAloneDirectiveProto (cons "StopTrillSpan" "\\stopTrillSpan") #f LG-UpPrall)
+    (d-DirectivePut-standalone-gx tag 10)
+    (d-DirectivePut-standalone-grob tag tag)
+    (d-MoveCursorRight)))
 (d-RefreshDisplay)
 (d-SetSaved #f))
-		
+        
