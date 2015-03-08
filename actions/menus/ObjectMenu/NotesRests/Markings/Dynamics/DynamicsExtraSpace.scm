856151c8cfec40c242162f3b96e9137df2e20123
Richard Shann <richard.shann@virgin.net>
2015-03-08 09:58:29 +0000

    Make DynamicsExtraSpace edit properly and document better

--- actions/menus/ObjectMenu/NotesRests/Markings/Dynamics/DynamicsExtraSpace.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/Dynamics/DynamicsExtraSpace.scm
@@ -1,6 +1,10 @@
 ;;DynamicsExtraSpace
-(let ((tag "DynamicExtraSpace") (amount #f))
-(set! amount (d-GetUserInput (_ "Dynamics Spacing") (_ "Give extra spacing required") "20"))
-(if amount
-	(StandAloneDirectiveProto (cons tag  (string-append "\\override DynamicText.extra-spacing-width = #'(-" amount " . " amount ") "))
-			 #t "\nS\nDenemo" )))
+(let ((tag "DynamicsExtraSpace") (amount #f))
+(set! amount (d-DirectiveGet-standalone-data tag))
+(set! amount (d-GetUserInput (_ "Dynamics Spacing") (_ "Give extra spacing required") (if amount amount "10")      ))
+(if (and (string? amount) (string->number amount))
+	(begin
+		(StandAloneDirectiveProto (cons tag  (string-append "\\override DynamicText.extra-spacing-width = #'(-" amount " . " amount ") "))
+			 #f "\nS\nDenemo" )
+		(d-DirectivePut-standalone-data tag amount)
+		(d-SetSaved #f))))
