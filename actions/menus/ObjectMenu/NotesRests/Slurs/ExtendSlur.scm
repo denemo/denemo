7baff5411edfdbb2f603f70b2423065933383490
Richard Shann <richard.shann@virgin.net>
2015-05-16 14:50:28 +0100

    Make extend slur more useful

--- actions/menus/ObjectMenu/NotesRests/Slurs/ExtendSlur.scm
+++ actions/menus/ObjectMenu/NotesRests/Slurs/ExtendSlur.scm
@@ -1,17 +1,27 @@
-  ;;;ExtendSlur
+;;;ExtendSlur
   (if (and (d-IsSlurEnd) (not (d-IsSlurStart)))
     (begin 
 	(d-ToggleEndSlur)
 	(d-NextChord)
-	(d-ToggleEndSlur));;that is, either put the end slur back or put it on the next chord
+	(d-ToggleEndSlur)
+	(if (not (d-IsSlurEnd))
+		(d-ToggleEndSlur)));;that is, either put the end slur back or put it on the next chord
    (begin
+     (if (d-IsSlurStart)
+     	(begin
+     		(d-NextChord)
+		(d-ToggleEndSlur))
    	(if (d-PrevChord)
    	 	(if (d-IsSlurEnd)
    		    (begin 
 			(d-ToggleEndSlur)
 			(d-NextChord)
 			(d-ToggleEndSlur))
-		  (d-NextChord)))))
+			(if (d-IsSlurStart)
+			     	(begin
+			     		(d-NextChord)
+					(d-ToggleEndSlur))
+		  		(d-NextChord)))))))
 		
 
 		
