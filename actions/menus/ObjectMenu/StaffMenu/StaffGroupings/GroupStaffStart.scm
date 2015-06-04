a8c8e1a4a86410a93120e346338c7ba0b20dcc49
Richard Shann <richard.shann@virgin.net>
2015-06-04 20:04:22 +0100

    Get syntax right

--- actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GroupStaffStart.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GroupStaffStart.scm
@@ -1,4 +1,4 @@
 (let ((tag "GroupStaffStart"))
     (if (d-Directive-staff? tag)
         (d-DirectiveDelete-staff tag)
-        (AttachDirective "staff" "prefix" `(,tag . "Group Staff Start") " \\new GroupStaff <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)))
+        (AttachDirective "staff" "prefix" `(,tag . "Group Staff Start") " \\new StaffGroup <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)))
