f87b733b47cdc520c2750da63b7b7e6b9504f073
Richard Shann <richard.shann@virgin.net>
2015-07-02 14:51:08 +0100

    Fix spurious text edit calls, and add the markup to markup at end command

--- actions/menus/ObjectMenu/MovementMenu/MarkupAtEnd.scm
+++ actions/menus/ObjectMenu/MovementMenu/MarkupAtEnd.scm
@@ -1,16 +1,20 @@
 ;;;MarkupAtEnd
-(let ((tag "MarkupAtEnd")(themarkup ""))
+(let* ((tag "MarkupAtEnd")(themarkup #f)(data (d-DirectiveGet-movementcontrol-data tag)))
         (d-DirectivePut-movementcontrol-override tag (logior
-DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_TAGEDIT))
+            DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_TAGEDIT))
         (d-DirectivePut-movementcontrol-display tag (_ "Markup At End"))
-        (disp "checking previous markup ...")
-        (if (d-Directive-movementcontrol? tag)
-                (set! themarkup (d-DirectiveGet-movementcontrol-postfix tag)))
+        (if data
+        (set! data (eval-string data))
+        (set! data '()))
+        (set! themarkup (assq-ref data 'text))
+        (if (not themarkup)
+            (set! themarkup ""))
         (set! themarkup (d-GetUserInputWithSnippets (_ "Markup At End") (_ "Edit markup:") themarkup))
         (if themarkup
-        	(begin
-        		(set! themarkup (car themarkup))
-       			 (disp themarkup)
-        		(d-DirectivePut-movementcontrol-postfix tag themarkup)
-        		(d-SetSaved #f))
-        	(d-InfoDialog "Cancelled")))
+            (begin
+                (set! data (assq-set! data 'text (car themarkup)))
+                (d-DirectivePut-movementcontrol-data tag (format #f "'~s" data))
+                (set! themarkup (cdr themarkup))
+                (d-DirectivePut-movementcontrol-postfix tag (string-append "\\markup " themarkup))
+                (d-SetSaved #f))
+            (d-InfoDialog "Cancelled")))
