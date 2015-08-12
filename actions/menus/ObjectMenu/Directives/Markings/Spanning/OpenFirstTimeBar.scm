ebaa2ea8b280e14cb8517cf7f28a81efbda5148f
Richard Shann <richard.shann@virgin.net>
2015-08-12 17:19:02 +0100

    Improve tooltips

--- actions/menus/ObjectMenu/Directives/Markings/Spanning/OpenFirstTimeBar.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/OpenFirstTimeBar.scm
@@ -1,18 +1,18 @@
 ;;;OpenFirstTimeBar
 (let ((tag "OpenFirstTimeBar"))
-	(if (d-Directive-standalone? tag)
-		(begin
-			(d-WarningDialog (_ "This starts a first time bar. To change the text use the Nth time bar command.")))
-		(begin
-			(d-Directive-standalone tag)
-			(d-DirectivePut-standalone-minpixels  tag 50)
-			(d-DirectivePut-standalone-postfix tag "
-			\\set Score.repeatCommands = #'((volta \"1\"))
-			")
-			(d-DirectivePut-standalone-gx  tag 37)
-			(d-DirectivePut-standalone-gy  tag -34)
-			(d-DirectivePut-standalone-graphic tag "FirstTimeBar")
-			(d-MoveCursorRight)
-			(d-RefreshDisplay)
-			(d-SetSaved #f))))
+    (if (d-Directive-standalone? tag)
+        (begin
+            (d-WarningDialog (_ "This starts a first time bar. To customize the text \"1.\" delete this object and use the Nth time bar command.")))
+        (begin
+            (d-Directive-standalone tag)
+            (d-DirectivePut-standalone-minpixels  tag 50)
+            (d-DirectivePut-standalone-postfix tag "
+            \\set Score.repeatCommands = #'((volta \"1\"))
+            ")
+            (d-DirectivePut-standalone-gx  tag 37)
+            (d-DirectivePut-standalone-gy  tag -34)
+            (d-DirectivePut-standalone-graphic tag "FirstTimeBar")
+            (d-MoveCursorRight)
+            (d-RefreshDisplay)
+            (d-SetSaved #f))))
  
--- actions/menus/ObjectMenu/Directives/Markings/Spanning/OpenSecondTimeBar.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/OpenSecondTimeBar.scm
@@ -1,19 +1,19 @@
 ;;;OpenSecondTimeBar
 (let ((tag "OpenSecondTimeBar"))
-	(if (d-Directive-standalone? tag)
-		(begin
-			(d-WarningDialog (_ "This starts a second time bar. To change the text \"2.\" use the Nth time bar command.")))
-		(begin
-			(d-Directive-standalone tag)
-			(d-DirectivePut-standalone-minpixels  tag 50)
-			(d-DirectivePut-standalone-postfix tag "
-			\\set Score.repeatCommands = #'((volta \"2\"))
-			")
-			(d-DirectivePut-standalone-gx  tag 37)
-			(d-DirectivePut-standalone-gy  tag -34)
-			(d-DirectivePut-standalone-graphic tag "SecondTimeBar")
-			(d-MoveCursorRight)
-			(d-RefreshDisplay)
-			(d-SetSaved #f))))
+    (if (d-Directive-standalone? tag)
+        (begin
+            (d-WarningDialog (_ "This starts a second time bar. To customize the text \"2.\" delete this object and use the Nth time bar command.")))
+        (begin
+            (d-Directive-standalone tag)
+            (d-DirectivePut-standalone-minpixels  tag 50)
+            (d-DirectivePut-standalone-postfix tag "
+            \\set Score.repeatCommands = #'((volta \"2\"))
+            ")
+            (d-DirectivePut-standalone-gx  tag 37)
+            (d-DirectivePut-standalone-gy  tag -34)
+            (d-DirectivePut-standalone-graphic tag "SecondTimeBar")
+            (d-MoveCursorRight)
+            (d-RefreshDisplay)
+            (d-SetSaved #f))))
  
  
