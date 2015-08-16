da704ecb397f53d66d71468a2450f752f1cc479f
Richard Shann <richard.shann@virgin.net>
2015-08-16 11:50:57 +0100

    split off the user markup stuff

--- src/scripting/scheme-callbacks.c
+++ src/scripting/scheme-callbacks.c
@@ -3372,157 +3372,6 @@ scheme_get_user_input (SCM label, SCM prompt, SCM init, SCM modal)
   return scm;
 }
 
-static void
-paste_snippet_lilypond (GtkWidget * button)
-{
-  DenemoProject *gui = Denemo.project;
-  GtkWidget *hbox = gtk_widget_get_parent (button);
-  GtkWidget *textbuffer = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textbuffer");
-  if (textbuffer)
-    {
-      RhythmPattern *r = (gui->currhythm) ? ((RhythmPattern *) gui->currhythm->data) : NULL;
-      if (r)
-        {
-          const gchar *clefname = get_prevailing_clef_as_lilypond (), *keysigname = get_prevailing_keysig_as_lilypond (), *timesigname = get_prevailing_timesig_as_lilypond ();
-          gchar *text = g_strdup_printf ("§\\raise #0.5 \\score{\n\\DenemoGlobalTranspose {{%s}{%s}{%s}%s}\\layout{indent=0.0}\n}§", clefname, keysigname, timesigname, r->lilypond?r->lilypond->str:"");
-          gtk_text_buffer_insert_at_cursor (GTK_TEXT_BUFFER (textbuffer), text, -1 /*gint len */ );
-          g_free (text);
-        }
-    }
-  else
-    {
-      g_warning ("Denemo program error, widget hierarchy changed???");
-    }
-  GtkWidget *textview = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textview");
-  gtk_widget_grab_focus (textview);
-}
-static void
-paste_current_lilypond_as_fakechord (GtkWidget * button)
-{
-  DenemoProject *gui = Denemo.project;
-  GtkWidget *hbox = gtk_widget_get_parent (button);
-  GtkWidget *textbuffer = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textbuffer");
-  if (textbuffer)
-    {
-      gchar *text = get_fakechord_as_markup ();
-      if(text)
-        {
-            gchar *insert = g_strdup_printf("§%s§", text);   
-            gtk_text_buffer_insert_at_cursor (GTK_TEXT_BUFFER (textbuffer), insert, -1 /*gint len */ );
-            g_free (text);
-            g_free (insert);
-        }
-    }
-  else
-    {
-      g_warning ("Denemo program error, widget hierarchy changed???");
-    }
-  GtkWidget *textview = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textview");
-  gtk_widget_grab_focus (textview);
-}
-static void
-paste_current_lilypond_as_fretdiagram (GtkWidget * button)
-{
-  DenemoProject *gui = Denemo.project;
-  GtkWidget *hbox = gtk_widget_get_parent (button);
-  GtkWidget *textbuffer = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textbuffer");
-  if (textbuffer)
-    {
-      gchar *text = get_fretdiagram_as_markup ();
-      if(text)
-        {
-            gchar *insert = g_strdup_printf("§%s§", text);   
-            gtk_text_buffer_insert_at_cursor (GTK_TEXT_BUFFER (textbuffer), insert, -1 /*gint len */ );
-            g_free (text);
-            g_free (insert);
-        }
-    }
-  else
-    {
-      g_warning ("Denemo program error, widget hierarchy changed???");
-    }
-  GtkWidget *textview = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textview");
-  gtk_widget_grab_focus (textview);
-}
-#define SECTION_UTF8_STRING "§"
-
-static void
-insert_markup (GtkWidget * button, gchar *text)
-{
-  DenemoProject *gui = Denemo.project;
-  GtkWidget *hbox = gtk_widget_get_parent (button);
-  GtkWidget *textbuffer = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textbuffer");
-  if (textbuffer)
-          gtk_text_buffer_insert_at_cursor (GTK_TEXT_BUFFER (textbuffer), text, -1);
-   else
-    {
-      g_warning ("Denemo program error, widget hierarchy changed???");
-    }      
- GtkWidget *textview = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textview");
- gtk_widget_grab_focus (textview);
-}
-
-static void
-insert_font_mag (GtkWidget * button)
-{
-  DenemoProject *gui = Denemo.project;
-  GtkWidget *hbox = gtk_widget_get_parent (button);
-  GtkWidget *textbuffer = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textbuffer");
-  gchar *text = string_dialog_entry (gui, _( "Font Magnification"), _("Give a relative font size +/- "), "-2");
-  if (text && *text && atoi(text))
-    {
-        gchar *out = g_strdup_printf ("§\\fontsize #%s §", text);
-      if (textbuffer)
-              gtk_text_buffer_insert_at_cursor (GTK_TEXT_BUFFER (textbuffer), out, -1);
-       else
-        {
-          g_warning ("Denemo program error, widget hierarchy changed???");
-        }      
-     GtkWidget *textview = (GtkWidget *) g_object_get_data (G_OBJECT (hbox), "textview");
-     gtk_widget_grab_focus (textview);
-     g_free (out);
-    }
- g_free (text);
-}
-
-
-
-//#define SECTION_UTF8_STRING "\302\247"
-static gchar *
-create_lilypond_from_text (gchar * orig)
-{
-  gchar *text = g_strdup (orig);
-  GString *ret = g_string_new ("\\line\\large{");
-  g_debug ("looking at %s\n", text);
-  gunichar section = g_utf8_get_char (SECTION_UTF8_STRING);
-  gunichar lf = g_utf8_get_char ("\n");
-  gchar *next, *this;
-  gboolean in_section = FALSE;
-  for(this = text;*this;this = g_utf8_next_char (this))
-    {
-        gunichar thechar = g_utf8_get_char (this);
-        if (thechar == section)
-            {
-                in_section = !in_section;
-            } else if ((!in_section) && (*this == '\n'))
-            {
-              g_string_append (ret, "}\\line\\large{");
-            } else {
-                g_string_append_printf (ret, "%c", *this);
-            }
-    }
-  g_string_append (ret, "}\n");
-  g_free (text);
-  if(in_section) 
-    {
-        g_warning ("Unbalanced § marks");
-        g_string_free (ret, TRUE);
-        return g_strdup ("%{error %}");
-    }
-  else
-    return g_string_free (ret, FALSE);
-}
-
 
 SCM
 scheme_get_user_input_with_snippets (SCM label, SCM prompt, SCM init, SCM modal)
@@ -3548,81 +3397,16 @@ scheme_get_user_input_with_snippets (SCM label, SCM prompt, SCM init, SCM modal)
     }
   else
     initial_value = strdup (" ");
-  GtkWidget *hbox = gtk_hbox_new (FALSE, 8);
-  GtkWidget *button = gtk_button_new_with_label (_("Paste Current Snippet"));
-  gtk_widget_set_tooltip_text (button, _("Pastes the music captured in the currently selected Snippet into the text at the cursor.\nThe music appears here in the LilyPond typesetter syntax between two markers (§).\nIt will print as typeset music embedded in the sentence you are writing.\nYou can edit the syntax, taking care to leave the markers in position. If you delete one marker be sure to delete the other.\n"));
+ GString *text = g_string_new(""), *lilypond=g_string_new("");   
+ g_print ("Calling with modal %d format=%d\n",  !scm_is_false (modal), (!scm_is_false (modal)) && (modal != SCM_UNDEFINED));
+    gboolean ok = get_user_markup (text, lilypond, title, instruction, initial_value, !scm_is_false (modal), (!scm_is_false (modal)) && (modal != SCM_UNDEFINED));
 
-  g_signal_connect (button, "clicked", G_CALLBACK (paste_snippet_lilypond), NULL);
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-  button = gtk_button_new_with_label (_("Next Snippet"));
-  gtk_widget_set_tooltip_text (button, _("Makes the next Snippet the one that can be pasted.\nTo see the music snippets you need to check View → Snippets\nThe one selected is in bold black."));
-  GtkAction *action = gtk_ui_manager_get_action (Denemo.ui_manager, "/ObjectMenu/NotesRests/SelectDuration/NextRhythm");
-  if (action)
-    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_action_activate), action);
-  else
-    gtk_widget_set_sensitive (button, FALSE);
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-  
-  
-  button = gtk_button_new_with_label (_("Paste Chord Symbol"));
-  gtk_widget_set_tooltip_text (button, _("Pastes the note or chord at the cursor as a Chord Symbol\n"
-    "The music appears here in the LilyPond typesetter syntax between two markers (§).\n"
-    "It will print as chord name/symbol in the sentence you are writing, transposed according to the global transposition set.\n"
-    "This is not just for chords, use this to reference a note name.\n"));
-
-  g_signal_connect (button, "clicked", G_CALLBACK (paste_current_lilypond_as_fakechord), NULL);
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-
-  button = gtk_button_new_with_label (_("Paste Fret Diagram"));
-  gtk_widget_set_tooltip_text (button, _("Pastes the chord at the cursor as a Fret Diagram\n"
-    "The music appears here in the LilyPond typesetter syntax between two markers (§).\n"
-    "It will print as fret diagram in the sentence you are writing, transposed according to the global transposition set.\n"));
-
-  g_signal_connect (button, "clicked", G_CALLBACK (paste_current_lilypond_as_fretdiagram), NULL);
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-  
-
-  button = gtk_button_new_with_label (_("Bold"));
-  gtk_widget_set_tooltip_text (button, _("Inserts markup to make the following text bold. Enclose the words to be bold in {}. \nNote that the section markers (§) must come in pairs"));
-  g_signal_connect (button, "clicked", G_CALLBACK (insert_markup), "§\\bold §");
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-  
-  button = gtk_button_new_with_label (_("Italic"));
-  gtk_widget_set_tooltip_text (button, _("Inserts markup to make the following text italic. Enclose the words to be bold in {}.\nNote that the section markers (§) must come in pairs"));
-  g_signal_connect (button, "clicked", G_CALLBACK (insert_markup), "§\\italic §");
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-
-  button = gtk_button_new_with_label (_("“"));
-  gtk_widget_set_tooltip_text (button, _("Inserts code for open quotes - leave a space after this code. Note that this is not the \" character which is used for grouping words and must be paired or LilyPond will not typeset the music."));
-  g_signal_connect (button, "clicked", G_CALLBACK (insert_markup), "§\\char ##x201C §");
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-
-  button = gtk_button_new_with_label (_("”"));
-  gtk_widget_set_tooltip_text (button, _("Inserts code for close quotes - leave a space after this code. Note that this is not the \" character which is used for grouping words and must be paired or LilyPond will not typeset the music."));
-  g_signal_connect (button, "clicked", G_CALLBACK (insert_markup), "§\\char ##x201D §");
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-
-  button = gtk_button_new_with_label (_("size"));
-  gtk_widget_set_tooltip_text (button, _("Inserts code to change the relative font size."));
-  g_signal_connect (button, "clicked", G_CALLBACK (insert_font_mag), NULL);
-  gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, TRUE, 0);
-
-  gchar *text;
-  if ((modal == SCM_UNDEFINED))
-            text = string_dialog_editor_with_widget_opt (Denemo.project, title, instruction, initial_value, hbox, TRUE);
-  else  
-   if (scm_is_false (modal))
-            text = string_dialog_editor_with_widget_opt (Denemo.project, title, instruction, initial_value, hbox, FALSE);
-  else
-    text = g_strdup (initial_value); //anything else, just format the passed in string
-  if (text)
+ if (ok)
     {
-      gchar *lilypond = create_lilypond_from_text (text);
-      scm = scm_cons (scm_from_locale_string (text), scm_from_locale_string (lilypond));
-      g_free (lilypond);
+      scm = scm_cons (scm_from_locale_string (text->str), scm_from_locale_string (lilypond->str));
     }
   else
-    scm = SCM_BOOL_F;
+    scm = SCM_BOOL_F; 
 
   if (title)
     free (title);
@@ -3630,8 +3414,8 @@ scheme_get_user_input_with_snippets (SCM label, SCM prompt, SCM init, SCM modal)
     free (instruction);
   if (initial_value)
     free (initial_value);
-  if (text)
-    g_free (text);
+  g_string_free (text, TRUE);
+  g_string_free (lilypond, TRUE);
 
   return scm;
 }
