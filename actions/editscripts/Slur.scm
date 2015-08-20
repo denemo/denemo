4a7e5a95a2a92d576fe2e5291e1bebf30585afb5
Richard Shann <richard.shann@virgin.net>
2015-08-20 14:49:04 +0100

    Allow Slur shape to self-edit

---
+++ actions/editscripts/Slur.scm
@@ -0,0 +1,1 @@
+(d-InfoDialog (_ "This Directive modifies the slur shape - you can further edit the slur shape in the Print View window, or delete this directive to return to the default shape") #f)
