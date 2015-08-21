bc45815f236a0867144dfba02c591d5cdccb36e1
Richard Shann <richard.shann@virgin.net>
2015-08-21 18:10:01 +0100

    Send the sustain on/off MIDI messages

--- actions/menus/ObjectMenu/Directives/Markings/Spanning/SustainOff.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/SustainOff.scm
@@ -2,16 +2,17 @@
 ;;;SustainOff
 (let ((tag "SustainOff"))
  (if (d-Directive-standalone? tag)
-	(d-DirectiveDelete-standalone tag)
-	(begin
-		(if (d-MoveCursorLeft)
-			(if (d-Directive-standalone? tag)
-			  (d-DirectiveDelete-standalone tag)
-				(d-MoveCursorRight)))
-	(StandAloneDirectiveProto (cons "SustainOff" "\\sustainOff") #f "\n!P\nDenemo\n24")
-	(d-DirectivePut-standalone-gy tag 44)
-	(d-DirectivePut-standalone-grob tag tag)
-	(d-MoveCursorRight)))
+    (d-DirectiveDelete-standalone tag)
+    (begin
+        (if (d-MoveCursorLeft)
+            (if (d-Directive-standalone? tag)
+              (d-DirectiveDelete-standalone tag)
+                (d-MoveCursorRight)))
+    (StandAloneDirectiveProto (cons "SustainOff" "\\sustainOff") #f "\n!P\nDenemo\n24")
+    (d-DirectivePut-standalone-midibytes tag "0xB$ 0x40 0x00")
+    (d-DirectivePut-standalone-gy tag 44)
+    (d-DirectivePut-standalone-grob tag tag)
+    (d-MoveCursorRight)))
 (d-RefreshDisplay)
 (d-SetSaved #f))
-		
+        
--- actions/menus/ObjectMenu/Directives/Markings/Spanning/SustainOn.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/SustainOn.scm
@@ -1,16 +1,17 @@
 ;;;SustainOn
 (let ((tag "SustainOn"))
  (if (d-Directive-standalone? tag)
-	(d-DirectiveDelete-standalone tag)
-	(begin
-		(if (d-MoveCursorLeft)
-			(if (d-Directive-standalone? tag)
-			  (d-DirectiveDelete-standalone tag)
-				(d-MoveCursorRight)))
-	(StandAloneDirectiveProto (cons "SustainOn" "\\sustainOn") #f "\nP\nDenemo\n24")
-	(d-DirectivePut-standalone-gy tag 44)
-	(d-DirectivePut-standalone-grob tag tag)
-	(d-MoveCursorRight)))
+    (d-DirectiveDelete-standalone tag)
+    (begin
+        (if (d-MoveCursorLeft)
+            (if (d-Directive-standalone? tag)
+              (d-DirectiveDelete-standalone tag)
+                (d-MoveCursorRight)))
+    (StandAloneDirectiveProto (cons "SustainOn" "\\sustainOn") #f "\nP\nDenemo\n24")
+    (d-DirectivePut-standalone-midibytes tag "0xB$ 0x40 0x7F")
+    (d-DirectivePut-standalone-gy tag 44)
+    (d-DirectivePut-standalone-grob tag tag)
+    (d-MoveCursorRight)))
 (d-RefreshDisplay)
 (d-SetSaved #f))
-		
+        
