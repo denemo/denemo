fa790b1c8585d60ab6e09c782df1cd40a09134ea
Richard Shann <richard.shann@virgin.net>
2015-08-27 12:15:48 +0100

    Simplify

--- actions/menus/ObjectMenu/NotesRests/RestEntry/MultiMeasureRestNumber.scm
+++ actions/menus/ObjectMenu/NotesRests/RestEntry/MultiMeasureRestNumber.scm
@@ -1,16 +1,16 @@
 ;;;MultiMeasureRestNumber
 (let ((tag "MultiMeasureRestNumber"))
- (if (d-Directive-standalone? tag)
-	(d-EditSimilar)
-	(begin
-		(if (d-MoveCursorLeft)
-			(if (d-Directive-standalone? tag)
-			  (d-EditSimilar)
-				(d-MoveCursorRight)))
-	(StandAloneDirectiveProto (cons tag "\\once \\override MultiMeasureRestNumber #'transparent = ##t ") #f "\nNx\nDenemo\n48")
-	(d-DirectivePut-standalone-gy tag -44)
-	(d-DirectivePut-standalone-grob tag tag)
-	(d-MoveCursorRight)))
-(d-RefreshDisplay)
-(d-SetSaved #f))
+	(if (d-MoveCursorLeft)
+		(if (not (d-Directive-standalone? tag))
+			(d-MoveCursorRight)))
+	 (if (d-Directive-standalone? tag)
+		(d-EditSimilar)
+		(begin
+			(StandAloneDirectiveProto (cons tag "\\once \\override MultiMeasureRestNumber #'transparent = ##t ") #f "\nNx\nDenemo\n48")
+			(d-DirectivePut-standalone-gy tag -44)
+			(d-DirectivePut-standalone-grob tag tag)
+			(d-RefreshDisplay)
+			(d-SetSaved #f)
+			(d-MoveCursorRight))))
+	
 		
