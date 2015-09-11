f73cf408df7a9f43ba2f4ac9fb4c99acaa4dab83
Richard Shann <richard.shann@virgin.net>
2015-09-11 13:55:42 +0100

    protect against double inclusion, get rid of unused struct

--- src/command/measure.h
+++ src/command/measure.h
@@ -4,16 +4,10 @@
  * for Denemo, a gtk+ frontent to GNU Lilypond
  * (c) 2000-2005 Matthew Hiller
  */
+#ifndef MEASURE_H
+#define MEASURE_H
 
 #include <denemo/denemo.h>
-
-struct fourints
-{
-  gint time1;
-  gint time2;
-  gint clef;
-  gint stem_directive;
-};
 
 measurenode *addmeasures (DenemoMovement * si, gint pos, guint nummeasures, gint all);
 
@@ -30,3 +24,5 @@ void forceaccidentals (DenemoObject * theobj);
 objnode *measure_first_obj_node (measurenode * mnode);
 
 objnode *measure_last_obj_node (measurenode * mnode);
+
+#endif
