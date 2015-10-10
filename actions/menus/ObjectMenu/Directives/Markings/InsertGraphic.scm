c5d6e01ff6bc75fd930aab892b22eaf99707005d
Richard Shann <richard.shann@virgin.net>
2015-10-10 14:05:08 +0100

    Update Insert Graphic to use file chooser and offer editing scale

--- actions/menus/ObjectMenu/Directives/Markings/InsertGraphic.scm
+++ actions/menus/ObjectMenu/Directives/Markings/InsertGraphic.scm
@@ -1,7 +1,6 @@
 ;;;InsertGraphic
 (let ((tag "InsertGraphic")(params InsertGraphic::params)(file "")(data #f)(x-offset "0")(y-offset "0")(scale "1"))
-    (if (equal? params "edit")
-        (set! params #f))
+
     (set! data (d-DirectiveGet-standalone-data tag))
     (if data
         (begin
@@ -10,15 +9,19 @@
             (set! scale (assq-ref data 'scale))
             (set! x-offset (assq-ref data 'x-offset))
             (set! y-offset (assq-ref data 'y-offset))))
-            
-    (if  params
+    (if (equal? params "edit")
         (begin
-            (set! file (assq-ref params 'file))
-            (set! scale (assq-ref params 'scale))
-            (set! x-offset (assq-ref params 'x-offset))
-            (set! y-offset (assq-ref params 'y-offset))))
-    (if (not params)
-        (set! file (d-GetUserInput "Graphic File" "Give File Name (eps)" file)))
+            (set! scale (d-GetUserInput (_ "Scaling Graphic") (_ "Give scale to be used: ") (if scale scale "1.0")))
+            (set! params 'finished))           
+            (if  params
+                (begin
+                    (set! file (assq-ref params 'file))
+                    (set! scale (assq-ref params 'scale))
+                    (set! x-offset (assq-ref params 'x-offset))
+                    (set! y-offset (assq-ref params 'y-offset)))))
+    
+    (if (and (not (eq? params 'finished)) (not params))
+        (set! file (d-ChooseFile (_ "Graphic File") DENEMO_GRAPHICS_DIR (list "*.eps" "*.EPS"))))
     (if (not scale)
         (set! scale "1"))
     (if file
