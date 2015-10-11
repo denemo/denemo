e1673bf63d3483370b0a4d99ccc5440266af15d1
Richard Shann <richard.shann@virgin.net>
2015-10-11 15:00:02 +0100

    fix boxed alphabetic

--- actions/menus/ObjectMenu/Score/Typesetter/ScoreRehearsalMarkFormat.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/ScoreRehearsalMarkFormat.scm
@@ -2,7 +2,7 @@
 (let ((tag "ScoreRehearsalMarkFormat")
         (choice (RadioBoxMenu (cons (_ "alphabetic") "#format-mark-alphabet")
                 (cons (_ "Numeric") "#format-mark-numbers")
-                (cons (_ "Boxed alphabetic") "")
+                (cons (_ "Boxed alphabetic") "#format-mark-box-alphabet")
                 (cons (_ "Boxed numeric") "#format-mark-box-numbers")
                 (cons (_ "Circled alphabetic") "#format-mark-circle-alphabet")
                 (cons (_ "Circled numeric") "#format-mark-circle-numbers"))))
