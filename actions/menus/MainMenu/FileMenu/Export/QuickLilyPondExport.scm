cf99f7fb342460241187fbfc52b594b7a776d6f7
Richard Shann <richard.shann@virgin.net>
2015-10-30 11:10:56 +0000

    Fix quick lilypond export for no filename

--- actions/menus/MainMenu/FileMenu/Export/QuickLilyPondExport.scm
+++ actions/menus/MainMenu/FileMenu/Export/QuickLilyPondExport.scm
@@ -1,3 +1,4 @@
 ;;QuickLilyPondExport
 (d-ExportMUDELA (let ((f (d-Open "query=filename"))) 
-	 (substring f 0 (- (string-length f) 7))))
+    (if f (substring f 0 (- (string-length f) 7))
+        (d-WarningDialog  (_ "The score does not have a file name, so no file name for the output LilyPond file can be constructed. Save the score first.")))))
--- actions/menus/MainMenu/FileMenu/Export/QuickLilyPondPart.scm
+++ actions/menus/MainMenu/FileMenu/Export/QuickLilyPondPart.scm
@@ -2,15 +2,15 @@
 (d-PushPosition)
 (d-GoToBeginning)
 (let ((name  (d-Open "query=filename")))
-	(if name
-		(let* ((filename  (string-append (substring name 0 (- (string-length name) 7)) "-denemo-" (number->string (d-GetStaff)) ".ly"))   
-				(port (open-file filename "w")))
-			(let loop ()
-				(if port
-					(begin 
-						(format port "~A " (d-GetLilyPond))
-						(if (d-MoveCursorRight)
-							(loop)
-							(close-port port))))))
-		(d-WarningDialog (_ "The score has not been saved, so no name or location for the part is defined"))))
-(d-PopPosition)	
+    (if name
+        (let* ((filename  (string-append (substring name 0 (- (string-length name) 7)) "-denemo-" (number->string (d-GetStaff)) ".ly"))   
+                (port (open-file filename "w")))
+            (let loop ()
+                (if port
+                    (begin 
+                        (format port "~A " (d-GetLilyPond))
+                        (if (d-MoveCursorRight)
+                            (loop)
+                            (close-port port))))))
+        (d-WarningDialog (_ "The score does not have a file name, so no file name for the output LilyPond file can be constructed. Save the score first."))))
+(d-PopPosition) 
