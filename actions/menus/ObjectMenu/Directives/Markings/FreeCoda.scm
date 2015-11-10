a7b154f597d812d8a1f24713f94b1df65bc6268e
Richard Shann <richard.shann@virgin.net>
2015-11-10 11:50:25 +0000

    Add free Coda

---
+++ actions/menus/ObjectMenu/Directives/Markings/FreeCoda.scm
@@ -0,0 +1,14 @@
+;;;Coda symbol attached to the next object, for example a barline. But shown between notes
+(let ((tag "FreeCoda"))
+   (if (d-Directive-standalone? tag)
+         (let ((choice (RadioBoxMenu 
+                (cons (_ "Object Inspector") 'help) 
+                (cons (_ "Delete") 'delete))))
+            (case choice
+                ((help)
+                   (d-DisplayCurrentObject))
+                  ((delete)
+                    (d-DirectiveDelete-standalone tag))))  
+    (StandAloneDirectiveProto (cons tag "\\tweak break-visibility  #begin-of-line-invisible \\mark \\markup { \\musicglyph #\"scripts.coda\" } ") #t LG-Coda "" )))
+
+        
