f068f6b07534112dc4d465a8b2e011b98dd6c539
Richard Shann <richard.shann@virgin.net>
2015-11-15 13:58:09 +0000

    avoid legacy name

--- src/command/scorelayout.c
+++ src/command/scorelayout.c
@@ -665,7 +665,7 @@ remove_lyric_element (GtkWidget * widget, gchar * context_text)
 }
 
 
-static void get_voice_list (GtkWidget *button, GtkWidget *frame)
+static void substitute_voice_name (GtkWidget *button, GtkWidget *frame)
 {
     if (!clone_scoreblock_if_needed (frame))
         return;
@@ -751,7 +751,7 @@ create_voice_widget (DenemoStaff * staff, gchar * voicename, guint location)
 
   GtkWidget *button = gtk_button_new_with_label (_("Substitute"));
   gtk_widget_set_tooltip_text (button, _("Substitute the music of this staff/voice with the music of the current staff/voice.\nYou can click the \"Music for ...\" button to move the cursor in the Denemo Display onto the staff/voice whose music you want to use before clicking this button to substitute the music. The editing affects only this layout."));
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (get_voice_list), w);//prefix_edit_callback), w);
+  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (substitute_voice_name), w);//prefix_edit_callback), w);
   gtk_box_pack_start (GTK_BOX (ret), button, FALSE, TRUE, 0);
 
   return ret;
