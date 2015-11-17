d5f53fa34d9b1daa4d4811f463b7beaa79becd86
Richard Shann <richard.shann@virgin.net>
2015-11-17 12:51:59 +0000

    Avoid hanging on rests. Extend to duplicate measure for full measures

--- actions/menus/ObjectMenu/NotesRests/InsertNote/FillMeasure.scm
+++ actions/menus/ObjectMenu/NotesRests/InsertNote/FillMeasure.scm
@@ -1,9 +1,18 @@
 ;;;FillMeasure
 (let ((appending (Appending?)))
-(if appending
-    (d-MoveCursorLeft))
-(if (and (Music?) (> (d-GetDurationInTicks) 0))
-    (while (not (MeasureFillStatus)) 
-         (d-DuplicateChord)))
-(if appending
-    (d-MoveCursorRight)))
+    (if (FullDurationMeasure?)
+        (d-AddDuplicateMeasure)
+        (if (ZeroDurationMeasure?)
+            (d-PutNote)
+            (begin
+                (if appending
+                    (d-MoveCursorLeft))
+                (if (and (Music?) (> (d-GetDurationInTicks) 0))
+                    (while (not (MeasureFillStatus)) 
+                         (d-DuplicateRestNoteChord))
+                     (begin
+                        (if appending
+                            (d-MoveCursorRight))
+                        (d-PutNote)))
+                (if appending
+                    (d-MoveCursorRight))))))
