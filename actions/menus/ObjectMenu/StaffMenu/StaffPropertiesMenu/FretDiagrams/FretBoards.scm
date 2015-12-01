8a923872d3a027fd6d1a6cdb28a96ea4a6023f3c
Richard Shann <richard.shann@virgin.net>
2015-12-01 12:44:05 +0000

    Add support for mandolin and ukulele fret diagrams, label Chord Symbols command as toggle, correct its tooltip

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/FretDiagrams/FretBoards.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/FretDiagrams/FretBoards.scm
@@ -1,6 +1,16 @@
 ;;;FretBoards
-(let ((tag "FretBoards"))
-	(ToggleDirective "staff" "postfix" tag " \\new FretBoards <<\n"  DENEMO_OVERRIDE_LILYPOND)
-	(ToggleDirective "voice" "postfix" tag "\n" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
-	(ToggleDirective "clef" "postfix" tag "{} \n"  (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND)))
+(let ((tag "FretBoards")(choice #f))
+     (if (not (d-Directive-staff? tag)) 
+        (set! choice (RadioBoxMenu
+                    (cons (_ "Guitar")   #f)   
+                    (cons (_ "Mandolin") "mandolin")
+                    (cons (_ "Ukulele") "ukulele"))))
+    (ToggleDirective "voice" "postfix" tag "\n" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+    (if choice
+        (begin
+            (d-LilyPondInclude (string-append "predefined-" choice "-fretboards.ly"))
+            (ToggleDirective "staff" "postfix" tag 
+                (string-append " \\new FretBoards <<\n\\set Staff.stringTunings = #" choice "-tuning\n")  DENEMO_OVERRIDE_LILYPOND))
+        (ToggleDirective "staff" "postfix" tag " \\new FretBoards <<\n"  DENEMO_OVERRIDE_LILYPOND)))
+        
 
