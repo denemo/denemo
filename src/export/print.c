769af763c25f6d788eef88035ca613e41de1ac77
Richard Shann <richard.shann@virgin.net>
2015-12-12 16:35:36 +0000

    Remove spurious,if harmless else

--- src/export/print.c
+++ src/export/print.c
@@ -296,7 +296,6 @@ process_lilypond_errors (gchar * filename)
       g_free (bytes);
       return;
     }
-    else
   //g_print("\nLilyPond error messages\n8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8>< %s \n8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><8><\n", bytes);
   epoint = g_strstr_len (bytes, strlen (bytes), filename_colon);
   if (epoint)
