170e020b51ad297a84276c69ce96865c90b0c37f
Richard Shann <richard.shann@virgin.net>
2016-01-12 12:48:42 +0000

    Fix problems with script

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/MidiInstrumentName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/MidiInstrumentName.scm
@@ -1,17 +1,16 @@
 ;;;MidiInstrumentName
-(let ((tag  "MidiInstrumentName") (current "") (thematch "") (indent "0.0") (size 16.0) (nextmovement #f) (staff (number->string (d-GetStaff))))
-(if (equal? MidiInstrumentName::params "edit")
-    (set! MidiInstrumentName::params #f))
-  (if (string? MidiInstrumentName::params)
+(let ((params MidiInstrumentName::params) (tag  "MidiInstrumentName") (current ""))
+(if (equal? params "edit")
+    (set! params #f))
+  (if (string? params)
     (begin
-        (set! current InstrumentName::params)
-        (set! InstrumentName::params #f))
+        (set! current params)
+        (set! params #f))
     (begin
-            (set! current "Violin")
-            (set! current (d-GetUserInput (_ "InstrumentName") (_ "Give name of MIDI instrument for current staff:") current))))
-
+            (set! current "violin")
+            (set! current (d-GetUserInput (_ "MIDI Instrument Name") (_ "Give name of MIDI instrument for current staff:") current))))
   (if (string? current)
      (begin
         (d-DirectivePut-staff-display tag current)
         (d-DirectivePut-staff-override tag  DENEMO_OVERRIDE_GRAPHIC)
-        (d-DirectivePut-staff-postfix tag  (string-append "\\set Staff.midiInstrument = #\"" current "\" ")))))
+        (d-DirectivePut-staff-postfix tag  (string-append "\n\\set Staff.midiInstrument = #\"" current "\" ")))))
