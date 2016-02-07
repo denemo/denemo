56c123821c6ee56fa287bff97ff780b56b92bcae
Richard Shann <richard.shann@virgin.net>
2016-02-07 16:10:56 +0000

    Install d-InsertBreakInStaff command to insert a gap in the current line

---
+++ actions/menus/ObjectMenu/MeasureMenu/InsertBreakInStaff.scm
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;
+(let ((tag1 "StartBreak")(tag2 "StopBreak"))
+    (d-InsertMeasureBefore)
+    (d-DirectivePut-standalone tag1)
+    (d-DirectivePut-standalone-minpixels tag1 30)
+    (d-DirectivePut-standalone-graphic tag1 "\nX\nDenemo\n48")
+    (d-DirectivePut-standalone-postfix tag1 "\\hideNotes \\stopStaff ")
+    (d-WholeMeasureSpacer)
+    (d-DirectivePut-standalone tag2)
+    (d-DirectivePut-standalone-postfix tag2 (string-append
+        "\\unHideNotes \\startStaff  \\set Staff.forceClef = ##t \\bar "" \\set Staff.forceClef = ##t \\once \\override Staff.Clef.full-size-change = ##t \\clef " (d-GetPrevailingClef) " "))
+    (d-DirectivePut-standalone-minpixels tag2 30)
+    (d-DirectivePut-standalone-graphic tag1 "\nY\nDenemo\n48")
+    (d-RefreshDisplay)
+    (d-SetSaved #f))
