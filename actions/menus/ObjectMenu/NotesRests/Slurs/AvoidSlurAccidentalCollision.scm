a59352d8a234d7d294dcc2ea4f8b0122b0a41004
Richard Shann <richard.shann@virgin.net>
2016-03-02 15:17:18 +0000

    Allow adjustments to slur collision avoidance

--- actions/menus/ObjectMenu/NotesRests/Slurs/AvoidSlurAccidentalCollision.scm
+++ actions/menus/ObjectMenu/NotesRests/Slurs/AvoidSlurAccidentalCollision.scm
@@ -2,15 +2,18 @@
 ;;;http://code.google.com/p/lilypond/issues/detail?id=796
 (let ((tag "AvoidSlurAccidentalCollision"))
     (if (d-Directive-chord? tag)
- 	(begin
- 		(d-DirectiveDelete-chord  tag)
- 		(d-InfoDialog (_ "Slur/Accidental avoidance removed")))
- 	(begin
- 		(if (d-IsSlurStart)
- 			(begin
-				(d-DirectivePut-chord-prefix tag "\\once \\override Slur #'details #'edge-attraction-factor = #1 ")
-				(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
-				(d-DirectivePut-chord-display tag "(X"))
-			(d-InfoDialog (_ "Use only on a slur start to make the slur avoid accidentals on following notes")))))
-(d-RefreshDisplay)
-(d-SetSaved #f))
+        (begin
+            (d-DirectiveDelete-chord  tag)
+            (d-InfoDialog (_ "Slur/Accidental avoidance removed")))
+        (begin
+            (if (d-IsSlurStart)
+                (let ((value (d-GetUserInput "Collision Avoidance" "Give edge attraction factor for slur" "0.5")))
+                    (if (and value (string->number value))
+                        (begin
+                            (d-DirectivePut-chord-prefix tag (string-append "\\once \\override Slur #'details #'edge-attraction-factor = #" value " "))
+                            (d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
+                            (d-DirectivePut-chord-display tag "(X"))   ; here  ) to match the string paren open
+                         (d-InfoDialog (_ "Cancelled"))))   
+                (d-InfoDialog (_ "Use only on a slur start to make the slur avoid accidentals on following notes")))))
+    (d-RefreshDisplay)
+    (d-SetSaved #f))
