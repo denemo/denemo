b0423267910770d24eda334a1f2ce462ef8de338
Richard Shann <richard.shann@virgin.net>
2016-03-22 13:05:59 +0000

    Do not allow instrument name to be set on Dynamics staff

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/InstrumentName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/InstrumentName.scm
@@ -2,48 +2,54 @@
 (let ((tag  "InstrumentName") (current "") (thematch "") (indent "0.0") (size 16.0) (nextmovement #f) (staff (number->string (d-GetStaff))))
 (if (equal? InstrumentName::params "edit")
     (set! InstrumentName::params #f))
-  (if (string? InstrumentName::params)
+    
+  (if (d-Directive-staff? "DynamicsStaff")
     (begin
-        (set! current InstrumentName::params)
-        (set! InstrumentName::params #f))
+        (d-WarningDialog (_ "Instrument Name should not be set on a Dynamics Line"))
+        (d-DirectiveDelete-staff tag))
     (begin
-        (if (d-NextMovement)
+          (if (string? InstrumentName::params)
             (begin
-                (set! nextmovement (d-GetMovement))
-                (d-PreviousMovement)))
-            ;so nextmovement is 2 if there is more than one movement and the name setting is on a staff in the first movement.      
-        (set! current (d-DirectiveGet-staff-display tag ))
-        (if (not current)
-            (set! current "Violin"))
-            (set! current (d-GetUserInput (_ "InstrumentName") (_ "Give name of instrument/voice/part\nfor current staff:") current))))
+                (set! current InstrumentName::params)
+                (set! InstrumentName::params #f))
+            (begin
+                (if (d-NextMovement)
+                    (begin
+                        (set! nextmovement (d-GetMovement))
+                        (d-PreviousMovement)))
+                    ;so nextmovement is 2 if there is more than one movement and the name setting is on a staff in the first movement.      
+                (set! current (d-DirectiveGet-staff-display tag ))
+                (if (not current)
+                    (set! current "Violin"))
+                    (set! current (d-GetUserInput (_ "InstrumentName") (_ "Give name of instrument/voice/part\nfor current staff:") current))))
 
-  (if (string? current)
-     (let ((transparent-start "") (transparent-end "")(movement (number->string (d-GetMovement))))
-        (d-DirectivePut-staff-display tag current)
-        (d-DirectivePut-staff-override tag  DENEMO_OVERRIDE_GRAPHIC)
-        (if (equal?  current (_ "Unnamed"))
-                (begin
-                    (set! transparent-start "\\transparent {")
-                    (set! transparent-end "}")))
-        (d-DirectivePut-staff-postfix tag  (string-append "\\set Staff.instrumentName = \\markup {  \\with-url #'\"scheme:(d-GoToPosition " 
-                movement 
-                " " staff " 1 1)(let ((choice (d-PopupMenu (list (cons (_ \\\"Change Name\\\") d-InstrumentName)   (cons (_ \\\"Change Indent\\\") d-ScoreIndent)))))
-                    (if choice (choice)))    \" " transparent-start "\"" current "\" " transparent-end"}"))
+          (if (string? current)
+             (let ((transparent-start "") (transparent-end "")(movement (number->string (d-GetMovement))))
+                (d-DirectivePut-staff-display tag current)
+                (d-DirectivePut-staff-override tag  DENEMO_OVERRIDE_GRAPHIC)
+                (if (equal?  current (_ "Unnamed"))
+                        (begin
+                            (set! transparent-start "\\transparent {")
+                            (set! transparent-end "}")))
+                (d-DirectivePut-staff-postfix tag  (string-append "\\set Staff.instrumentName = \\markup {  \\with-url #'\"scheme:(d-GoToPosition " 
+                        movement 
+                        " " staff " 1 1)(let ((choice (d-PopupMenu (list (cons (_ \\\"Change Name\\\") d-InstrumentName)   (cons (_ \\\"Change Indent\\\") d-ScoreIndent)))))
+                            (if choice (choice)))    \" " transparent-start "\"" current "\" " transparent-end"}"))
 
-        (if (> (string-length current) 0)
-                    (d-StaffProperties (string-append "denemo_name=" current))
-                    (d-StaffProperties "denemo_name=unnamed"))
-            ;;if this is the first of several movements, ask to set the rest if called directly           
-        (if (and (not InstrumentName::params) (equal? nextmovement 2))
-                (let ((do-all (d-GetUserInput (_ "Instrument Name") (_ "This sets the name in Movement 1\nSet this name for the same staff in other movements?") (_ "y"))))
-                    (if (equal? do-all (_ "y"))
-                        (let ((staffnum (d-GetStaff)))
-                            (d-PushPosition)
-                            (while (d-NextMovement)
-                                (if (d-GoToPosition #f staffnum 1 1)
-                                    (d-InstrumentName current)))
-                            (d-PopPosition)))))
-                    
-        (set! size (/ (string->number (d-ScoreProperties "query=fontsize")) 10.0))
-        (set! indent (max (string->number indent) (* size (string-length current))))
-        (d-ScoreIndent indent))))
+                (if (> (string-length current) 0)
+                            (d-StaffProperties (string-append "denemo_name=" current))
+                            (d-StaffProperties "denemo_name=unnamed"))
+                    ;;if this is the first of several movements, ask to set the rest if called directly           
+                (if (and (not InstrumentName::params) (equal? nextmovement 2))
+                        (let ((do-all (d-GetUserInput (_ "Instrument Name") (_ "This sets the name in Movement 1\nSet this name for the same staff in other movements?") (_ "y"))))
+                            (if (equal? do-all (_ "y"))
+                                (let ((staffnum (d-GetStaff)))
+                                    (d-PushPosition)
+                                    (while (d-NextMovement)
+                                        (if (d-GoToPosition #f staffnum 1 1)
+                                            (d-InstrumentName current)))
+                                    (d-PopPosition)))))
+                            
+                (set! size (/ (string->number (d-ScoreProperties "query=fontsize")) 10.0))
+                (set! indent (max (string->number indent) (* size (string-length current))))
+                (d-ScoreIndent indent))))))
--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/ShortInstrumentName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/ShortInstrumentName.scm
@@ -1,17 +1,22 @@
 ;ShortInstrumentName
 (let ((current "") (thematch ""))
-  (set! current (d-DirectiveGet-staff-postfix "ShortInstrumentName" ))
-  (if (boolean? current)
-      (set! current "Vln")
-      (begin
-	(set! thematch (string-match "\\\\set Staff.shortInstrumentName = \"([^\"]*)\"" current))
-	(if (regexp-match? thematch)
-	    (set! current (match:substring thematch 1))
-	    (set! current "Vln"))))
-  (set! current (d-GetUserInput (_ "Short Instrument Name") (_ "Give Short Instrument Name:") current))
-  (if current
-  	(begin 
-  		(d-DirectivePut-staff-display "ShortInstrumentName" current)
-  		(d-DirectivePut-staff-override "ShortInstrumentName"  DENEMO_OVERRIDE_GRAPHIC)
-  		(d-DirectivePut-staff-postfix "ShortInstrumentName"  (string-append "\\set Staff.shortInstrumentName = \"" current "\""))
-  		(d-SetSaved #f))))
+  (if (d-Directive-staff? "DynamicsStaff")
+    (begin
+        (d-WarningDialog (_ "Instrument Name should not be set on a Dynamics Line"))
+        (d-DirectiveDelete-staff tag))
+    (begin
+      (set! current (d-DirectiveGet-staff-postfix "ShortInstrumentName" ))
+      (if (boolean? current)
+          (set! current "Vln")
+          (begin
+        (set! thematch (string-match "\\\\set Staff.shortInstrumentName = \"([^\"]*)\"" current))
+        (if (regexp-match? thematch)
+            (set! current (match:substring thematch 1))
+            (set! current "Vln"))))
+      (set! current (d-GetUserInput (_ "Short Instrument Name") (_ "Give Short Instrument Name:") current))
+      (if current
+        (begin 
+            (d-DirectivePut-staff-display "ShortInstrumentName" current)
+            (d-DirectivePut-staff-override "ShortInstrumentName"  DENEMO_OVERRIDE_GRAPHIC)
+            (d-DirectivePut-staff-postfix "ShortInstrumentName"  (string-append "\\set Staff.shortInstrumentName = \"" current "\""))
+            (d-SetSaved #f))))))
