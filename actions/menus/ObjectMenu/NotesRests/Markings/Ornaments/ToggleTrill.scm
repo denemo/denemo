dc87fca2f2ac6c8ebd81428ba4ccfcf0ca99bbec
Richard Shann <richard.shann@virgin.net>
2016-04-15 10:56:20 +0100

    Better rendition of trill ornament

--- actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/ToggleTrill.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/Ornaments/ToggleTrill.scm
@@ -1,5 +1,14 @@
-;;;Trill                   
-					(let ((tag "ToggleTrill"))
-						(ChordAnnotation tag "\\trill"    ToggleTrill::params    LG-Trill)
-					(d-PlayMidiNote 78  255 9 100))
+;;;ToggleTrill                   
+	(let ((tag "ToggleTrill") (note (d-GetNoteAsMidi)) (lower #f)(upper #f) )
+		(ChordAnnotation tag "\\trill"    ToggleTrill::params    LG-Trill)
+			(if (d-Directive-chord? tag)
+				(begin		
+					(set! lower (string-append "(d-PlayMidiNote " (number->string note) "  255 0 100)"))
+					(set! upper (string-append "(d-PlayMidiNote " (number->string (+ 2 note)) "  255 0 100)"))
+					(eval-string upper)
+					(d-OneShotTimer 100 lower)
+					(d-OneShotTimer 200 upper)
+					(d-OneShotTimer 300 lower)
+					(d-OneShotTimer 400 upper)
+					(d-OneShotTimer 500  (string-append "(d-PlayMidiNote " (number->string note) "  255 0 300)")))))
 	
