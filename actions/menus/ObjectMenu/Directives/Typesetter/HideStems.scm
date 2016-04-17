54d6da83405d071ec3f22b8b6a91ac961c7c2cc3
Richard Shann <richard.shann@virgin.net>
2016-04-17 15:34:27 +0100

    Fix typos

--- actions/menus/ObjectMenu/Directives/Typesetter/HideStems.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/HideStems.scm
@@ -1,11 +1,11 @@
 ;;;HideStems
 (let ((tag "HideStems"))
     (if (d-Directive-standalone? tag)
-      (d-InfoDialog (_ "This directive turns off stems")))
-   (begin
-	(d-DirectivePut-standalone tag)
-	(d-DirectivePut-standalone-postfix tag  "\\hide Staff.Stem ")
-	(d-DirectivePut-standalone-graphic tag "\n|x\nDenemo\n20")
-	(d-DirectivePut-standalone-minpixels "BeamingOff" 20)
-	(d-SetSaved #f)
-	(d-RefreshDisplay)))
+           (d-InfoDialog (_ "This directive turns off stems"))
+	   (begin
+		(d-DirectivePut-standalone tag)
+		(d-DirectivePut-standalone-postfix tag  "\\hide Staff.Stem ")
+		(d-DirectivePut-standalone-graphic tag "\n|x\nDenemo\n20")
+		(d-DirectivePut-standalone-minpixels tag 20)
+		(d-SetSaved #f)
+		(d-RefreshDisplay))))
--- actions/menus/ObjectMenu/Directives/Typesetter/ShowStems.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/ShowStems.scm
@@ -1,11 +1,11 @@
 ;;;ShowStems
 (let ((tag "ShowStems"))
     (if (d-Directive-standalone? tag)
-      (d-InfoDialog (_ "This directive turns on typesetting of stems on notes.")))
-   (begin
-	(d-DirectivePut-standalone tag)
-	(d-DirectivePut-standalone-postfix tag  "\\undo \\hide Staff.Stem ")
-	(d-DirectivePut-standalone-graphic tag "\n|\nDenemo\n20")
-	(d-DirectivePut-standalone-minpixels "BeamingOff" 20)
-	(d-SetSaved #f)
-	(d-RefreshDisplay)))
+      	(d-InfoDialog (_ "This directive turns on typesetting of stems on notes."))
+	(begin
+		(d-DirectivePut-standalone tag)
+		(d-DirectivePut-standalone-postfix tag  "\\undo \\hide Staff.Stem ")
+		(d-DirectivePut-standalone-graphic tag "\n|\nDenemo\n20")
+		(d-DirectivePut-standalone-minpixels tag 20)
+		(d-SetSaved #f)
+		(d-RefreshDisplay))))
