31f9c5b7e7bc87f6e3e3b114a269bfe5dbc4a00a
Richard Shann <richard.shann@virgin.net>
2016-04-17 15:56:07 +0100

    Update for LilyPond syntax change

--- actions/menus/ObjectMenu/Directives/Markings/Barlines/HalfBarline.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Barlines/HalfBarline.scm
@@ -1,2 +1,2 @@
 ;;;Half Barline
-(StandAloneDirectiveProto (cons "HalfBarline" "\\once \\override Staff . BarLine #'bar-size = #2 \\bar \"|\""))
+(StandAloneDirectiveProto (cons "HalfBarline" "\\once \\override Staff.BarLine #'bar-extent = #'(-1 . 1) \\bar \"|\""))
