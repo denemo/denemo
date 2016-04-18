2a9a215d583dc11776e5fec4f3f82ffbae0fc722
Richard Shann <richard.shann@virgin.net>
2016-04-18 16:23:12 +0100

    Add lyric alignment command

---
+++ actions/menus/ObjectMenu/Lyrics/InsertLyricAlignment.scm
@@ -0,0 +1,4 @@
+;InsertLyricAlignment
+(let ((text  (RadioBoxMenu (cons (_ "Center") "CENTER") (cons (_ "Left") "LEFT")(cons (_ "Right") "RIGHT"))))
+       (if text 
+            (d-InsertTextInVerse (string-append "\n\\override LyricText.self-alignment-X = #" text  "%{" (_ "Note: start lyrics on a new line") " %}\n"))))
