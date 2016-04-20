1dc77adabc00fd6d2cf7e206134df321c4e11f86
Richard Shann <richard.shann@virgin.net>
2016-04-20 10:59:00 +0100

    Set size of text

---
+++ actions/menus/ObjectMenu/Directives/Typesetter/Spanning/CadenzaOn.scm
@@ -0,0 +1,6 @@
+;;;CadenzaOn
+          (d-CadenzaTime)
+          (d-SmallFontStart)
+          (d-MultiLineTextAnnotation (list (cons 'text "Cadenza \\italic {ad lib.} ") (cons 'scale "1") '()))
+       
+        
