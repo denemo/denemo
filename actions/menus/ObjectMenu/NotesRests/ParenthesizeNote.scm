c182f811da31482a516c065ff590656feff518fd
Richard Shann <richard.shann@virgin.net>
2016-05-02 16:32:59 +0100

    Let parenthesize note apply to rests

--- actions/menus/ObjectMenu/NotesRests/ParenthesizeNote.scm
+++ actions/menus/ObjectMenu/NotesRests/ParenthesizeNote.scm
@@ -1,16 +1,23 @@
 ;;; ParenthesizeNote
  (let ((tag  "ParenthesizeNote"))
-   (if (d-Directive-note? tag)
- 	(let ((choice (RadioBoxMenu 
-                (cons (_ "Object Inspector") 'help) 
-                (cons (_ "Delete") 'delete))))
-            (case choice
-                ((help)
-                   (d-DisplayCurrentObject))
-                  ((delete)
-                    (d-DirectiveDelete-note tag)))) 
+ (if (Note?)
  	(begin
-		(d-DirectivePut-note-prefix tag "\\parenthesize ")
-		(d-DirectivePut-note-display tag "()")))
-  (d-SetSaved #f)
-  (d-RefreshDisplay))
+	   (if (d-Directive-note? tag)
+	 	(let ((choice (RadioBoxMenu 
+		        (cons (_ "Object Inspector") 'help) 
+		        (cons (_ "Delete") 'delete))))
+		    (case choice
+		        ((help)
+		           (d-DisplayCurrentObject))
+		          ((delete)
+		            (d-DirectiveDelete-note tag)))) 
+	 	(begin
+			(d-DirectivePut-note-prefix tag "\\parenthesize ")
+			(d-DirectivePut-note-display tag "()")))
+	  (d-SetSaved #f))
+      (begin
+      	(if (Music?)
+      		(d-ParenthesizeChord)
+      		(begin
+      			(d-WarningDialog (_ "Applies to Note, Chord or Rest")))))))
+ 
