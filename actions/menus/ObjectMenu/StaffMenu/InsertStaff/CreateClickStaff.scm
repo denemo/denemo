908f9cb64a2db087bfed9b21480b3f26e2d9a83b
Richard Shann <richard.shann@virgin.net>
2016-06-05 21:01:58 +0100

    Allow for upbeats and re-starting on empty measure

--- actions/menus/ObjectMenu/StaffMenu/InsertStaff/CreateClickStaff.scm
+++ actions/menus/ObjectMenu/StaffMenu/InsertStaff/CreateClickStaff.scm
@@ -3,8 +3,9 @@
       (old_volume (d-MasterVolume)))
       
     (define (insert-duration str)
-        (disp "Inserting (d-" str ")\n")
-        (eval-string (string-append "(d-" str ")")))
+        (if (and str (string->number str) (not (string-suffix? "." str)))
+            (eval-string (string-append "(d-" str ")"))
+            (d-InfoDialog (_ "Fill in the upbeat in the click track before playing"))))
     (define (duplicate-measures)
         (let loop ()
             (if (and (FullDurationMeasure?) (d-MoveToMeasureRight) (EmptyMeasure?))
@@ -24,7 +25,7 @@
             (d-Play "(d-StaffToPlay)(d-CreateClickStaff)"))
             
 ;;;procedure adds beats to the bar and plays it the recursively call the procedure to ask if it should be duplicated.               
-    (define (populate-measures)
+    (define (populate-measure)
         (let* ((numerator  (duration::GetNumerator))
                 (denominator (duration::lilypond->denemo (duration::GetDenominator)))
                 (put-beat (eval-string (string-append "d-" (number->string denominator)))))
@@ -43,32 +44,37 @@
     ;;main procedure     
     (if (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name"))
             (begin
-                (if (FullDurationMeasure?)
-                    (let ((choice (RadioBoxMenu (cons (_ "Populate with these clicks") 'populate) (cons (_ "Play clicks again") 'play) (cons (_ "Edit clicks") 'edit))))
-                        (case choice
-                            ((populate)
-                                (if (duplicate-measures)
-                                    (begin
-                                        (d-MoveCursorRight)
-                                        (populate-measures))))   
-                            ((play)
-                                (play-clicks-and-recurse))
-                            ((edit)
-                                (d-InfoDialog (_ "Edit the clicks and then re-run this command to populate the subsequent empty measures up to any time signature change. Then repeat for each change of time signature.")))))))
+                (if (EmptyMeasure?)
+                    (populate-measure)
+                    (if (FullDurationMeasure?)
+                        (let ((choice (RadioBoxMenu (cons (_ "Populate with these clicks") 'populate) (cons (_ "Play clicks again") 'play) (cons (_ "Edit clicks") 'edit))))
+                            (case choice
+                                ((populate)
+                                    (if (duplicate-measures)
+                                        (begin
+                                            (d-MoveCursorRight)
+                                            (populate-measure))))   
+                                ((play)
+                                    (play-clicks-and-recurse))
+                                ((edit)
+                                    (d-InfoDialog (_ "Edit the clicks and then re-run this command to populate the subsequent empty measures up to any time signature change. Then repeat for each change of time signature."))))))))
             (begin
                     (d-MoveToBeginning)
                     (d-MasterVolume 0)
                     (d-NewStructuredStaff 'initial)
                     (while (d-StaffUp))
                     (d-StaffProperties "midi_channel=9")
+                    (d-DirectivePut-clef-graphic "DrumClef" "DrumClef")
+                    (d-DirectivePut-clef-override "DrumClef" DENEMO_OVERRIDE_GRAPHIC)
                     (d-NonPrintingStaff)
                     (d-MuteStaff "unmute")
                     (d-StaffProperties (string-append "denemo_name=" DenemoClickTrack))
                     (if (d-Directive-standalone? "Upbeat")
                         (begin
+                            (d-MoveCursorRight)
                             (insert-duration (duration::shortfall))
                             (d-MoveToMeasureRight)))
-                    (populate-measures)))
+                    (populate-measure)))
     (d-MasterVolume old_volume)     
     (d-HighlightCursor old_highlight)
     (d-SetSaved #f))
