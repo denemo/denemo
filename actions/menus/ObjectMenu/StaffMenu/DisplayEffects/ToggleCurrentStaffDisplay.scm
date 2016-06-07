fbb413215ee7df28fd77aac5ce7b516a1be4b205
Richard Shann <richard.shann@virgin.net>
2016-06-07 09:40:03 +0100

    Reposition cursor after hiding staff and set dirty flag and refresh display

--- actions/menus/ObjectMenu/StaffMenu/DisplayEffects/ToggleCurrentStaffDisplay.scm
+++ actions/menus/ObjectMenu/StaffMenu/DisplayEffects/ToggleCurrentStaffDisplay.scm
@@ -1,2 +1,6 @@
 ;;;ToggleCurrentStaffDisplay
 (d-StaffHidden (not (d-StaffHidden)))
+(while (and  (d-StaffHidden) (d-MoveToStaffDown)))
+(while (and  (d-StaffHidden) (d-MoveToStaffUp)))
+(d-RefreshDisplay)
+(d-SetSaved #f)
