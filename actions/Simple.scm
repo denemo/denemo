fb796e16ac24588960bf1a0d47aed53d7f191cec
Richard Shann <richard.shann@virgin.net>
2016-06-15 16:03:26 +0100

    Do not set up non-default LilyPond values

--- actions/Simple.scm
+++ actions/Simple.scm
@@ -1,8 +1,5 @@
 (d-MouseInsertion)
-(if (and (= 1 (d-GetMovement)) (not (d-Directive-header? "ScoreTitle")))
-(SetHeaderField "title" "Untitled"))
 
-(d-LimitInterSystemSpace 1.2)
 (define (InitializeTypesetting)
   (d-PushPosition)
   (d-CheckScore)
@@ -10,11 +7,11 @@
     (let ((ok #f))
       (d-InfoDialog (_ "Score Check: Error in this measure"))
       (set! ok (RadioBoxMenu (cons (_ "Print Anyway") "y") (cons (_ "Cancel") "n")))
-			(if (equal? ok "n")
-				(begin
-					(d-PopPushPosition);;goes to the position on the top of the stack, replacing the top of the stack with the position before it went
-					(d-PopPosition);;goes to the error position that was pushed
-					(exit)))))
-	(d-PopPosition))
+            (if (equal? ok "n")
+                (begin
+                    (d-PopPushPosition);;goes to the position on the top of the stack, replacing the top of the stack with the position before it went
+                    (d-PopPosition);;goes to the error position that was pushed
+                    (exit)))))
+    (d-PopPosition))
 (d-SetSaved #t)
 (display "Simple Profile\n")
