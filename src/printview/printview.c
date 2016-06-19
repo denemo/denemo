25d04c872ebd84a9d4989d0e67af180f65658749
Dr. Tobias Quathamer <toddy@debian.org>
2016-06-19 16:55:30 +0200

    Replace deprecated function ev_document_model_set_dual_page()

--- src/printview/printview.c
+++ src/printview/printview.c
@@ -146,7 +146,14 @@ set_printarea_doc (EvDocument * doc)
       g_object_unref (ev_document_model_get_document (model));  //FIXME check if this releases the file lock on windows.s
       ev_document_model_set_document (model, doc);
     }
-  ev_document_model_set_dual_page (model, GPOINTER_TO_INT (g_object_get_data (G_OBJECT (Denemo.printarea), "Duplex")));
+  if (GPOINTER_TO_INT (g_object_get_data (G_OBJECT (Denemo.printarea), "Duplex")))
+    {
+      ev_document_model_set_page_layout (model, EV_PAGE_LAYOUT_DUAL);
+    }
+  else
+    {
+      ev_document_model_set_page_layout (model, EV_PAGE_LAYOUT_SINGLE);
+    }
   get_wysiwyg_info ()->Mark.width = 0;  //indicate that there should no longer be any Mark placed on the score
 }
 
