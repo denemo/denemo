ac9caa096cc63e3a0cbf4b5ecdebde07fc524d66
Richard Shann <richard.shann@virgin.net>
2016-07-20 15:19:45 +0100

    Tell travis about fann

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -4,22 +4,23 @@ set -e
 gcc_dependencies()
 {
   sudo apt-get -qq install \
-	intltool \
-	guile-2.0-dev \
-	libsndfile-dev \
-	libsmf-dev \
-	libgtk-3-dev \
-	libgtksourceview-3.0-dev \
-	libevince-dev \
-	libfluidsynth-dev \
-	librubberband-dev \
-	portaudio19-dev \
-	libaubio-dev \
-	libfftw3-dev \
-	libportmidi-dev \
-	gtk-doc-tools \
-	lilypond \
-	cppcheck
+    intltool \
+    guile-2.0-dev \
+    libsndfile-dev \
+    libsmf-dev \
+    libgtk-3-dev \
+    libgtksourceview-3.0-dev \
+    libevince-dev \
+    libfluidsynth-dev \
+    librubberband-dev \
+    portaudio19-dev \
+    libaubio-dev \
+    libfann-dev \
+    libfftw3-dev \
+    libportmidi-dev \
+    gtk-doc-tools \
+    lilypond \
+    cppcheck
 }
 
 gcc_dependencies
