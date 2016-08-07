f7b2e3c6fb58736999ca889e93500c1f29f9137f
Richard Shann <richard.shann@virgin.net>
2016-08-07 18:08:16 +0100

    Add standalone D.C and fine commands

---
+++ actions/menus/ObjectMenu/Directives/Markings/TextMarks/DaCapo.scm
@@ -0,0 +1,1 @@
+(d-MultiLineTextAnnotation (list (cons 'display "D.C al fine")(cons 'scale "1") (cons 'text "\\bold\\italic {D. C. al fine}"))))
---
+++ actions/menus/ObjectMenu/Directives/Markings/TextMarks/Fine.scm
@@ -0,0 +1,1 @@
+(d-MultiLineTextAnnotation (list (cons 'display "fine")(cons 'scale "1") (cons 'text "\\bold\\italic {fine}"))))
