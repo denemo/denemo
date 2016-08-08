2ae2872d297f2429babb2532c3811c157e4eade8
Richard Shann <richard.shann@virgin.net>
2016-08-08 17:07:30 +0100

    Add d-AssignInstrument command

---
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/AssignInstrument.scm
@@ -0,0 +1,15 @@
+;;;AssignInstrument
+(let ((num-staffs (d-GetStaffsInMovement)) (staff (d-GetStaff)))
+	(SelectStaff)
+	(d-Copy)
+	(d-AddInstrumentStaff)
+	(if (> (d-GetStaffsInMovement) (1+ num-staffs))
+		(d-WarningDialog (_ "Copy and Paste the music from the old staffs to the new, and then delete the old staffs"))
+		(if (>= (d-GetStaff) (1+ staff))
+			(begin
+				(while (and (> (d-GetStaff) (1+ staff))  (d-SwapStaffs)))
+				(d-Paste)
+				(d-MoveToStaffUp)
+				(d-DeleteStaff))
+			(d-WarningDialog (_ "Cancelled")))))
+	
