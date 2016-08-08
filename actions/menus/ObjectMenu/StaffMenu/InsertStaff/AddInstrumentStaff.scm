563468d34204e9e10be3c317e03cd38ef7885942
Richard Shann <richard.shann@virgin.net>
2016-08-08 17:06:48 +0100

    Set up initial time and key signatures for added instrument

--- actions/menus/ObjectMenu/StaffMenu/InsertStaff/AddInstrumentStaff.scm
+++ actions/menus/ObjectMenu/StaffMenu/InsertStaff/AddInstrumentStaff.scm
@@ -1,3 +1,19 @@
-;;;AddInstrument
-(d-AddStaffs DENEMO_INSTRUMENTS_DIR)
-(while (d-MoveToStaffDown))
+;;AddInstrumentStaff
+(let (  (num-staffs (d-GetStaffsInMovement))
+        (key (d-InitialKey "query=keysigname"))
+        (time (d-InitialTimeSig "query=timesigname"))
+        (staff (d-GetStaff)))
+        
+    (d-AddStaffs DENEMO_INSTRUMENTS_DIR)
+    (while (d-MoveToStaffDown))
+    (if (> (d-GetStaff) num-staffs)
+        (begin
+            (while (and (> (d-GetStaff) num-staffs)
+                        (d-InitialKey key)
+                        (d-InitialTimeSig time)
+                        (d-MoveToStaffUp)))
+            (while (d-MoveToStaffDown)))
+        (d-WarningDialog (_ "Cancelled"))))
+    
+    
+    
