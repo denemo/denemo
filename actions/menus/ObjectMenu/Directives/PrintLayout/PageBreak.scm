b77b7938fb87622f414e71ad688520bdf1ae2561
Richard Shann <richard.shann@virgin.net>
2016-08-08 18:08:30 +0100

    Fix bug #42989: Page Break does not check it is at a bar boundary.

--- actions/menus/ObjectMenu/Directives/PrintLayout/PageBreak.scm
+++ actions/menus/ObjectMenu/Directives/PrintLayout/PageBreak.scm
@@ -1,21 +1,22 @@
 ;;;;;;PageBreak
-(if (d-Directive-standalone?  "PageBreak")
+(let ((tag "PageBreak"))
+(if (d-Directive-standalone?  tag)
 	(d-InfoDialog "This sign denotes a Page Break. The music after this point will start on new page. This page break must be on a bar line. Use the Conditional Directives menu to make the page break applicable only to specific layouts. Delete using Del or Backspace key.")
 	(begin
 		(if (d-IsVoice)
 			(begin
 				(d-WarningDialog "The cursor is on a voice. Put line breaks in the main staff that the voice belongs to."))
-
-			(let ((choice #f))
-				(d-DirectivePut-standalone "PageBreak")
-				(d-DirectivePut-standalone-postfix "PageBreak" "\\pageBreak")
-				(d-DirectivePut-standalone-minpixels "PageBreak" 50)
-				(d-DirectivePut-standalone-gy "PageBreak" -25)
-				(d-DirectivePut-standalone-graphic "PageBreak" "
+			(begin
+				(GoToMeasureEnd)
+				(d-DirectivePut-standalone tag)
+				(d-DirectivePut-standalone-postfix tag "\\pageBreak")
+				(d-DirectivePut-standalone-minpixels tag 50)
+				(d-DirectivePut-standalone-gy tag -25)
+				(d-DirectivePut-standalone-graphic tag "
 ⁋
 Denemo
 26")
-				(d-DirectivePut-standalone-minpixels "PageBreak" 10)
+				(d-DirectivePut-standalone-minpixels tag 10)
 				(d-RefreshDisplay)
 				(SetDirectiveConditional)
-				(d-SetSaved #f)))))
+				(d-SetSaved #f))))))
