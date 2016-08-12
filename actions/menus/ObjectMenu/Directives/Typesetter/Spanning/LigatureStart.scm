bc748221b241cd6335796022e26aafb14eee0796
Richard Shann <richard.shann@virgin.net>
2016-08-12 12:17:19 +0100

    Add Ligature commands

---
+++ actions/menus/ObjectMenu/Directives/Typesetter/Spanning/LigatureStart.scm
@@ -0,0 +1,8 @@
+;LigatureStart
+(let ((tag "LigatureStart"))
+    (StandAloneDirectiveProto (cons tag "\\[  ") #f)
+    (d-DirectivePut-standalone-graphic tag "\n[\nDenemo\n40")
+    (d-DirectivePut-standalone-gy tag 10)
+    (d-DirectivePut-standalone-display tag (_ "Ligature Start"))
+    (d-RefreshDisplay)
+    (d-SetSaved #f))
