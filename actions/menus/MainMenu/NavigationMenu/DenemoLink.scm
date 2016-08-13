06910605ea5b05c0c4c5ac2623e4abbc8b8ac13c
Richard Shann <richard.shann@virgin.net>
2016-08-13 12:36:13 +0100

    Allow user to delete a link after checking that is the intent

--- actions/menus/MainMenu/NavigationMenu/DenemoLink.scm
+++ actions/menus/MainMenu/NavigationMenu/DenemoLink.scm
@@ -1,14 +1,19 @@
 ;;;DenemoLink
-(let ((tag "DenemoLink"))
-    (if (d-Directive-standalone? tag)
-        (let ((link (d-DirectiveGet-standalone-data tag)))
-            (if (not link)
-                 (begin
-                    (set! link (d-DirectiveGet-standalone-postfix tag))
-                    (if link
-                        (set! link (string-trim-both link   (lambda (c)(or (eqv? c #\{) (eqv? c #\%))))))))
-            (if link
-                (begin
-                    (d-OpenSource link)
-                    (d-MoveCursorRight))
-                (d-WarningDialog (_ "There is no link here, open the source document and click on it to place one."))))))
+(let ((tag "DenemoLink")(params DenemoLink::params))
+  (if (equal? params "delete")
+  	(let ((choice (RadioBoxMenu (cons (_ "Delete") 'delete) (cons (_ "Cancel") 'cancel)))) 
+  		(case choice
+  			((delete) (d-LockDirective #f) (d-DirectiveDelete-standalone tag))
+  			(else (d-WarningDialog (_ "Cancelled")))))
+      (if (d-Directive-standalone? tag)
+		(let ((link (d-DirectiveGet-standalone-data tag)))
+		    (if (not link)
+		         (begin
+		            (set! link (d-DirectiveGet-standalone-postfix tag))
+		            (if link
+		                (set! link (string-trim-both link   (lambda (c)(or (eqv? c #\{) (eqv? c #\%))))))))
+		    (if link
+		        (begin
+		            (d-OpenSource link)
+		            (d-MoveCursorRight))
+		        (d-WarningDialog (_ "There is no link here, open the source document and click on it to place one.")))))))
