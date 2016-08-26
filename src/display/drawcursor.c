db8ad58340b76a9954209802a262423207a798a3
Richard Shann <richard.shann@virgin.net>
2016-08-26 10:10:23 +0100

    Give more definition to the cursor

--- src/display/drawcursor.c
+++ src/display/drawcursor.c
@@ -33,6 +33,13 @@ draw_cursor (cairo_t * cr, DenemoMovement * si, gint xx, gint y, gint insert_con
     {
       cairo_rectangle (cr, xx - (si->cursoroffend ? CURSOR_WIDTH : 0), height + y - CURSOR_HEIGHT, 2 * CURSOR_WIDTH, 2 * CURSOR_HEIGHT);
       cairo_fill (cr);
+      
+      cairo_set_source_rgba (cr, 0, 0, 0,1);
+      cairo_rectangle (cr, xx - (si->cursoroffend ? CURSOR_WIDTH : 0), height + y - CURSOR_HEIGHT, 2 * CURSOR_WIDTH, 2 * CURSOR_HEIGHT);
+      cairo_stroke (cr);
+    
+      
+      
     }
   else if (minpixels)
     {
@@ -51,6 +58,13 @@ draw_cursor (cairo_t * cr, DenemoMovement * si, gint xx, gint y, gint insert_con
     {
       cairo_rectangle (cr, xx - CURSOR_WIDTH / 2, height + y - CURSOR_MINUS - CURSOR_HEIGHT / 2, 2 * CURSOR_WIDTH, 2 * CURSOR_HEIGHT);
       cairo_fill (cr);
+      
+      
+      cairo_set_source_rgba (cr, 0, 0, 0,1);
+      cairo_rectangle (cr, xx - CURSOR_WIDTH / 2, height + y - CURSOR_MINUS - CURSOR_HEIGHT / 2, 2 * CURSOR_WIDTH, 2 * CURSOR_HEIGHT);
+      cairo_stroke (cr);
+      
+      
     }
 
 
