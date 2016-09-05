59dabbfaaa28173d1ca4eb22a71e0647693faed1
Richard Shann <richard.shann@virgin.net>
2016-09-05 09:01:29 +0100

    Fix tied note entry when using MIDI chords without pedal filter

--- actions/menus/MainMenu/InputMenu/MIDI/ChordEntry.scm
+++ actions/menus/MainMenu/InputMenu/MIDI/ChordEntry.scm
@@ -1,12 +1,14 @@
 ;;;;ChordEntry
 (define-once ChordEntry::active #f)
+(define-once ChordEntry::ignore_ties (d-GetBooleanPref "ignore_ties"))
 (if ChordEntry::active
    (begin ;;;;Filter is already running stop it
       (disp "Turning off Chord without pedal entry\n")
       (d-InputFilterNames (_ "No MIDI Filter"))
-
+      (d-SetPrefs (string-append "<ignore_ties>" (if ChordEntry::ignore_ties "1" "0") "</ignore_ties>"))
       (set! ChordEntry::active #f)
       (d-PutMidi 0)
+      (d-SetPrefs "<ignore_ties>0</ignore_ties>")
       (d-SetBackground #xFFFFFF))
    
   (let  ((ons '())) ;;;;Filter is not already running so run it
@@ -42,6 +44,7 @@
                     (loop))
                ((or (zero? midi) (and  (= command #xE0) (> note 32)))
                     (d-SetBackground #xFFFFFF)
+                    (d-SetPrefs (string-append "<ignore_ties>" (if ChordEntry::ignore_ties "1" "0") "</ignore_ties>"))
                     (disp "Abandoning getting chords\n"))
                (else (DenemoPutMidi midi)(loop)))))) ;;; end of GetChords
 
@@ -49,4 +52,6 @@
     (set! ChordEntry::active #t)
     (d-InputFilterNames (_ "Chord entry without pedal"))
     (d-SetBackground #xC0E0E0)
+    (set! ChordEntry::ignore_ties (d-GetBooleanPref "ignore_ties"))
+    (d-SetPrefs "<ignore_ties>1</ignore_ties>")
     (GetChords)))
