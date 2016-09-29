c6be353c5a9e280777deccce25bc46839f94af34
Richard Shann <richard.shann@virgin.net>
2016-09-29 16:45:31 +0100

    Use purpose built interface for transpose

--- actions/menus/ObjectMenu/Score/Typesetter/TransposeOnPrint.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/TransposeOnPrint.scm
@@ -1,43 +1,7 @@
 ;;;TransposeOnPrint
 (define-once Transpose::Interval #f)
-(let ((transpose "Transpose")(cancel "Cancel") 
-	(instruction (_ "⬅Edit the interval and click Transpose button above"))
-	(action (RadioBoxMenu (cons (_ "Transpose Score") "TransposeScorePrint") (cons (_ "Transpose Staff") "TransposeStaffPrint"))))
-	(if action
-	    (begin
-	    (d-NewWindow)
-	    (d-HideMenus)
-	    (if  (string? Transpose::Interval)
-		(let ((thenotes (string-split Transpose::Interval #\space)))
-		    (d-Insert2)
-		    (d-PutNoteName (car thenotes))
-		    (d-Insert2)
-		    (d-PutNoteName (cadr thenotes)))
-		 (begin
-		    (d-C) 
-		    (d-E) 
-		    (d-Flatten))) ;;;c ees, arbitrary choice.
-	    
-	    (d-DirectivePut-standalone-graphic "Instruction" (string-append "\n" instruction "\nDenemo 24"))
-	    (d-MoveCursorLeft)(d-MoveCursorLeft)
-	    (CreateButton transpose (string-append " <span font_desc=\"36\" foreground=\"blue\">" (_ "Transpose")"</span>"))
-	    (d-SetDirectiveTagActionScript transpose 
-		(string-append "(let ((interval #f))
-		    (d-MoveToBeginning)
-		    (set! interval (d-GetNote))
-		    (d-MoveCursorRight) 
-		    (set! interval (string-append interval \" \" (d-GetNote)))
-		    (d-SetSaved #t)
-		    (d-Close)
-		    (d-HideMenus #f)
-		     (d-ToggleReduceToDrawingArea)
-		    (d-ToggleReduceToDrawingArea)
-		    (d-" action " interval))"))
-		
-	    (CreateButton cancel (string-append " <span font_desc=\"36\" foreground=\"black\">" (_ "Cancel")"</span>"))
-	    (d-SetDirectiveTagActionScript cancel 
-		" (d-SetSaved #t)
-		    (d-Close)
-		     (d-HideMenus #f)
-		    (d-ToggleReduceToDrawingArea)
-		    (d-ToggleReduceToDrawingArea)"))))
+(let ((action (RadioBoxMenu (cons (_ "Transpose Score") d-TransposeScorePrint) (cons (_ "Transpose Staff") d-TransposeStaffPrint))))
+    (if action
+        (let ((interval (d-GetNoteNamesFromUser 2 (_ "<-- Transpose to -->") Transpose::Interval)))
+            (if interval
+                (action interval)))))
