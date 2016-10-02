4a3c97e9e63ac9a2f34bfe0b1bc0529e090cec7f
Richard Shann <richard.shann@virgin.net>
2016-10-02 20:41:45 +0100

    Make free segno positionable

--- actions/menus/ObjectMenu/Directives/Markings/FreeSegno.scm
+++ actions/menus/ObjectMenu/Directives/Markings/FreeSegno.scm
@@ -1,4 +1,4 @@
-;;;Segno attached to the next object, for example a barline. But shown between notes
+;;;Segno  symbol attached to the next object, for example a barline. But shown between notes
 (let ((tag "FreeSegno"))
    (if (d-Directive-standalone? tag)
          (let ((choice (RadioBoxMenu 
@@ -8,7 +8,12 @@
                 ((help)
                    (d-DisplayCurrentObject))
                   ((delete)
-                    (d-DirectiveDelete-standalone tag))))  
-    (StandAloneDirectiveProto (cons tag "\\tweak break-visibility  #begin-of-line-invisible \\mark \\markup\\scale #'(0.75 . 0.75) { \\musicglyph #\"scripts.segno\" } ") #t LG-Segno "" )))
+                    (d-DirectiveDelete-standalone tag))))
+     (let ((choice (RadioBoxMenu
+     		(cons (_ "Show if at end of line") "begin")
+     		(cons	(_ "Show if at start of line")  "end"))))
+     		(if choice  
+    		(StandAloneDirectiveProto (cons tag 
+    		(string-append "\\tweak break-visibility  #" choice "-of-line-invisible \\mark \\markup\\scale #'(0.75 . 0.75) { \\musicglyph #\"scripts.segno\" } ")) #t LG-Segno "" )))))
 
         
