09df742a99613ae61922535ee865c327355cd4f5
Richard Shann <richard.shann@virgin.net>
2016-10-02 20:51:51 +0100

    Add short indent command

---
+++ actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/ScoreShortIndent.scm
@@ -0,0 +1,17 @@
+;;;; ScoreShortIndent Set indent
+(let ((tag "ScoreShortIndent")(amount #f) (current "0.0")(thematch #f)(params ScoreShortIndent::params))
+  (set! current (d-DirectiveGet-score-data tag))
+  (if (not current)
+      (set! current "4.0"))
+  (if (and params (not (equal? params "edit")))
+    (set! amount (number->string params))
+    (set! amount (d-GetUserInput (_ "Choose indent of subsequent systems") (_ "Give amount in decimal") current)))
+
+  (if (and (string? amount) (string->number amount))
+    (begin
+        (d-DirectivePut-score-prefix tag (string-append "\\layout {short-indent = " amount "}\n"))
+        (d-DirectivePut-score-override tag DENEMO_OVERRIDE_GRAPHIC)
+        (d-DirectivePut-score-data tag amount)
+        (d-DirectivePut-score-display tag (string-append (_ "short indent=") amount)))
+    (d-DirectiveDelete-score tag))
+  (d-SetSaved #f))
