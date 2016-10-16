0f4218cd99a65898d448fd1d07131f05306e0c14
Richard Shann <richard.shann@virgin.net>
2016-10-16 14:53:08 +0100

    Make direction of transpose clear

--- actions/menus/ObjectMenu/Score/Typesetter/TransposeOnPrint.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/TransposeOnPrint.scm
@@ -2,6 +2,6 @@
 (define-once Transpose::Interval #f)
 (let ((action (RadioBoxMenu (cons (_ "Transpose Score") d-TransposeScorePrint) (cons (_ "Transpose Staff") d-TransposeStaffPrint))))
     (if action
-        (let ((interval (d-GetNoteNamesFromUser 2 Transpose::Interval (_ "<-- Transpose to -->") )))
+        (let ((interval (d-GetNoteNamesFromUser 2 Transpose::Interval (_ "--- Transpose to -->") )))
             (if interval
                 (action interval)))))
