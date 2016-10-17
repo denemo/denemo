7bacb173177c315719e88bd4e127d1387d9b7e3c
Richard Shann <richard.shann@virgin.net>
2016-10-17 20:43:34 +0100

    Fix escaping of scheme so editing works

--- actions/menus/ObjectMenu/MovementMenu/Titles/MarkupAtEnd.scm
+++ actions/menus/ObjectMenu/MovementMenu/Titles/MarkupAtEnd.scm
@@ -1,11 +1,14 @@
 ;;;MarkupAtEnd
 (let* ((tag "MarkupAtEnd")(themarkup #f)(data (d-DirectiveGet-movementcontrol-data tag)))
+(disp "eneter with " data "\n\n")
         (d-DirectivePut-movementcontrol-override tag (logior
             DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_TAGEDIT))
         (d-DirectivePut-movementcontrol-display tag (_ "Markup At End"))
         (if data
         (set! data (eval-string data))
         (set! data '()))
+        
+        (disp "so data is " data "\n\n")
         (set! themarkup (assq-ref data 'text))
         (if (not themarkup)
             (set! themarkup ""))
@@ -13,7 +16,7 @@
         (if themarkup
             (begin
                 (set! data 
-                (string-append "(list (cons 'text \" "  (car themarkup)  "\"))"))
+                (string-append "(list (cons 'text \" "  (scheme-escape (car themarkup))  "\"))"))
                 (d-DirectivePut-movementcontrol-data tag  data)
                 (set! themarkup (cdr themarkup))
                 (d-DirectivePut-movementcontrol-postfix tag (string-append "\\markup \\column { " themarkup " }"))
