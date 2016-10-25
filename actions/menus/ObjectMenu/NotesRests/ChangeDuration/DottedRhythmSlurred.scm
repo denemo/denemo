c358c264ae972db3f2031db9289d4d78ed2323f9
Richard Shann <richard.shann@virgin.net>
2016-10-25 10:28:32 +0100

    Add sound effect to slurred rhythm commands

--- actions/menus/ObjectMenu/NotesRests/ChangeDuration/DottedRhythmSlurred.scm
+++ actions/menus/ObjectMenu/NotesRests/ChangeDuration/DottedRhythmSlurred.scm
@@ -8,6 +8,7 @@
         (eval-string (string-append "(d-" (number->string (+ 1 duration)) ")"))
         (d-MoveCursorLeft)
         (d-ToggleEndSlur)
+        (d-PlayMidiNote 53 80 9 10)
         (if (and nonprinting (not (d-GetMarkedMidiNote)))
-        	(d-SetNonprinting))
+            (d-SetNonprinting))
         (d-MoveCursorRight)))))
--- actions/menus/ObjectMenu/NotesRests/EditDuration/Slurred/init.scm
+++ actions/menus/ObjectMenu/NotesRests/EditDuration/Slurred/init.scm
@@ -3,13 +3,14 @@
   (begin
     (if (Note?)
       (begin ;;; there is a note before, if it is end slur then remove that, else start slur
-	(if (d-IsSlurEnd)
-	  (begin
-	    (d-ToggleEndSlur))
-	  (begin ;;; there is a note before it is to be the start of the slurred notes	    
-	    (d-ToggleBeginSlur)))
-	(d-NextChord)
-	(d-ToggleEndSlur))
-	(begin ;;; there is a rest before
-	  (d-NextChord)))
-	(d-MoveCursorRight))))
+    (if (d-IsSlurEnd)
+      (begin
+        (d-ToggleEndSlur))
+      (begin ;;; there is a note before it is to be the start of the slurred notes      
+        (d-ToggleBeginSlur)))
+    (d-NextChord)
+    (d-PlayMidiNote 53 80 9 10)
+    (d-ToggleEndSlur))
+    (begin ;;; there is a rest before
+      (d-NextChord)))
+    (d-MoveCursorRight))))
