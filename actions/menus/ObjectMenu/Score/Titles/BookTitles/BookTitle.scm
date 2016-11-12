1924cb562489c9dabf05049430e261e633a27987
Richard Shann <richard.shann@virgin.net>
2016-11-12 18:20:37 +0000

    Ask for data in expected order

--- actions/menus/ObjectMenu/Score/Titles/BookTitles/BookTitle.scm
+++ actions/menus/ObjectMenu/Score/Titles/BookTitles/BookTitle.scm
@@ -5,14 +5,16 @@
     (if BookTitle::params
         (BookTitles::Do "Title" "title" BookTitle::params  #f)
         (let ((space (d-DirectiveGet-score-data tag)))
-            (if (not space)
-                (set! space "0"))
-            (set! space (DenemoGetUserNumberAsString (_ "Space Above Title") (_ "Give amount of space at top of title page") space))
-            (if (not space)
-            	(set! space "0"))
            (d-LilyPondInclude (cons 'delete "simplified-book-titling.ily"))
+           (BookTitles::Do "Title" "title" (_ "My Title")  (_ "Give a title for the whole score or blank out to delete: "))
+           
+           (if (not space)
+                (set! space "0"))
+           (set! space (DenemoGetUserNumberAsString (_ "Space Above Title") (_ "Give amount of space at top of title page") space))
+           (if (not space)
+                (set! space "0"))
+           (d-SetSaved #f)
            (d-DirectivePut-score-prefix tag (string-append "#(define denemo-top-margin " space ")\n"))
            (d-DirectivePut-score-data tag space)
-           (d-DirectivePut-score-override tag DENEMO_OVERRIDE_AFFIX)
-           (BookTitles::Do "Title" "title" (_ "My Title")  (_ "Give a title for the whole score or blank out to delete: ")))))
+           (d-DirectivePut-score-override tag DENEMO_OVERRIDE_AFFIX))))
 
