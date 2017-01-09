65a33a229a723183d05820c1e9a7cbfa71766a0f
Richard Shann <richard.shann@virgin.net>
2017-01-09 11:41:46 +0000

    adjust notes/rests menu order

---
+++ actions/menus/ObjectMenu/NotesRests/StemControl/HelpForStemControl.scm
@@ -0,0 +1,2 @@
+;;;HelpForStemControl
+(d-InfoDialog (_ "Stem direction (up, down or automatic - depending on the note height on the staff- is normally controlled by selecting the LilyPond voice for the music on the staff. This menu is for primitive commands that force stems in one direction without altering slur, tie, ornament directions. Use right-click More Commands to un-hide these primitives if needed."))
