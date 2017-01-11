e24647755b240401fcdb6ae7b83905ee5df9e2fb
Richard Shann <richard.shann@virgin.net>
2017-01-11 14:25:40 +0000

    Avoid clutter on Ligature markings

--- actions/menus/ObjectMenu/Directives/Markings/Spanning/LigatureEnd.scm
+++ actions/menus/ObjectMenu/Directives/Markings/Spanning/LigatureEnd.scm
@@ -3,6 +3,7 @@
     (StandAloneDirectiveProto (cons tag "\\]  ") #f)
     (d-DirectivePut-standalone-graphic tag "\n]\nDenemo\n40")
     (d-DirectivePut-standalone-gy tag 10)
+    (d-DirectivePut-standalone-override tag DENEMO_OVERRIDE_GRAPHIC)
     (d-DirectivePut-standalone-display tag (_ "Ligature End"))
     (d-RefreshDisplay)
     (d-SetSaved #f))
