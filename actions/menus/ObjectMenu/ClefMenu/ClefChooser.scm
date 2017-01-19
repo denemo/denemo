fe548592131e4f42bfc98a0ba9f174695eeb3b56
Richard Shann <richard.shann@virgin.net>
2017-01-19 16:38:58 +0000

    Get Drum Clef in right place

--- actions/menus/ObjectMenu/ClefMenu/ClefChooser.scm
+++ actions/menus/ObjectMenu/ClefMenu/ClefChooser.scm
@@ -11,35 +11,36 @@
                           (cons (_ "LilyPond") 'lilypond))))
 
     (if choice
-    	(begin
-		(case choice
-		    ((lilypond)
-		        (let ((clef (d-GetUserInput (_ "Custom Clef") (_ "Give LilyPond syntax for clef name") "treble^8"))  (tag "ClefChooser"))
-		            (if clef
-		                (begin
-		                    (d-Directive-standalone tag)
-		                    (d-DirectivePut-standalone-postfix tag (string-append "\\clef \"" clef "\" "))
-		                    (d-DirectivePut-standalone-graphic tag "\nC\nDenemo\n24")
-		                    (d-DirectivePut-standalone-minpixels tag 50)
-		                    (d-InfoDialog (_ "N.B.The Denemo display will show notes using prevailing Denemo clef,\nbut they will be typeset in the clef given"))))))
-		    ((drum)
-		        (d-InitialClef "Bass")
-		        (d-DirectivePut-clef-override "DrumClef" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
-		        (d-DirectivePut-clef-postfix "DrumClef" "\\clef percussion\n ")
-		        (d-DirectivePut-clef-graphic "DrumClef" "DrumClef")
-		        (d-StaffProperties "midi_channel=9")
-		        (d-DirectivePut-voice-display "DrumClef" (_ "Drum Clef"))
-		        (d-DirectivePut-voice-override "DrumClef" DENEMO_OVERRIDE_GRAPHIC)
-		        (d-DirectivePut-voice-prefix "DrumClef" "\\new Voice \\with { middleCPosition = #6 }"))
-		    (else
-		    	(d-DirectiveDelete-voice "DrumClef")
-		    	(d-DirectiveDelete-clef "DrumClef")
-		        (if (d-MoveCursorLeft)
-		            (begin
-		            (d-MoveCursorRight)
-		            (d-InsertClef choice))
-		            (begin
-		                (if (equal? (d-DirectiveGetTag-clef ) "DrumClef" ) (d-DirectiveDelete-clef "DrumClef") )
-		                (d-InitialClef choice)))))
-             		(d-SetSaved #f)           
-            		(d-RefreshDisplay))))
+        (begin
+        (case choice
+            ((lilypond)
+                (let ((clef (d-GetUserInput (_ "Custom Clef") (_ "Give LilyPond syntax for clef name") "treble^8"))  (tag "ClefChooser"))
+                    (if clef
+                        (begin
+                            (d-Directive-standalone tag)
+                            (d-DirectivePut-standalone-postfix tag (string-append "\\clef \"" clef "\" "))
+                            (d-DirectivePut-standalone-graphic tag "\nC\nDenemo\n24")
+                            (d-DirectivePut-standalone-minpixels tag 50)
+                            (d-InfoDialog (_ "N.B.The Denemo display will show notes using prevailing Denemo clef,\nbut they will be typeset in the clef given"))))))
+            ((drum)
+                (d-InitialClef "Bass")
+                (d-DirectivePut-clef-override "DrumClef" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+                (d-DirectivePut-clef-postfix "DrumClef" "\\clef percussion\n ")
+                (d-DirectivePut-clef-graphic "DrumClef" "DrumClef")
+                (d-DirectivePut-clef-gy "DrumClef" -5)
+                (d-StaffProperties "midi_channel=9")
+                (d-DirectivePut-voice-display "DrumClef" (_ "Drum Clef"))
+                (d-DirectivePut-voice-override "DrumClef" DENEMO_OVERRIDE_GRAPHIC)
+                (d-DirectivePut-voice-prefix "DrumClef" "\\new Voice \\with { middleCPosition = #6 }"))
+            (else
+                (d-DirectiveDelete-voice "DrumClef")
+                (d-DirectiveDelete-clef "DrumClef")
+                (if (d-MoveCursorLeft)
+                    (begin
+                    (d-MoveCursorRight)
+                    (d-InsertClef choice))
+                    (begin
+                        (if (equal? (d-DirectiveGetTag-clef ) "DrumClef" ) (d-DirectiveDelete-clef "DrumClef") )
+                        (d-InitialClef choice)))))
+                    (d-SetSaved #f)           
+                    (d-RefreshDisplay))))
