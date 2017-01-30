3e125dc3cc88a1bc4d1edd77e02c19c1af346fdc
Richard Shann <richard.shann@virgin.net>
2017-01-30 16:43:06 +0000

    Fix syntax for lyric syllable alignment

--- actions/menus/ObjectMenu/Lyrics/InsertLyricAlignment.scm
+++ actions/menus/ObjectMenu/Lyrics/InsertLyricAlignment.scm
@@ -1,4 +1,4 @@
 ;InsertLyricAlignment
 (let ((text  (RadioBoxMenu (cons (_ "Center") "CENTER") (cons (_ "Left") "LEFT")(cons (_ "Right") "RIGHT"))))
        (if text 
-            (d-InsertTextInVerse (string-append "\n\\override LyricText.self-alignment-X = #" text  "%{" (_ "Note: start lyrics on a new line") " %}\n"))))
+            (d-InsertTextInVerse (string-append "\n\\override LyricText.self-alignment-X = #" text  " %{" (_ "Note: start lyrics on a new line") " %}\n"))))
