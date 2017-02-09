7f61b444616335f0b8dd2d5d7676d6de03e3d171
Richard Shann <richard.shann@virgin.net>
2017-02-09 15:02:42 +0000

    Fix Melismata command

--- actions/menus/ObjectMenu/Lyrics/InsertMelisma.scm
+++ actions/menus/ObjectMenu/Lyrics/InsertMelisma.scm
@@ -1,5 +1,5 @@
 ;InsertMelisma
-(let ((choice (RadioBoxMenu (cons (_ "Split Word") " -- ") (cons (_ "Extend Word") " __ "))))
+(let ((choice (RadioBoxMenu (cons (_ "During Word") " -- _ ") (cons (_ "End of Word") " __ _ ")    (cons (_ "Extend") " _ "))))
  (if choice
     (begin
         (d-InsertTextInVerse choice)
