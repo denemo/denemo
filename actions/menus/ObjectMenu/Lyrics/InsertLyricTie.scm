4def1c6f099ad2e53771bb277f31ab6eef187e34
Richard Shann <richard.shann@virgin.net>
2017-02-10 15:12:18 +0000

    Upgrade Melisma and Lyric tie commands from JW

--- actions/menus/ObjectMenu/Lyrics/InsertLyricTie.scm
+++ actions/menus/ObjectMenu/Lyrics/InsertLyricTie.scm
@@ -1,6 +1,8 @@
 ;InsertLyricTie
-(let ((choice (RadioBoxMenu (cons (_ "Printing") "~") (cons (_ "Non Printing") "_"))))
- (if choice
-    (begin
-        (d-InsertTextInVerse choice)
-        (d-SetSaved #f))))
+(let ((choice (RadioBoxMenu
+(cons (_ "Printed as \"ˬ\" (lyric tie symbol)") "~")
+(cons (_ "Printed as space") "_"))))
+  (if choice
+     (begin
+         (d-InsertTextInVerse choice)
+         (d-SetSaved #f))))
--- actions/menus/ObjectMenu/Lyrics/InsertMelisma.scm
+++ actions/menus/ObjectMenu/Lyrics/InsertMelisma.scm
@@ -1,6 +1,11 @@
 ;InsertMelisma
-(let ((choice (RadioBoxMenu (cons (_ "During Word") " -- _ ") (cons (_ "End of Word") " __ _ ")    (cons (_ "Extend") " _ "))))
- (if choice
-    (begin
-        (d-InsertTextInVerse choice)
-        (d-SetSaved #f))))
+(let ((choice (RadioBoxMenu
+	(cons (_ "During Word, with Slur") " -- ")
+	(cons (_ "Word End, with Slur") " __ ")
+	(cons (_ "During Word, no Slur") " -- _ ")
+	(cons (_ "Word End, no Slur") " __ _ ")
+	(cons (_ "Extend Melisma") " _ "))))
+  (if choice
+     (begin
+         (d-InsertTextInVerse choice)
+         (d-SetSaved #f))))
