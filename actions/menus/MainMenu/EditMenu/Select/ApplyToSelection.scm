78d026979aa57278a3f7ff57d9715c093aa171ce
Richard Shann <richard.shann@virgin.net>
2017-02-24 09:46:20 +0000

    Use sa fer apply to selection command

--- actions/menus/MainMenu/EditMenu/Select/ApplyToSelection.scm
+++ actions/menus/MainMenu/EditMenu/Select/ApplyToSelection.scm
@@ -3,5 +3,4 @@
 	(d-WarningDialog "Close this dialog, then press a shortcut key/shortcut keys to apply the command to every object in the selection\n\nThis command will execute the command you give once with the cursor on  each object in the selection. E.g. if you select 8 notes and give a shortcut for ZoomIn it will zoom 8 times."))
 (let ()
 	(define var (d-GetCommandFromUser))
-	(if var (SingleAndSelectionSwitcher (string-append "(d-" var ")")))
-)
+	(if var (ApplyToTaggedSelection  (eval-string (string-append "d-" var)))))
