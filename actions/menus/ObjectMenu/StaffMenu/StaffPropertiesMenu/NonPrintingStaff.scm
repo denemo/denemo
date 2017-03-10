2ab73917e1d44d74e919676673ca0c6db87711e6
Richard Shann <richard.shann@virgin.net>
2017-03-10 10:41:24 +0000

    Fix bug #50486: Chords staff ignores Non Printing command

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/NonPrintingStaff.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/NonPrintingStaff.scm
@@ -12,7 +12,8 @@
 					(d-InfoDialog (_ "Staff will be printed")))
 				(d-DirectiveDelete-staff tag))
 			(begin 
-				(d-DirectivePut-staff-prefix tag " \\void ")
+				(d-DirectivePut-staff-prefix tag " \\void { ")
+				(d-DirectivePut-staff-postfix tag " }\n ")
 				(d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE DENEMO_OVERRIDE_GRAPHIC))
 				(d-DirectivePut-staff-display tag "Hidden Staff")
 				(if (not params)
