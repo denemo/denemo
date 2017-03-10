cbf60c865fa634e06c79ecd76208a71df7087a79
Richard Shann <richard.shann@virgin.net>
2017-03-10 10:41:52 +0000

    Leave Default layout unchanged when creating selection of staffs as layout

--- actions/menus/MainMenu/FileMenu/PrintMenu/NonPrintingStaffs.scm
+++ actions/menus/MainMenu/FileMenu/PrintMenu/NonPrintingStaffs.scm
@@ -1,5 +1,21 @@
 ;;;NonPrintingStaffs
-(let ((tag "NonPrintingStaff") (ptr #f)(allmovements #f) (layout #f)(numstaffs (d-GetStaffsInMovement)) (thelist '()))
+(let ((tag "NonPrintingStaff") (ptr #f)(allmovements #f) (layout #f)(numstaffs (d-GetStaffsInMovement)) (initial-list '()) (thelist '()))
+(define (SetNonPrintingStaffs stafflist)
+    	(let movement-loop ()
+    		 (set! ptr stafflist);(disp "Setting to " ptr "\n")
+		(d-MoveToMovementBeginning) 
+		(let loop ()
+		    (if  (cdar ptr)
+		        (d-NonPrintingStaff 'unset)
+		        (d-NonPrintingStaff 'set))
+		    (set! ptr (cdr ptr)) 
+		    (if (d-MoveToStaffDown)
+		        (loop)
+		        (if (and allmovements (d-NextMovement))
+		        	(movement-loop))))))
+
+
+
    (d-SelectDefaultLayout)   
     (d-PushPosition)
     (d-MoveToMovementBeginning)
@@ -9,6 +25,7 @@
             (loop)))
  
  (set! thelist (reverse thelist)) 
+ (set! initial-list (reverse thelist))
   (set! thelist (cons (cons (_ "Do Not Create Layout for this selection") #t) thelist))
  (set! thelist (cons (cons (_ "Apply to All Movements") #t) thelist))
  
@@ -22,24 +39,19 @@
     (begin
     	(if allmovements
     		(while (d-PreviousMovement)))
-    	(let movement-loop ()
-    		 (set! ptr thelist)
-		(d-MoveToMovementBeginning) 
-		(let loop ()
-		    (if  (cdar ptr)
-		        (d-NonPrintingStaff 'unset)
-		        (d-NonPrintingStaff 'set))
-		    (set! ptr (cdr ptr)) 
-		    (if (d-MoveToStaffDown)
-		        (loop)
-		        (if (and allmovements (d-NextMovement))
-		        	(movement-loop)))))
+       (SetNonPrintingStaffs thelist)
+       (d-RefreshLilyPond)
+      ; (disp "Now we have " thelist "and " initial-list "for layout " layout "\n")
     	(if (not layout)
     		(let ((name (d-GetUserInput (_ "Creating Layout") (_ "Give Layout Name") (_ "Woodwind"))))
     			(if (and name (positive? (string-length name)))
     				(begin
     					(d-CreateLayout name)
-    					(d-SelectDefaultLayout))))))
+    					(if allmovements
+    						(while (d-PreviousMovement)))
+       					(SetNonPrintingStaffs initial-list)
+    					(d-SelectDefaultLayout)
+    					(d-WarningDialog (string-append (_ "Use Typeset->") name (_ " in the Print View to typeset your new layout"))))))))
         (d-WarningDialog (_ "Cancelled")))
     (d-PopPosition))
   
