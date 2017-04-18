9a8cfe9ee3e5da3419519cddfd1680de578dc975
Richard Shann <richard.shann@virgin.net>
2017-04-18 14:49:18 +0100

    Add tweak for figured bass typesetting

---
+++ actions/menus/ObjectMenu/NotesRests/Markings/FiguredBass/FiguredBassTweak.scm
@@ -0,0 +1,86 @@
+(let ((tag "FiguredBassTweak"))
+(d-DirectivePut-score-prefix tag 
+"#(define-public (format-bass-figures figure event context)
+  (let* ((fig (ly:event-property event 'figure))
+         (fig-markup (if (number? figure)
+
+                         ;; this is not very elegant, but center-aligning
+                         ;; all digits is problematic with other markups,
+                         ;; and shows problems in the (lack of) overshoot
+                         ;; of feta-alphabet glyphs.
+                         ((if (<= 10 figure)
+                              (lambda (y) (make-translate-scaled-markup
+                                           (cons -0.7 0) y))
+                              identity)
+
+                          (cond
+                           ((eq? #t (ly:event-property event 'diminished))
+                            (markup #:slashed-digit figure))
+                           ((eq? #t (ly:event-property event 'augmented-slash))
+                            (markup #:backslashed-digit figure))
+                           (else (markup #:number (number->string figure 10)))))
+                         #f))
+
+         (alt (ly:event-property event 'alteration))
+         (alt-markup
+          (if (number? alt)
+              (markup
+               #:general-align Y DOWN #:fontsize
+               (if (not (= alt DOUBLE-SHARP))
+                   ;-2 2 ;; is the default
+                    (if (null? figure) 0 -2) 4 ;; changed
+                 )
+               (alteration->text-accidental-markup alt))
+              #f))
+
+         (plus-markup (if (eq? #t (ly:event-property event 'augmented))
+                          (markup #:number \"+\")
+                          #f))
+
+         (alt-dir (ly:context-property context 'figuredBassAlterationDirection))
+         (plus-dir (ly:context-property context 'figuredBassPlusDirection)))
+
+    (if (and (not fig-markup) alt-markup)
+        (begin
+          (set! fig-markup (markup #:left-align #:pad-around 0.3 alt-markup))
+          (set! alt-markup #f)))
+
+
+    ;; hmm, how to get figures centered between note, and
+    ;; lone accidentals too? this code centers on whole notes but is too far to right on shorter ones
+
+    ;;    (if (markup? fig-markup)
+    ;;  (set!
+    ;;   fig-markup (markup #:translate (cons 1.0 0)
+    ;;                      #:center-align fig-markup)))
+
+    (if alt-markup
+        (set! fig-markup
+              (markup #:put-adjacent
+                      X (if (number? alt-dir)
+                            alt-dir
+                            LEFT)
+                      fig-markup
+                      #:pad-x 0.2 #:raise (if (= alt SHARP) -0.1 0.1) alt-markup)))
+
+    (if plus-markup
+        (set! fig-markup
+              (if fig-markup
+                  (markup #:put-adjacent
+                          X (if (number? plus-dir)
+                                plus-dir
+                                LEFT)
+                          fig-markup
+                          #:pad-x 0.2 plus-markup)
+                  plus-markup)))
+
+    (if (markup? fig-markup)
+        (markup #:fontsize -2 fig-markup)
+        empty-markup)))
+\\layout {
+  \\context {
+    \\Score
+    figuredBassFormatter = #format-bass-figures
+  }
+} "))
+(d-SetSaved #f)
