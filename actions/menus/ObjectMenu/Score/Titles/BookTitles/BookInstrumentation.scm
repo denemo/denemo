5d1101096e780cdfaa7f44da9aa21ef9bc5a0100
Richard Shann <richard.shann@virgin.net>
2017-05-19 17:31:07 +0100

    Fix bug #51055 for Instrumentation

--- actions/menus/ObjectMenu/Score/Titles/BookTitles/BookInstrumentation.scm
+++ actions/menus/ObjectMenu/Score/Titles/BookTitles/BookInstrumentation.scm
@@ -1,4 +1,5 @@
 ;;BookInstrumentation
-(if BookInstrumentation::params
-    (BookTitles::Do "Instrumentation" "instrumentation" BookInstrumentation::params #f #f)
-        (BookTitles::Do "Instrumentation" "instrumentation" "Full Score"  (_ "Give instrumentation for the default layout or blank out to delete: ") #f))
+(let ((edit (d-Directive-scoreheader? "BookInstrumentation")))
+    (if BookInstrumentation::params
+    (BookTitles::Do "Instrumentation" "instrumentation" BookInstrumentation::params #f edit)
+        (BookTitles::Do "Instrumentation" "instrumentation" "Full Score"  (_ "Give instrumentation for the default layout or blank out to delete: ") edit)))
