01e5c7a8f0f1263e5b4b95a2c5091291625fccb1
Richard Shann <richard.shann@virgin.net>
2017-06-25 12:56:22 +0100

    Fix to just ignore one warning - should be upgraded to do multiple warnings by repeating the syntax

--- actions/menus/ObjectMenu/StaffMenu/Voices/Part-Combining/IgnoreClashingNoteColumns.scm
+++ actions/menus/ObjectMenu/StaffMenu/Voices/Part-Combining/IgnoreClashingNoteColumns.scm
@@ -5,7 +5,8 @@
 			(d-DirectiveDelete-score tag)
 			(d-InfoDialog (_ "Clashing Note Columns will generate a warning from the LilyPond typesetter")))
 		(begin
-			(d-DirectivePut-score-prefix tag "\\layout {\\override NoteColumn #'ignore-collision = ##t }\n")
+			;(d-DirectivePut-score-prefix tag "\\layout {\\override NoteColumn #'ignore-collision = ##t }\n")
+			(d-DirectivePut-score-prefix tag "\n#(ly:expect-warning \"ignoring too many clashing note columns\")\n")
 			(d-DirectivePut-score-display tag (_ "Ignore Clashing Columns"))
 			(d-DirectivePut-score-override tag 4)))
 	(d-SetSaved #f))
