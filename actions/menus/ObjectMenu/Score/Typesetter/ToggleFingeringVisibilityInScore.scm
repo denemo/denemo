c04a31ba513de7441ef103e60308788edd6452ea
Richard Shann <richard.shann@virgin.net>
2017-06-25 10:17:43 +0100

    Hide Fingering as directive

--- actions/menus/ObjectMenu/Score/Typesetter/ToggleFingeringVisibilityInScore.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/ToggleFingeringVisibilityInScore.scm
@@ -1,2 +1,10 @@
 ;;;ToggleFingeringVisibilityInScore
-(ForAllObjectsInScoreExecute (lambda () (ForAllNotesInChordExecute (lambda () (d-ToggleFingeringVisibility 'noninteractive)))))
+(let ((tag "ToggleFingeringVisibilityInScore"))
+	(if (d-Directive-score? tag)
+		(begin
+			(d-DirectiveDelete-score tag)
+			(d-InfoDialog (_ "Fingerings will be typeset")))
+		(begin
+			(d-DirectivePut-score-prefix tag "\n\\layout { \\omit Fingering }\n")
+			(d-InfoDialog (_ "Fingerings will be not typeset")))))
+(d-SetSaved #f)
