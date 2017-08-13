64b33c6940fe0635be800cf1877f1412ee806ec2
Richard Shann <richard@rshann.plus.com>
2017-08-13 16:46:55 +0100

    Fix bug #51744: Ambitus does not work in connection with instrument name

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Ambitus.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Ambitus.scm
@@ -1,1 +1,1 @@
-(ToggleDirective "staff" "prefix" "Ambitus" "\\with { \\consists \"Ambitus_engraver\" }")
+(ToggleDirective "staff" "prefix" "Ambitus" " \\consists \"Ambitus_engraver\" " DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC )
