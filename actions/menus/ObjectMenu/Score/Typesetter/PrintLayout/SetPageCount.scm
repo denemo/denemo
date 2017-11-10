ba268bd0b003caedcaa936ac8a88fd1453dcbbd7
Richard Shann <richard@rshann.plus.com>
2017-11-10 17:14:09 +0000

    Do not allow zero pages

--- actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/SetPageCount.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/SetPageCount.scm
@@ -1,6 +1,6 @@
 ;;;SetPageCount
 (let ((tag "SetPageCount")(count (d-GetUserInput (_ "Total Page Count") (_ "Give Pages Required\n(0 for optimal): ") "4")))
-	(if (and count (string->number count))
+	(if (and count (string->number count) (> (string->number count) 0))
 		(begin
 			(d-DirectivePut-score-prefix tag (string-append "\\paper { page-count=" count "}"))
 			(d-DirectivePut-score-display tag (string-append (_ "Page Count") count))
