358439ddf919812543d80fec4c26a2f1bb9e92f9
Richard Shann <richard@rshann.plus.com>
2017-12-07 15:10:56 +0000

    Fix problems with ends of selection in percent repeat script

--- actions/menus/ObjectMenu/NotesRests/InsertNote/PercentRepeat.scm
+++ actions/menus/ObjectMenu/NotesRests/InsertNote/PercentRepeat.scm
@@ -4,14 +4,11 @@
     (d-InfoDialog (_ "This Denemo Directive is part of a set of four creating a \"Percent Repeat\". Be sure to delete them all if you delete one of them."))
     (if (d-IsInSelection)
         (let ((tag "PercentRepeat") (start-measurenum #f)(num_repeats (d-GetUserInput (_ "Percent Repeat") (_ "Give number of further repeats 1,2,...") "1")))
-            (d-GoToMark)
-            (set! start-measurenum (d-GetMeasure))
+         
             (MoveToEndOfSelection)
-            (set! start-measurenum (not (= start-measurenum (d-GetMeasure))))
-            
             
             (if num_repeats
-                (let ((numbering  (if (and (FullDurationMeasure?)   start-measurenum) (d-GetUserInput (_ "Percent Repeat") (_ "Give frequency of numbering 0, 1,...") "1") #f))
+                (let ((numbering  (if (FullDurationMeasure?)  (d-GetUserInput (_ "Percent Repeat") (_ "Give frequency of numbering 0, 1,...") "1") #f))
                         (freq "\\set countPercentRepeats = ##f "))
                     (if numbering
                         (begin
@@ -22,16 +19,16 @@
                     (set! num_repeats (string->number num_repeats))
                     (d-Copy)
                     (d-PushPosition)
-                    (d-GoToMark)
+                    (MoveToSelectionBeginningInThisStaff)
                     (d-Directive-standalone tag)
                     (d-DirectivePut-standalone-minpixels tag 40)
                     (d-DirectivePut-standalone-postfix tag (string-append freq "\\repeat percent " (number->string (1+ num_repeats)) "{ "))
                     (d-DirectivePut-standalone-display tag (string-append "% " (number->string (1+ num_repeats))))
                     (d-DirectivePut-standalone-graphic tag (string-append "\nR{\nDenemo\n36"))
                     (d-DirectivePut-standalone-gy tag 10)
-                    (d-PopPosition)
-                    (if (not start-measurenum)
-                    	(d-MoveCursorRight))  ;;we have inserted one object so when in the same measure we move right
+                    (d-PopPosition) ;;moves to where selection end was
+                    
+                    (d-MoveCursorRight) ;;we have inserted one object so when in the same measure we move right
                     (d-MoveCursorRight)
                     (d-Directive-standalone tag)
                     (d-DirectivePut-standalone-minpixels tag 60)
