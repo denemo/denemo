cfc71a07a9cc032669d1c00f940870fc282dee30
Richard Shann <richard@rshann.plus.com>
2018-07-19 16:16:07 +0100

    Allow Unmeasured Staff to be undone

--- actions/menus/ObjectMenu/TimeSig/Unmeasured.scm
+++ actions/menus/ObjectMenu/TimeSig/Unmeasured.scm
@@ -1,7 +1,12 @@
 ;Unmeasured
 (let ((tag "Unmeasured"))
-            (d-InitialTimeSig "256/1")
-            (d-DirectivePut-timesig-postfix tag "\\cadenzaOn ")
-             (d-DirectivePut-timesig-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
-            (d-DirectivePut-timesig-graphic tag "\n \n")
+	(if (d-Directive-timesig? tag)
+		(begin (disp "Delet")
+			(d-DirectiveDelete-timesig tag)
+			(d-InitialTimeSig))
+		(begin
+		    (d-InitialTimeSig "256/1")
+		    (d-DirectivePut-timesig-postfix tag "\\cadenzaOn ")
+		     (d-DirectivePut-timesig-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+		    (d-DirectivePut-timesig-graphic tag "\n \n")))
             (d-SetSaved #f))
