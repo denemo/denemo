aa04f5f35017f7256f17da1b4c37e2e991480603
Richard Shann <richard@rshann.plus.com>
2018-08-27 09:31:37 +0100

    Allow better toggling of hide empty staffs

--- actions/menus/ObjectMenu/MovementMenu/Typesetter/HideEmptyStaffsAllSystems.scm
+++ actions/menus/ObjectMenu/MovementMenu/Typesetter/HideEmptyStaffsAllSystems.scm
@@ -1,15 +1,16 @@
 ;;;HideEmptyStaffsAllSystems
 (let ((tag "HideEmptyStaffsAllSystems"))
   (if (d-Directive-layout? tag)
-      (begin
+      (let ((Hes (not (eq? (d-DirectiveGet-layout-data tag) "'HideEmptyStaffs"))))
 	(d-DirectiveDelete-layout tag)
-	(if (d-Directive-layout? "HideEmptyStaffs")
+	(if (and (d-Directive-layout? "HideEmptyStaffs") Hes)
 	    (d-HideEmptyStaffs)))
       (begin
 	  (if (not (d-Directive-layout? "HideEmptyStaffs"))
-	    (d-HideEmptyStaffs))
+	    (d-HideEmptyStaffs)
+	    (d-DirectivePut-layout-data tag "'HideEmptyStaffs"))
 	  (d-DirectivePut-layout-postfix tag 
 				     " \\context { \\Staff \\override VerticalAxisGroup #'remove-first = ##t } ")
           (d-DirectivePut-layout-override  tag DENEMO_OVERRIDE_GRAPHIC)
-	  (d-DirectivePut-layout-display  tag (_ "Empty staffs hidden in first line"))))
+	  (d-DirectivePut-layout-display  tag (_ "Empty staffs hidden in all systems"))))
   (d-SetSaved #f))
