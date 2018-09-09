6856494ef5dc9b9531a0c8119fe8d4a7b061ee65
Richard Shann <richard@rshann.plus.com>
2018-09-09 20:56:51 +0100

    Allow re-edit of movement comment on re-running command

--- actions/menus/ObjectMenu/MovementMenu/MvntComment.scm
+++ actions/menus/ObjectMenu/MovementMenu/MvntComment.scm
@@ -1,5 +1,7 @@
 ;;;;;;;;;;;;;;MvntComment
 (let ((tag "MvmntComment"))
-(d-DirectivePut-movementcontrol-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
-(d-DirectivePut-movementcontrol-display tag (_ "Click to edit comment"))
-(d-DisplayDirectiveTextEditor "movementcontrol" tag))
+	(if (not (d-Directive-movementcontrol? tag))
+		(begin
+			(d-DirectivePut-movementcontrol-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
+			(d-DirectivePut-movementcontrol-display tag (_ "Click to edit comment"))))
+	(d-DisplayDirectiveTextEditor "movementcontrol" tag))
