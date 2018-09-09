46dd52b6f6d8181cab6889227d399ca460ce48ab
Richard Shann <richard@rshann.plus.com>
2018-09-09 16:57:54 +0100

    upgrade score comment so that it does not erase on second invocation

--- actions/menus/ObjectMenu/Score/ScoreComment.scm
+++ actions/menus/ObjectMenu/Score/ScoreComment.scm
@@ -1,4 +1,7 @@
 ;;;;;;;;;;;;;;
-(d-DirectivePut-score-override "ScoreComment" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
-(d-DirectivePut-score-display "ScoreComment" (_ "Click to edit comment"))
-(d-DisplayDirectiveTextEditor "score" "ScoreComment")
+(let ((tag "ScoreComment"))
+	(if (not (d-Directive-score? tag))
+		(begin
+			(d-DirectivePut-score-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
+			(d-DirectivePut-score-display tag (_ "Type any notes about the score you are working on here"))))
+	(d-DisplayDirectiveTextEditor "score" tag))
