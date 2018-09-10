4fb38bf33e04b75b945b8c7116c3473f89952b76
Richard Shann <richard@rshann.plus.com>
2018-09-10 08:41:21 +0100

    Make staff comment self edit

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/StaffComment.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/StaffComment.scm
@@ -1,4 +1,8 @@
-;;;;;;;;;;;;;;;;;;
-(d-DirectivePut-staff-override "StaffComment" (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
-(d-DirectivePut-staff-display "StaffComment" (_ "Click to edit comment"))
-(d-RefreshDisplay)
+;;;;;;;;;;;;;;;;;;StaffComment
+(let* ((tag "StaffComment")(current (d-DirectiveGet-staff-display tag)))
+	(if (not current)
+		(set! current (_ "comment for this staff")))
+	(set! current (d-GetUserInput (_ "Staff Comment") (_ "Give comment text for current staff ") current #f))
+	(d-DirectivePut-staff-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_EDITOR))
+	(d-DirectivePut-staff-display tag current)
+	(d-DisplayDirectiveTextEditor "staff" tag))
