686aaf9917fa3cd466256644975119489c8f7170
Richard Shann <richard@rshann.plus.com>
2018-09-22 08:38:35 +0100

    separate off checking tabs from compare scores script

--- actions/denemo.scm
+++ actions/denemo.scm
@@ -1515,6 +1515,39 @@
             
 (define-once Transpose::Interval "c g");;; a more sensible default used by all typeset-transposed functions
 
-
-        
+(define (CheckTabs first second staffnum)
+;check score headers
+    (define message (d-DifferenceOfProjects first second))
+    (if message
+       (d-WarningDialog  (string-append (_ "Scores have different header information: ") message)))
+;check movement headers
+    (set! message (d-DifferenceOfMovements first second))
+    (if message
+            (d-WarningDialog (string-append (_ "Movement number ") (number->string (d-GetMovement))(_ "Movements have different header information: "))))
+;check staff headers    
+    (while (= (d-GetStaff) (1+ staffnum))
+        (set! staffnum (1+ staffnum))
+        (set! message (d-DifferenceOfStaffs first second)) ;moves to staff below after creating difference string
+        (if message
+             (d-WarningDialog (string-append (_ "Staff number ") (number->string (d-GetStaff)) ": " (_ "Staffs have different properties: ") message))))
+;check staff contents
+    (let loop ((staff 1))
+        (d-SelectTab first)(disp "Doing staff " staff "\n")
+        (if (d-GoToPosition 1 staff 1 1)
+            (let ((move #f))
+                (d-SelectTab second)
+                (if (d-GoToPosition 1 staff 1 1)
+                    (let inner-loop ()
+                        (set! message (d-CompareObjects first second move))
+                        (if (not message)
+                                (loop (+ 1 staff))
+                                (let ((response
+                                        (d-GetUserInput (_ "Comparing Music") (string-append message "\n" (_ "Continue Searching?")) "y")))
+                                     (if (and (string? response) (equal? response "y"))
+                                        (begin
+                                            (set! move #t)
+                                            (inner-loop))
+                                        (loop (+ 1 staff))))))
+                      (d-InfoDialog (_ "Different numbers of staffs."))))))
+    (d-InfoDialog (_ "No (further) differences found, but lyric verses are not checked, and additional staffs will be ignored")))       
         
