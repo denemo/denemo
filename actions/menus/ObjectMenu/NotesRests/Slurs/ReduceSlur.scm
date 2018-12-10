0f55ebb202c64c79a73373ca3c899c4eacbdf77e
Richard Shann <richard@rshann.plus.com>
2018-12-10 09:44:47 +0000

    Make Reduce Slur reduce from either end

--- actions/menus/ObjectMenu/NotesRests/Slurs/ReduceSlur.scm
+++ actions/menus/ObjectMenu/NotesRests/Slurs/ReduceSlur.scm
@@ -7,14 +7,16 @@
 			(d-ToggleBeginSlur)
 			(d-ToggleEndSlur))))
    (begin
-   	(if (d-PrevChord)
-   		(if (d-IsSlurEnd)
-   			(begin 	
-				(d-ToggleEndSlur)
-				(if (d-PrevChord)
-					(if (d-IsSlurStart)
-					(d-ToggleBeginSlur)
-					(d-ToggleEndSlur))))))))
+	(if (d-IsSlurStart)
+		(d-LaterSlurStart)
+		(if (d-PrevChord)
+			(if (d-IsSlurEnd)
+				(begin 	
+					(d-ToggleEndSlur)
+					(if (d-PrevChord)
+						(if (d-IsSlurStart)
+						(d-ToggleBeginSlur)
+						(d-ToggleEndSlur)))))))))
 
 
 
