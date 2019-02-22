9c93a7a051d193e379b73009cf32b5af8a7b49e1
Richard Shann <richard@rshann.plus.com>
2019-02-22 18:05:53 +0000

    Create new command ChangeStencil to allow altering typeset appearance of grobs

---
+++ actions/menus/ObjectMenu/Directives/Typesetter/ChangeStencil.scm
@@ -0,0 +1,28 @@
+;;ChangeStencil
+(let ((tag "ChangeStencil")
+        (choice (RadioBoxMenuPairs  
+        	(cons (_ "Augmentation Dot") "Dots") 
+        	(cons (_ "Accidental") "Accidental") 
+        	(cons (_ "Note Head") "NoteHead")
+        	(cons (_ "Clef") "Staff.Clef")
+        	;(cons (_ "Clef Modifier") "Staff.ClefModifier")
+        	(cons (_ "Cue Clef") "Staff.CueClef")
+        	(cons (_ "Cue End Clef") "Staff.CueEndClef")
+        	(cons (_ "Key Signature") "Staff.KeySignature")
+        	(cons (_ "Key Cancellation") "Staff.KeyCancellation")
+        	(cons (_ "Arpeggio") "Staff.Arpeggio")
+        	(cons (_ "Breath") "Staff.BreathingSign")
+        	(cons (_ "Custos") "Staff.Custos")
+        	(cons (_ "Fingering") "Fingering")
+        	(cons (_ "Multi-Measure Rest") "MultiMeasureRest")
+        	(cons (_ "Rest") "Rest")
+            )))
+   (if choice
+   	(let ((markup (d-GetUserInputWithSnippets (string-append (_ "Markup to use for ") (car choice))))) 
+   (if markup
+   	(StandAloneDirectiveProto (cons tag (string-append  "\\once \\override " 
+                                                        (cdr choice) ".stencil = #ly:text-interface::print "
+                                                        "\\once \\override " 
+                                                        (cdr choice) ".text = \\markup{"
+                                                        (cdr markup) "}\n"))
+                            #f #f (car choice) 20)))))
