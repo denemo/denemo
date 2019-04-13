3224d87d346c9f55614a78af018bf5f3ec381657
Richard Shann <richard@rshann.plus.com>
2019-04-13 11:15:35 +0100

    as before for menus

--- src/core/menusystem.c
+++ src/core/menusystem.c
@@ -802,7 +802,7 @@ menu_click (GtkWidget * widget, GdkEventButton * event, DenemoAction * action)
         {
           append_scheme_call ((gchar *) func_name);
         }
-      g_string_printf (Denemo.input_filters, "%s <span foreground=\"blue\">%s</span>", func_name, _(" (use Fn12 to Repeat)"));
+      g_string_printf (Denemo.input_filters, "%s  <span foreground=\"Green\">%s.</span> <span foreground=\"blue\">%s</span>", func_name, _("Help"), _("(use Fn12 to Repeat)"));
       write_input_status();
      return FALSE;
    }
