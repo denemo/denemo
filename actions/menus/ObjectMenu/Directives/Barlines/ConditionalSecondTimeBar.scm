ba5e9920a12f710754f0f9267e7014be73bf4bad
Richard Shann <richard@rshann.plus.com>
2019-05-17 20:29:27 +0100

    Allow for mid- or end- movement second time bars

--- actions/menus/ObjectMenu/Directives/Barlines/ConditionalSecondTimeBar.scm
+++ actions/menus/ObjectMenu/Directives/Barlines/ConditionalSecondTimeBar.scm
@@ -3,7 +3,10 @@
 (d-SetMark)
 (d-GoToEnd)
 (d-Copy)
-(d-RepeatEndStart)
+(let ((choice (RadioBoxMenu (cons (_ "End of Movement") 'end) (cons (_ "Mid-Movement") 'mid))))
+                            (case choice
+                                ((end) (d-RepeatEnd))
+                                ((mid) (d-RepeatEndStart))))
 (d-OnlyForLayout (cons 'noninteractive (cons (d-StaffProperties "query=lily_name") (d-GetCurrentStaffLayoutId))))
 (d-AddMeasure)
 (d-IgnoreOn 'noninteractive)
