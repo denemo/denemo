717f9d737a88e23f0ce308a829a15c244284da2f
Richard Shann <richard@rshann.plus.com>
2019-05-31 10:06:46 +0100

    do not assume a space in time fields

--- actions/menus/MainMenu/ViewMenu/DisplayEditingTime.scm
+++ actions/menus/MainMenu/ViewMenu/DisplayEditingTime.scm
@@ -1,2 +1,2 @@
-;DisplayEditTime
-(d-InfoDialog (string-append (_ "This score has taken\n") (d-GetEditingTime) (_ "to create, not counting unsaved edits.")))
+;DisplayEditingTime
+(d-InfoDialog (string-append (_ "This score has taken\n") (d-GetEditingTime) (_ " to create, not counting unsaved edits.")))
