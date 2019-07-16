206694dd1710652d89e83e4ef84750292660a3f7
Richard Shann <richard@rshann.plus.com>
2019-07-16 16:14:44 +0100

    Upgrade non-printing book/rehearsal mark

--- actions/menus/MainMenu/NavigationMenu/Bookmarks/BookmarkNonPrinting.scm
+++ actions/menus/MainMenu/NavigationMenu/Bookmarks/BookmarkNonPrinting.scm
@@ -1,16 +1,13 @@
 ;;;RehearsalMarkNonPrinting 
- ;;;Non Printing Bookmark 
-;;;by Nils Gey RTS Modified to avoid poluting the global namespace with the variable user-input
-
-(let ((user-input "XXX"))
-(set! user-input (d-GetUserInput "Named Bookmark" "Give a name" "X"))
-
- (if user-input   ;in case the user pressed Escape do nothing
- (begin
-  (d-Directive-standalone "RehearsalMark")
-  (d-DirectivePut-standalone-display "RehearsalMark" user-input)
-  (d-DirectivePut-standalone-ty  "RehearsalMark" -40)
-  (d-DirectivePut-standalone-tx  "RehearsalMark" -10)
-  ))
-(d-RefreshDisplay))
-
+;;;Non Printing Bookmark - note it uses the RehearsalMark tag which allows it to be found via book mark navigation, but should have its own edit script FIXME
+(let* ((tag "RehearsalMark")
+      (current (d-DirectiveGet-standalone-display tag)))
+  (set! current (d-GetUserInput (_ "Insert Non-printing Rehearsal Mark") (_ "Give a name ") (if current current "X") #f))
+	(if current 
+    (begin
+      (d-Directive-standalone tag)
+      (d-DirectivePut-standalone-minpixels tag 30)	
+      (d-DirectivePut-standalone-override tag DENEMO_OVERRIDE_EDITOR)
+      (d-DirectivePut-standalone-display tag current)
+      (d-SetSaved #f)
+      (d-RefreshDisplay))))
