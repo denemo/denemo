d0e200c574956ecbb6075d72027cda34a58d8311
Richard Shann <richard@rshann.plus.com>
2019-08-24 10:12:32 +0100

    Fix spurious error message on Windows temp directory creation

--- src/core/utils.c
+++ src/core/utils.c
@@ -221,7 +221,7 @@ make_temp_dir (gboolean removal)
           gchar *newdir = g_build_filename (g_get_tmp_dir (), "Denemo_XXXXXX", NULL);
 #ifdef G_OS_WIN32
           // Windows does not delete the temporary directory, use a constant one.
-          if (!g_mkdir_with_parents (newdir, 0700))
+          if (g_mkdir_with_parents (newdir, 0700))
             g_warning ("Creation of temp dir %s failed\n", newdir);
 #else
           if (!g_mkdtemp (newdir))
