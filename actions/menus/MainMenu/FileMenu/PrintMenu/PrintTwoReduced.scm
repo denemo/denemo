43ac6187161429ab890bce5ad944eb961415675d
Richard Shann <richard@rshann.plus.com>
2020-02-22 15:50:50 +0000

    Allow PartCombine to take a layout name and use it in PrintTwoReduced

--- actions/menus/MainMenu/FileMenu/PrintMenu/PrintTwoReduced.scm
+++ actions/menus/MainMenu/FileMenu/PrintMenu/PrintTwoReduced.scm
@@ -1,103 +1,7 @@
-;;PrintTwoReduced New version                 
+;;PrintTwoReduced Simpler version using partCombine                
 (let ((id #f)(tag "Share Staff" )(stems-swapped #f)(upstart 0)(downstart 0)(up 0)(down 0)(o1 1)(o2 1) (m 1) (bar 1)(s1 1)(s2 2)) 
-    
-    (define (swap-stems)
-        (define (apply-directive nobeam)
-            
-            (d-DirectivePut-chord-prefix tag (if stems-swapped "\\voiceOne " "\\voiceTwo "))
-            (d-DirectivePut-chord-postfix tag nobeam)
-            (d-DirectivePut-chord-display tag (_ "Share Staff"))
-            (d-DirectivePut-chord-allow tag id)
-            (d-DirectivePut-chord-override tag (logior DENEMO_ALT_OVERRIDE DENEMO_OVERRIDE_AFFIX)))
-        (d-GoToPosition m s1 bar o1)
-        (if stems-swapped
-            (apply-directive "\\noBeam ^\\markup \"1\" _\\markup \"2\"")
-            (apply-directive "\\noBeam ^\\markup \"2\" _\\markup \"1\""))
-        (set! stems-swapped (not stems-swapped))
-        (d-GoToPosition m s2 bar o2)
-        (apply-directive "\\noBeam"))
-      
-     (define (find-upper-note)
-            (let loop () (disp "fu: bar:" bar " o1:" o1 " o2:" o2 " upstart:" upstart " downstart:" downstart)
-                (if (d-GoToPosition m s1 bar o1)
-                    (if (or (not (Note?)) (> downstart (d-GetStartTick)))
-                        (begin
-                            (set! o1 (1+ o1))
-                            (loop))
-                        (begin
-                            (set! up (d-GetNoteAsMidi))
-                            (set! upstart (d-GetStartTick))))
-                    (begin
-                        (set! upstart #f)
-                        #f))))
-    (define (find-lower-note)
-        (let loop () (disp "fl: bar:" bar " o1:" o1 " o2:" o2  " upstart:" upstart " downstart:" downstart)
-            (if (d-GoToPosition m s2 bar o2)
-                (if (or (not (Note?)) (> upstart (d-GetStartTick)))
-                    (begin
-                        (set! o2 (1+ o2))
-                        (loop))
-                    (begin
-                            (set! down (d-GetNoteAsMidi))
-                            (set! downstart (d-GetStartTick))))
-                    (begin
-                        (set! downstart #f)
-                        #f))))
-    (define (find-notes)
-        (if (and upstart downstart)
-            (if (> upstart downstart)
-                (find-lower-note)
-                (find-upper-note))
-            #f))
-    (define (check-pitches)
-        (if (d-GoToPosition m s2 bar o2)
-            (begin
-                (set! down (d-GetNoteAsMidi))
-                (if (d-GoToPosition m s1 bar o1)
-                    (begin
-                        (set! up (d-GetNoteAsMidi))
-                        (if (or (and (> up down) stems-swapped)
-                                (and (> down up)(not stems-swapped)))
-                            (swap-stems))
-                        (disp "\n\nSuccess: bar:" bar " o1:" o1 " o2:" o2 "\n") 
-                        (disp "Up pitch:" up " Down pitch:" down)
-                        #t)))))
-        
-    (define (do-bar)
-        (disp "upstart:" upstart " downstart:" downstart)
-        (while (and (find-notes) upstart downstart (not (= upstart downstart))))
-        (disp "find-notes " upstart " vs " downstart)
-        (if (and upstart downstart)
-                    (begin
-                        (check-pitches)
-                        (set! o1 (1+ o1))
-                        (set! o2 (1+ o2))
-                        (do-bar))
-                    (let loop () 
-                        (set! bar (1+ bar))
-                        
-                        (if (<= bar (d-GetMeasuresInStaff))
-                            (begin
-                                (disp "Onward to bar " bar "\n")
-                                (set! o1 1)
-                                (set! o2 1)
-                                (set! upstart 0)
-                                (set! downstart 0)
-                                (find-upper-note)
-                                    (if upstart
-                                        (begin
-                                            (find-lower-note)
-                                            (if downstart
-                                                (check-pitches)
-                                                (loop)))
-                                        (loop)))
-                            #f))))
-;;;;
-(d-CreateLayout tag)  
-(set! id (d-GetLayoutId))
+(set! id (d-GetIdForName tag))
 (d-DirectivePut-scoreheader-postfix tag "\ninstrumentation=\"Accompanist's Score\"\n")
-
-(d-DeleteLayout tag)
 
 (while (<= m (d-GetMovementsInScore))
     (set! s1 1)
@@ -108,29 +12,17 @@
     (set! o2 1)
     
     (d-GoToPosition m s1 bar o1)
-    (d-SmallerStaff) ;; FIXME make conditional
-    (d-DirectivePut-voice-postfix tag "\\voiceOne") 
-    (d-DirectivePut-voice-allow tag id)
+    (d-SmallerStaff)
     (d-DirectivePut-staff-prefix tag "instrumentName = \\markup Soli")
     (d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC))
     (d-DirectivePut-standalone tag)
     (d-DirectivePut-standalone-minpixels tag 5)
-    (d-DirectivePut-standalone-postfix tag "\\override Voice.Slur.stencil = ##f                   
-                                    \\override Voice.Tie.stencil = ##f ")
-    (d-DirectivePut-standalone-allow tag id) (disp "set 1 allow " id " on " tag "\n")
+    (d-DirectivePut-standalone-postfix tag "\\omit Staff.Slur \\omit Staff.Tie ")
+    (d-DirectivePut-standalone-allow tag id)
     (d-GoToPosition m s2 bar o2)
     (d-SmallerStaff)
-    (d-DirectivePut-voice-postfix tag "\\voiceTwo") 
-    (d-DirectivePut-voice-allow tag id)
-    
-    
     (d-SetCurrentStaffAsVoice)
-    (d-DirectivePut-standalone tag)
-    (d-DirectivePut-standalone-minpixels tag 5)
-    (d-DirectivePut-standalone-postfix tag "\\override Voice.Slur.stencil = ##f                   
-                                    \\override Voice.Tie.stencil = ##f ")
-    (d-DirectivePut-standalone-allow tag id) (disp "set 2 allow " id " on " tag "\n")
-    (while (and (do-bar) (<= bar (d-GetMeasuresInStaff))))
+    (d-PartCombine tag)
     (set! m (1+ m)))
 (d-CreateLayout tag)  
 (d-SelectDefaultLayout)
--- actions/menus/ObjectMenu/StaffMenu/Voices/Part-Combining/PartCombine.scm
+++ actions/menus/ObjectMenu/StaffMenu/Voices/Part-Combining/PartCombine.scm
@@ -1,6 +1,7 @@
 ;;;PartCombine
-(let ((tag "PartCombine"))
+(let ((tag "PartCombine")(name PartCombine::params))
 	(define (install-combine)
+
 		(if (d-Directive-voice? tag)
 			(begin
 				(d-DirectiveDelete-voice tag)
@@ -9,14 +10,19 @@
 				(d-InfoDialog (_ "Parts are now uncombined")))
 			(begin
 				(d-DirectivePut-voice-prefix tag " \\set Staff.soloText = #\"\"
-  \\set Staff.soloIIText = #\"\"
-  \\set Staff.aDueText = #\"\" \\partcombine { ")
+                                                  \\set Staff.soloIIText = #\"\"
+                                                  \\set Staff.aDueText = #\"\" \\partcombine { ")
 				(d-DirectivePut-voice-display tag (_ "Part Combine "))
 				(d-DirectivePut-voice-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+				
+				(if name (d-DirectivePut-voice-allow tag (d-GetIdForName name)))
+				
 				(d-MoveToVoiceDown)
 				(d-DirectivePut-voice-prefix tag " } %{next voice is combined with previous part %} ")
 				(d-DirectivePut-voice-display tag (_ "Part Combine "))
-				(d-DirectivePut-voice-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))))
+				(d-DirectivePut-voice-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_LILYPOND))
+				
+				(if name (d-DirectivePut-voice-allow tag (d-GetIdForName name)))))
 		(d-SetSaved #f))
 
 	(d-PushPosition)
@@ -28,7 +34,8 @@
 				(install-combine)))
 		(if (d-MoveToVoiceUp)
 				(if (d-MoveToVoiceUp)
-					(d-WarningDialog (_ "Only two voices can be part-combined"))
+					(d-WarningDialog (_ "Can only part-combine two voices"))
 					(install-combine))
 				(d-WarningDialog (_ "Must be on a staff with two voices"))))
+    
 	(d-PopPosition))
