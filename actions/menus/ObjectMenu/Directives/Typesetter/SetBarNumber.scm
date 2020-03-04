ba549d1b4f5e9964c3c001129492f32d2e3d8d3f
Richard Shann <richard@rshann.plus.com>
2020-03-04 09:22:44 +0000

    Avoid set bar number running into other lilypond syntax following

--- actions/menus/ObjectMenu/Directives/Typesetter/SetBarNumber.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/SetBarNumber.scm
@@ -1,7 +1,7 @@
 (let ((num (number->string (1- (d-GetMeasure)))))
 (define result (d-GetUserInput (_ "Change Barnumber in typeset score") (_ "Please enter a number to set the current bar number. The typeset score will continue to count up from there.") num))
 	(if result 
-	(let ((barnumberstring (string-append "\\set Score.currentBarNumber = #" result ) ))
+	(let ((barnumberstring (string-append "\\set Score.currentBarNumber = #" result " " ) ))
 		(d-Directive-standalone "BarNumber")
 		(d-DirectivePut-standalone-display "BarNumber" (string-append (_ "BarNumber = ") result))
 		(d-DirectivePut-standalone-minpixels  "BarNumber"  30)
