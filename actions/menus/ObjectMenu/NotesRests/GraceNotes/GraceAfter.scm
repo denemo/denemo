1bf2acf494e6685223f94b6ea95dcfea94b089c8
Richard Shann <richard@rshann.plus.com>
2020-03-17 12:10:29 +0000

    Improve GraceAfter command

--- actions/menus/ObjectMenu/NotesRests/GraceNotes/GraceAfter.scm
+++ actions/menus/ObjectMenu/NotesRests/GraceNotes/GraceAfter.scm
@@ -1,43 +1,39 @@
 ;;GraceAfter
-(d-PushPosition)
-(if (Appending?)
-  (d-MoveCursorLeft))
-(while  (and (d-IsGrace) (d-MoveCursorLeft))
-	#t)
-
-(if (and (Note?) (not (d-IsGrace)))
-    (begin
-      (while (and (d-MoveCursorRight) (Note?) (d-IsGrace))
-      	#t)
-      (if (or (Appending?) (not (Note?)) (not (d-IsGrace)))
-	  (d-MoveCursorLeft))
-     (if (d-IsGrace)
-     	(begin
-     		 ;; on last grace note
-   		 (let loop ((close "}"))
- 		(if (d-IsGrace)
- 			(let ((beam (d-DirectiveGet-chord-postfix "Beam")))
-			   (if beam
-			      (d-DirectivePut-chord-override "Beam" (logior (d-DirectiveGet-chord-override "Beam") DENEMO_OVERRIDE_HIDDEN))
-		  	    (set! beam ""))		
-	    		   (d-DirectivePut-chord-postfix "GraceAfter" (string-append (d-GetNote) (d-GetNoteDuration)  beam (if (d-IsSlurStart) "(" "") (if (d-IsSlurEnd) ")" "") close))
-       		   	(d-DirectivePut-chord-override "GraceAfter" DENEMO_OVERRIDE_LILYPOND)
-       		   	(if (d-MoveCursorLeft)
-       		   		(loop "")))))
-      	(d-MoveCursorRight)
-     	 (d-DirectivePut-chord-prefix "GraceAfter" "{")
-    	  (d-MoveCursorLeft)
-    	  ; now on main note
-    	  (d-DirectivePut-chord-display "MainGraceAfter"  "Grace After" )
-     	 
- 	   (d-DirectivePut-chord-prefix "MainGraceAfter"  "\\afterGrace ")
-      	   (d-DirectivePut-chord-override "MainGraceAfter"  DENEMO_OVERRIDE_AFFIX)	
- 	   (d-DirectivePut-chord-minpixels  "MainGraceAfter" 20)
-	    (d-PopPosition)
-	    (d-SetSaved #f))
-  	(begin
-   	   (d-PopPosition)
-   	   (d-WarningDialog  (_ "The cursor is not on a grace note after a main note or chord\nFirst create the main note, then follow it with the grace note(s) then invoke this command with the cursor on the grace note")))))
+(let ((tag "GraceAfter"))
+(if 	(d-Directive-chord? tag)
+	(d-DirectiveDelete-chord tag)
 	(begin
-   	  	 (d-PopPosition)
-   	  	 (d-WarningDialog  (_ "The cursor is not on a grace note after a main note or chord\nFirst create the main note, then then follow it with the grace note(s) then invoke this command with the cursor on the grace note"))))
+ 		(d-DirectivePut-chord-prefix tag "\\GraceAfter ")
+ 		(d-DirectivePut-chord-display tag (_ "GraceAfter"))
+ 		(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
+ 		(d-DirectivePut-score-prefix tag "GraceAfter = #(define-music-function (parser location note graces) (ly:music? ly:music?)
+			 (if (null? (ly:music-property graces 'element))
+			     (begin
+			      (ly:warning \"No grace notes follow this note the afterGrace will be ignored.\")
+			      (make-music
+				      'SequentialMusic 
+				      'elements
+				      (list note graces)))
+			     (let ((thegraces  (ly:music-property (list-ref (ly:music-property 
+				                             (ly:music-property graces 'element) 'elements) 1) 'elements)))
+				(define gracenotes (make-music 'GraceMusic 'element (make-music
+				                'SequentialMusic
+				                'elements
+				                thegraces)))
+				  
+				   (make-music
+				      'SimultaneousMusic 
+				      'elements
+				      (list
+				         note 
+				        (make-music
+				         'SequentialMusic
+				          'elements
+				          (list
+				                 (make-music
+				                      'SkipMusic
+				                      'duration
+				                      (ly:make-duration 0 0 3/16))
+				                    gracenotes)))))))")
+  		(d-DirectivePut-score-override tag DENEMO_OVERRIDE_AFFIX)
+		(d-SetSaved #f))))
