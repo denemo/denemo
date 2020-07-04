be62e3b5e68be55f389e54ad87d64df0811381c5
Richard Shann <richard@rshann.plus.com>
2020-07-04 16:48:21 +0100

    Add user-friendly command to change timbre of instrument

---
+++ actions/menus/ObjectMenu/Directives/Playback/MIDI/ChangeTimbre.scm
@@ -0,0 +1,14 @@
+;;;ChangeTimbre
+(let ((tag "ChangeTimbre")(prognum "46")(text "pizz."))
+    (set! prognum (d-GetUserInput (_ "Change Instrument Timbre") (_ "Give MIDI program number (1-256): ") prognum))
+    (if (and prognum (string->number prognum))
+        (begin
+            (set! text (d-GetUserInput (_ "Change Instrument Timbre") (_ "Give accompanying text to typset here (if any): ") text))
+            (if text
+                (begin
+                    (d-Directive-standalone tag)
+                    (d-DirectivePut-standalone-postfix tag (string-append "<>^\\markup{\\italic {" text "}} "))
+                    (d-DirectivePut-standalone-display tag text)
+                    (d-DirectivePut-standalone-midibytes tag (string-append "0xc$ " prognum))
+                    (d-DirectivePut-standalone-minpixels tag 30)
+                    (d-SetSaved #f)(d-RefreshDisplay))))))
