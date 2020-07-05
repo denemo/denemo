9eabccd7ba1916d8a3fa7e090647f2a80fc4aca2
Richard Shann <richard@rshann.plus.com>
2020-07-05 09:19:45 +0100

    Fix off by one error in program number entry

--- actions/menus/ObjectMenu/Directives/Playback/MIDI/ChangeTimbre.scm
+++ actions/menus/ObjectMenu/Directives/Playback/MIDI/ChangeTimbre.scm
@@ -3,6 +3,7 @@
     (set! prognum (d-GetUserInput (_ "Change Instrument Timbre") (_ "Give MIDI program number (1-256): ") prognum))
     (if (and prognum (string->number prognum))
         (begin
+            (set! prognum (number->string (1-  (string->number prognum))))
             (set! text (d-GetUserInput (_ "Change Instrument Timbre") (_ "Give accompanying text to typset here (if any): ") text))
             (if text
                 (begin
@@ -12,3 +13,4 @@
                     (d-DirectivePut-standalone-midibytes tag (string-append "0xc$ " prognum))
                     (d-DirectivePut-standalone-minpixels tag 30)
                     (d-SetSaved #f)(d-RefreshDisplay))))))
+
