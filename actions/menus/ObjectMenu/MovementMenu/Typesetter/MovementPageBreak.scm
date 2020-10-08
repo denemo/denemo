d41acbcdd73a7290abfc131fc14b78f90ea18832
Richard Shann <richard@rshann.plus.com>
2020-10-08 12:30:28 +0100

    Allow page break to be inserted non-interactively

--- actions/menus/ObjectMenu/MovementMenu/Typesetter/MovementPageBreak.scm
+++ actions/menus/ObjectMenu/MovementMenu/Typesetter/MovementPageBreak.scm
@@ -5,14 +5,18 @@
     (Inserted  (_ "Page Break Inserted before Movement Title"))
     (TitledPiece "TitledPiece"))
     (define (put-break)
+		(if (not (d-IsInteractive))
+			(d-DirectiveDelete-movementcontrol tag)) ;;to remove any conditionality
         (d-DirectivePut-movementcontrol-override tag (logior DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_TAGEDIT))
-            
         (d-DirectivePut-movementcontrol-display tag "Page Break before Movement")
         (d-DirectivePut-movementcontrol-prefix  tag "\n\\pageBreak\n")
-        (SetDirectiveConditional  #f (cons "movementcontrol"  tag))
-        (TimedNotice Inserted)        
+        (if (d-IsInteractive)
+			(begin
+				(SetDirectiveConditional  #f (cons "movementcontrol"  tag))
+				(TimedNotice Inserted)))        
         (d-DirectivePrioritizeTag-movementcontrol tag))
-  (if (d-Directive-movementcontrol? tag)
+        
+  (if (and (d-IsInteractive) (d-Directive-movementcontrol? tag))
             (let ((choice (RadioBoxMenu (cons (_ "Make Conditional") 'conditional) (cons (_ "Delete Page Break") 'delete))))
                 (if (eq? choice 'delete)
                     (begin
