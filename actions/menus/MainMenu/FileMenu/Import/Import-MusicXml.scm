2dc8eacdb4f29d3cec421deb6e82c8680577998d
Richard Shann <richard@rshann.plus.com>
2020-11-20 17:57:39 +0000

    Allow to import either book or simple titles

--- actions/menus/MainMenu/FileMenu/Import/Import-MusicXml.scm
+++ actions/menus/MainMenu/FileMenu/Import/Import-MusicXml.scm
@@ -5,4 +5,9 @@
 		(while (d-StaffDown)
 			(d-DeleteStaff))
 			(d-DeleteStaff)))
+(if (not (or (d-Directive-movementcontrol? "TitledPiece") (d-Directive-scoreheader? "BookTitle")))
+	(let ((choice (RadioBoxMenu (cons (_ "Simple Titles") 'simple) (cons (_ "Book Titles") 'book))))
+		(case choice
+			 ((simple) (d-DirectivePut-movementcontrol-display "TitledPiece" (_ "Untitled")))
+			 ((book) (d-BookTitle (_ "Untitled"))))))
 (d-ImportMusicXml)
