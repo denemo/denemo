4dcc8e6b1dba0110d8d74a94b1ced13c692c8cf1
Richard Shann <richard@rshann.plus.com>
2020-12-02 15:33:17 +0000

    Fix transpose staff for staffs that have figured bass

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/TransposeStaffPrint.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/TransposeStaffPrint.scm
@@ -1,15 +1,17 @@
 ;;;;; TransposeStaffPrint
 (define-once Transpose::Interval "c ees")
-(let ((tag "TransposeStaffPrint")(lily #f) (text #f)(params TransposeStaffPrint::params))
-    (if (and params  (not (equal? params "edit")))
-        (set!  Transpose::Interval params)
-        (set! Transpose::Interval  (d-GetNoteNamesFromUser 2 Transpose::Interval (_ "--> Transpose to -->") )))
-    (if Transpose::Interval
-        (begin
-            (set! lily (string-append  "\\transpose " Transpose::Interval " "))
-            (set! text (string-append  (_ "Print transposed:  ") Transpose::Interval " "))
-            (d-DirectivePut-staff-override tag  DENEMO_OVERRIDE_GRAPHIC)
-            (d-DirectivePut-staff-display  tag text)
-            (d-DirectivePut-staff-postfix  tag lily)
-            (d-SetSaved #f))
-        (d-WarningDialog (_ "Cancelled"))))
+(if (d-HasFigures)
+	(d-TransposeVoices)
+	(let ((tag "TransposeStaffPrint")(lily #f) (text #f)(params TransposeStaffPrint::params))
+	    (if (and params  (not (equal? params "edit")))
+		(set!  Transpose::Interval params)
+		(set! Transpose::Interval  (d-GetNoteNamesFromUser 2 Transpose::Interval (_ "--> Transpose to -->") )))
+	    (if Transpose::Interval
+		(begin
+		    (set! lily (string-append  "\\transpose " Transpose::Interval " "))
+		    (set! text (string-append  (_ "Print transposed:  ") Transpose::Interval " "))
+		    (d-DirectivePut-staff-override tag  DENEMO_OVERRIDE_GRAPHIC)
+		    (d-DirectivePut-staff-display  tag text)
+		    (d-DirectivePut-staff-postfix  tag lily)
+		    (d-SetSaved #f))
+		(d-WarningDialog (_ "Cancelled")))))
