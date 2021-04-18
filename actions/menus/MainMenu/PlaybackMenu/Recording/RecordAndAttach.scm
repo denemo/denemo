413fd010462fb74dc98db4cd23ae03c69582cc97
Richard Shann <richard@rshann.plus.com>
2021-04-18 16:51:50 +0100

    Only explain once per session about Record and Attach. Position cursor back after use

--- actions/menus/MainMenu/PlaybackMenu/Recording/RecordAndAttach.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/RecordAndAttach.scm
@@ -1,5 +1,6 @@
 ;RecordAndAttach - Record (Start/Stop) and Attach at Cursor
 (define-once RecordAndAttach::position '(1 1 1 1))
+(define-once RecordAndAttach::explain #t)
 (let ((filename (string-append DENEMO_LOCAL_ACTIONS_DIR "audio-recording.wav")))
 	(d-ToggleRecordingAudio)
 	(if (d-RecordingAudio)
@@ -10,20 +11,24 @@
 			(if (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
 				(begin
 					(d-CreateClickStaffForMidi (d-GetMeasuresInStaff))
-					(d-GoToPosition #f (list-ref RecordAndAttach::position 1)(list-ref RecordAndAttach::position 2)(list-ref RecordAndAttach::position 3))
+					(d-GoToPosition #f (1+ (list-ref RecordAndAttach::position 1))(list-ref RecordAndAttach::position 2)(list-ref RecordAndAttach::position 3))
 					(set! RecordAndAttach::position (GetPosition))))
 			(d-GoToPosition #f 1 (list-ref RecordAndAttach::position 2) (list-ref RecordAndAttach::position 3))
-			(d-InfoDialog (_ "Play music you want to insert next\nThen re-issue this command.")))
+			(if RecordAndAttach::explain
+				(d-InfoDialog (_ "Play music you want to insert next\nThen re-issue this command."))))
 		(begin
 			(d-ExportRecordedAudio filename)
 			(if (EmptyMeasure?)
 				(d-2))
 			(d-OpenSourceAudioFile filename)
-			(d-GoToPosition RecordAndAttach::position)
+			(apply d-GoToPosition RecordAndAttach::position)
 			(d-SetPlaybackInterval (d-GetMidiOnTime) -1)
-			(d-InfoDialog (_ "Now the music is attached to the score starting at the cursor position.
+			(if RecordAndAttach::explain
+				(begin
+					(set! RecordAndAttach::explain #f)
+					(d-InfoDialog (_ "Now the music is attached to the score starting at the cursor position.
 The timing of the notes is shown at the top of the score.
 You can drag the timing of start of the music by left-clicking above the blue line.
 You can insert the rhythm in your staff and check by invoking Play.
-When that's correct, press the Shift key to switch to Inserting/Appending Pitches and play in the music again.")))))
+When that's correct, press the Shift key to switch to Inserting/Appending Pitches and play in the music again.")))))))
  
