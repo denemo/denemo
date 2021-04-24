0300da538cf100aa9354bf14e9b0c4849a2734bd
Richard Shann <richard@rshann.plus.com>
2021-04-24 12:48:58 +0100

    Tidy up the Recording manu, improving labels and tooltips

--- actions/menus/MainMenu/PlaybackMenu/Recording/CloseSourceAudioFile.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/CloseSourceAudioFile.scm
@@ -1,6 +1,6 @@
 ;;;CloseSourceAudioFile
 (if (d-CloseSourceAudio)
   (begin
-    (d-InfoDialog "The source audio is now dropped from this score.\nIt will not automatically be re-opened with the score if you save.")
+    (d-InfoDialog "The source audio recording is deleted.")
     (d-SetSaved #f))
-  (d-InfoDialog "There is no source audio for this movement.\nUse File->Open->Open Source Audio to attach audio to the movement."))
+  (d-InfoDialog "There is no source audio recording.\nUse File->Open->Open Source Audio to attach audio to the movement or make a live recording."))
