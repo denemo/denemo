980eaa7b0fe3d84eb76b5a0110d794773969da0c
Richard Shann <richard@rshann.plus.com>
2021-05-08 11:13:26 +0100

    Display figured bass lilypond syntax inserts better

--- src/display/drawfigure.c
+++ src/display/drawfigure.c
@@ -23,48 +23,57 @@ draw_figure (cairo_t * cr, gint xx, gint y, DenemoObject * theobj)
       GString *gstr = g_string_new ("");
       ch = (chord *) theobj->object;
       gint ystep = 0, xstep = 0;
-      gint accs = FALSE;
+      gboolean accs = FALSE;
+      gboolean in_dollar = FALSE;
       gchar *orig = ((GString *) (ch->figure))->str;
       gchar *str = orig;
       while (*str)
         {
-          if (*str == '+')
-            {
-              accs = TRUE, g_string_append (gstr, "♯");
-            }
-          else if (*str == '-')
-            {
-              accs = TRUE, g_string_append (gstr, "♭");
-            }
-          else if (*str == '!')
-            {
-              accs = TRUE, g_string_append (gstr, "♮");
-            }
-          else if (*str == '_')
-            {
-              /*do nothing */ ;
-            }
-          else if (*str == '|')
-            {
-              drawlargetext_cr (cr, gstr->str, xx + xstep, y + STAFF_HEIGHT + 20 + ystep);
-              ystep = 0;
-              xstep += 25 + (accs ? 10 : 0);
-              g_string_assign (gstr, "");
-              accs = FALSE;
-            }
-          else if (*str == ' ')
-            {
-              if (gstr->len)
-                {
-                  drawlargetext_cr (cr, gstr->str, xx + xstep, y + STAFF_HEIGHT + 20 + ystep);
-                  ystep += 20;
-                  g_string_assign (gstr, "");
-                }
-            }
-          else
-            {
-              g_string_append_c (gstr, *str);
-            }
+		if (!in_dollar)
+			{
+			 if (*str == '$')
+				{
+					g_string_append (gstr, "$");
+					in_dollar = !in_dollar;
+				}
+			  if (*str == '+')
+				{
+				  accs = TRUE, g_string_append (gstr, "♯");
+				}
+			  else if (*str == '-')
+				{
+				  accs = TRUE, g_string_append (gstr, "♭");
+				}
+			  else if (*str == '!')
+				{
+				  accs = TRUE, g_string_append (gstr, "♮");
+				}
+			  else if (*str == '_')
+				{
+				  /*do nothing */ ;
+				}
+			  else if (*str == '|')
+				{
+				  drawlargetext_cr (cr, gstr->str, xx + xstep, y + STAFF_HEIGHT + 20 + ystep);
+				  ystep = 0;
+				  xstep += 25 + (accs ? 10 : 0);
+				  g_string_assign (gstr, "");
+				  accs = FALSE;
+				}
+			  else if (*str == ' ')
+				{
+				  if (gstr->len)
+					{
+					  drawlargetext_cr (cr, gstr->str, xx + xstep, y + STAFF_HEIGHT + 20 + ystep);
+					  ystep += 20;
+					  g_string_assign (gstr, "");
+					}
+				}
+			  else
+				{
+				  g_string_append_c (gstr, *str);
+				}
+			}
           str++;
           if (*str == 0)
             {
