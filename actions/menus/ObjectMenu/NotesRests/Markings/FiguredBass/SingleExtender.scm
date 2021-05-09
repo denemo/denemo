e8dd94fcfd8d6bc9c88c9b448cfd5497cea93f94
Richard Shann <richard@rshann.plus.com>
2021-05-09 14:40:22 +0100

    Allow SingleExtender to take a parameter

--- actions/menus/ObjectMenu/NotesRests/Markings/FiguredBass/SingleExtender.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/FiguredBass/SingleExtender.scm
@@ -1,7 +1,7 @@
 ;;;SingleExtender
-(let ((fig (d-GetUserInput (_ "Figured Bass Extenders") (_ "Give Figure to Extend") "0")))
+;;;Create a single extender for the group of figures on the previous note. Append |0 to the group and use 0 for the extender to leave a little space between the group and the start of the extender
+(let ((params SingleExtender::params)(fig #f))
+	(set! fig (if params params (d-GetUserInput (_ "Figured Bass Extenders") (_ "Give Figure to Extend") "0")))
 	(if (string? fig)
 		(d-EditFiguredBass (string-append "figures $\\bassFigureExtendersOn <" (scheme-escape fig) ">4
 \\bassFigureExtendersOff$"))))
-
-
