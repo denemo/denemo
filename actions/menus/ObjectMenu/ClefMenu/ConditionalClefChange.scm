006e157d97a7f7902832139b30f1bdf0c1d2f370
Richard Shann <richard@rshann.plus.com>
2021-06-13 15:24:22 +0100

    Add command to insert clef changes conditional on layout

---
+++ actions/menus/ObjectMenu/ClefMenu/ConditionalClefChange.scm
@@ -0,0 +1,12 @@
+;ConditionalClefChange
+(let ((tag "ConditionalClefChange"))
+(d-InsertClef)
+(d-MoveCursorLeft)
+(if (Clef?)
+	(begin
+		(d-DirectivePut-clef-prefix tag "%{ ")
+		(d-DirectivePut-clef-postfix tag " %} ")
+		(d-DirectivePut-clef-graphic tag "\n?\nDenemo\n24")
+		(d-DirectivePut-clef-gy tag 80)
+		(SetDirectiveConditional #f (cons "clef" tag))
+		(d-SetSaved #f))))
