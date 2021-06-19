f0c99c448f22b4f8b2993db3c8936e7d138f0159
Richard Shann <richard@rshann.plus.com>
2021-06-19 14:31:04 +0100

    Connect users to #denemo on Libera.Chat via Help menu as freenode has gone

--- actions/menus/MainMenu/HelpMenu/IRC.scm
+++ actions/menus/MainMenu/HelpMenu/IRC.scm
@@ -1,8 +1,7 @@
 ;;; IRC on freenode
  (d-InfoDialog (_ "Your browser may take a few moments to start - be patient!
- 	When it has started you have to type in some anti-spam words
- 	and click connect
+ 	When it has started click Start
  	Then you get a page with a place to type your question at the bottom.
  	Type hello in there and hit Enter
  	If no one is there to help you can use the mailing list denemo-devel@gnu.org"))
-(d-Help (string-append "http://webchat.freenode.net/?nick=" (d-GetUserName) "_" DENEMO_VERSION "&channels=denemo"))
+(d-Help (string-append "https://web.libera.chat/?nick=" (d-GetUserName) "_" ((string-escaper '((#\. . "_"))) DENEMO_VERSION) "#denemo"))
