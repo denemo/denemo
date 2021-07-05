c6b0858e0dce0d0d01e411c65c6a52ba1f008d15
Richard Shann <richard@rshann.plus.com>
2021-07-05 12:01:12 +0100

    Drop guided midi import -needs re-working to use recorded midi

--- actions/menus/MainMenu/FileMenu/Import/SelectMidiImportStrategy.scm
+++ actions/menus/MainMenu/FileMenu/Import/SelectMidiImportStrategy.scm
@@ -1,9 +1,10 @@
 ;;;SelectMidiImportStrategy
-(let ((choice (d-GetOption  (string-append (_ "Guided Import") stop  (_ "Automatic Import") stop))))
-   (cond
-     ((boolean? choice)
-      (d-InfoDialog (_ Cancelled)))
-     ((equal? choice (_ "Guided Import") )
-      (d-GuidedMidiImport))
-     ((equal? choice (_ "Automatic Import"))
-		(d-ImportMidi))))
+;guided midi import needs re-working to import one track as a MIDI recording...
+;(let ((choice (d-GetOption  (string-append (_ "Guided Import") stop  (_ "Automatic Import") stop))))
+ ;  (cond
+ ;    ((boolean? choice)
+  ;    (d-InfoDialog (_ Cancelled)))
+  ;   ((equal? choice (_ "Guided Import") )
+  ;    (d-GuidedMidiImport))
+  ;   ((equal? choice (_ "Automatic Import"))
+		(d-ImportMidi);)))
