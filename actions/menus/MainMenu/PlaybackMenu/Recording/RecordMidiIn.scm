a780bab4c1bb4b7b89c89f7ad643e0af6b30e99f
Richard Shann <richard@rshann.plus.com>
2021-07-10 18:25:35 +0100

    Don't show clef on MIDI track

--- actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
@@ -1,13 +1,14 @@
 ;RecordMidiIn
-(let ()
+(let ((pos (GetPosition))(num (d-GetMeasuresInStaff)))
 	(d-MidiRecord)
 	(if (d-RecordingMidi)
 		(begin
-			(d-PushPosition)
 			(while (d-MoveToStaffUp))
 			(if (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
 					(begin
-						(d-CreateClickStaffForMidi 1)
+						(d-CreateClickStaffForMidi num)
+						 (d-DirectivePut-clef-graphic "NoDisplay" "")
+						 (d-DirectivePut-clef-override  "NoDisplay"  5)
 						(d-MuteStaff)))
-			(d-PopPosition))
+			(d-GoToPosition #f (+ 1 (list-ref pos 1)) (list-ref pos 2) (list-ref pos 3)))
 		(d-ExtendClickTrack)))
