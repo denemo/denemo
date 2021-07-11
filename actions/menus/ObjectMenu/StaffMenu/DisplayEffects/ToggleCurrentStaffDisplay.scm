b0a3760ad2b660538f4376c4a873c4994607779d
Richard Shann <richard@rshann.plus.com>
2021-07-11 18:07:29 +0100

    Make sure not all non-click staffs are hidden in the display

--- actions/menus/ObjectMenu/StaffMenu/DisplayEffects/ToggleCurrentStaffDisplay.scm
+++ actions/menus/ObjectMenu/StaffMenu/DisplayEffects/ToggleCurrentStaffDisplay.scm
@@ -2,5 +2,9 @@
 (d-StaffHidden (not (d-StaffHidden)))
 (while (and  (d-StaffHidden) (d-MoveToStaffDown)))
 (while (and  (d-StaffHidden) (d-MoveToStaffUp)))
+(if (d-Directive-clef? DenemoClickTrack)
+	(begin
+		(d-MoveToStaffDown)
+		(d-StaffHidden #f)))
 (d-RefreshDisplay)
 (d-SetSaved #f)
