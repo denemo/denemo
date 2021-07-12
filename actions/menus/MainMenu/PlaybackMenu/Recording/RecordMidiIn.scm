5f523274ec67c1b1c1b25061d2cae2ad036e85bb
Richard Shann <richard@rshann.plus.com>
2021-07-12 11:22:59 +0100

    Allow for pre-existing click track when recording from MIDI in

--- actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
@@ -1,16 +1,19 @@
 ;RecordMidiIn
-(let ((pos (GetPosition))(num (d-GetMeasuresInStaff)))
+(let ((pos (GetPosition)) (staff-added 0)(num (d-GetMeasuresInStaff)))
 	(d-MidiRecord)
 	(if (d-RecordingMidi)
 		(begin
 			(while (d-MoveToStaffUp))
 			(if (not (d-Directive-clef? DenemoClickTrack))
 					(begin
-						(d-StaffSetSpaceAbove 0)
-						(d-CreateClickStaffForMidi num)
+						(if (not (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name")))
+							(begin
+								(set! staff-added 1)
+								(d-StaffSetSpaceAbove 0)
+								(d-CreateClickStaffForMidi num)))
 						 (d-DirectivePut-clef-graphic DenemoClickTrack "")
 						 (d-DirectivePut-clef-override  DenemoClickTrack DENEMO_OVERRIDE_GRAPHIC)
 						(d-MuteStaff)))
 			(d-StaffSetSpaceAbove 50) (disp "Set 50")
-			(d-GoToPosition #f (+ 1 (list-ref pos 1)) (list-ref pos 2) (list-ref pos 3)))
+			(d-GoToPosition #f (+ staff-added (list-ref pos 1)) (list-ref pos 2) (list-ref pos 3)))
 		(d-ExtendClickTrack)))
