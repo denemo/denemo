bfd264e110c7b0401e62633ee010b0de1779582e
Richard Shann <richard@rshann.plus.com>
2021-07-12 12:04:46 +0100

    Include command to hide staffs while using recorded MIDI

---
+++ actions/menus/MainMenu/PlaybackMenu/Recording/ToggleHideOtherStaffsKeepingClick.scm
@@ -0,0 +1,8 @@
+;ToggleHideOtherStaffsKeepingClick
+;Hides/Unhides other staffs in the display while keeping the MIDI track at the top showing.
+(d-ToggleDisplayAllStaffs)
+(d-PushPosition)
+(d-GoToPosition #f 1 1 1)
+(if (d-Directive-clef? DenemoClickTrack)
+	(d-HideStaff #f)
+(d-PopPosition)
