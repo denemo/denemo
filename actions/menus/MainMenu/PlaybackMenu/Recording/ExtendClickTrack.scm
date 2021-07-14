7f69ef27be098595506e508a1bb6fb59324499e6
Richard Shann <richard@rshann.plus.com>
2021-07-14 19:55:19 +0100

    Get ExtendClickTrack working

--- actions/menus/MainMenu/PlaybackMenu/Recording/ExtendClickTrack.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/ExtendClickTrack.scm
@@ -7,15 +7,20 @@
 			(if (d-Directive-clef? DenemoClickTrack)
 				(let ((num 0))
 					(d-MoveToBeginning)
+					
 					(d-DirectiveDelete-standalone "MuteStaff");remove the speaker icon
+					
+					
 					(d-EvenOutStaffLengths)
+					(d-MoveToMeasureRight)
 					(set! num (d-GetMeasuresInStaff))
-					(if (d-MoveToMeasureRight)
+					(if (> num 1)
 						(d-DeleteFromCursorToEnd 'this))
 					(set! time (d-GetTimeAtCursor))
+					(d-EvenOutStaffLengths)
 					(while (or (< time rec-time) (< (d-GetMeasure) num))
 						(d-FillMeasure #t)
-						(d-MoveToMeasureRight)
+						
 						(set! time (d-GetTimeAtCursor)))
 					
 					(DenemoSetPlaybackEnd)
