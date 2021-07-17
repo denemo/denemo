22f4fa5606a0313793db515d5b7aa39b8016fe8d
Richard Shann <richard@rshann.plus.com>
2021-07-17 12:55:53 +0100

    Do not show keysignature on MIDI track

--- actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
@@ -13,6 +13,10 @@
 								(d-CreateClickStaffForMidi num)))
 						 (d-DirectivePut-clef-graphic DenemoClickTrack "")
 						 (d-DirectivePut-clef-override  DenemoClickTrack DENEMO_OVERRIDE_GRAPHIC)
+						 
+						 (d-DirectivePut-keysig-graphic DenemoClickTrack "")
+						 (d-DirectivePut-keysig-override  DenemoClickTrack DENEMO_OVERRIDE_GRAPHIC)
+						 
 						(d-MuteStaff)))
 			(d-StaffSetSpaceAbove 50) (disp "Set 50")
 			(d-GoToPosition #f (+ staff-added (list-ref pos 1)) (list-ref pos 2) (list-ref pos 3)))
