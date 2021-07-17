41443e7b09bfa790243bba6938d2b9ac2a0f3b0b
Richard Shann <richard@rshann.plus.com>
2021-07-17 12:10:09 +0100

    Allow deletion while recording

--- actions/menus/MainMenu/PlaybackMenu/Recording/DeleteMidiAndClickTrack.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/DeleteMidiAndClickTrack.scm
@@ -1,13 +1,9 @@
 ;DeleteMidiAndClickTrack
 (let ((pos (GetPosition)))
-	(if (d-RecordingMidi)
-		(d-WarningDialog (_ "Stop Recording First"))
+	(d-DeleteRecordedMidi)
+	(while (d-MoveToStaffUp))
+	(if (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name"))
 		(begin
-			(if (d-GetMidiRecordingDuration)
-				(d-DeleteRecordedMidi))
-			(while (d-MoveToStaffUp))
-			(if (equal? DenemoClickTrack (d-StaffProperties "query=denemo_name"))
-					(begin
-						(d-DeleteStaff)
-						(d-StaffSetSpaceAbove 20)))
-			(d-GoToPosition #f (- (list-ref pos 1) 1) (list-ref pos 2) (list-ref pos 3)))))
+			(d-DeleteStaff)
+			(d-StaffSetSpaceAbove 20)))
+			(d-GoToPosition #f (- (list-ref pos 1) 1) (list-ref pos 2) (list-ref pos 3)))
