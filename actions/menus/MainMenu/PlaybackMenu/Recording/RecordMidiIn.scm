7759c8c4932a8215f73396d328cdaae93fc36ef7
Richard Shann <richard@rshann.plus.com>
2021-07-18 11:21:45 +0100

    Less debug chatter

--- actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
+++ actions/menus/MainMenu/PlaybackMenu/Recording/RecordMidiIn.scm
@@ -18,6 +18,6 @@
 						 (d-DirectivePut-keysig-override  DenemoClickTrack DENEMO_OVERRIDE_GRAPHIC)
 						 
 						(d-MuteStaff)))
-			(d-StaffSetSpaceAbove 50) (disp "Set 50")
+			(d-StaffSetSpaceAbove 50)
 			(d-GoToPosition #f (+ staff-added (list-ref pos 1)) (list-ref pos 2) (list-ref pos 3)))
 		(d-ExtendClickTrack)))
