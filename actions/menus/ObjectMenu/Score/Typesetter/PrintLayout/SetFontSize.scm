3c0b9f3fc798ab1ba0144fa6500bf613ea9a5118
Richard Shann <richard@rshann.plus.com>
2021-09-13 12:50:12 +0100

    Use Alt override to implement SetFontSize properly

--- actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/SetFontSize.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/SetFontSize.scm
@@ -1,12 +1,24 @@
 ;;SetFontSize
-(let* ((tag "SetFontSize") (newsize (d-DirectiveGet-paper-data tag)) (size  (d-ScoreProperties "query=fontsize")))
+(let ((tag "SetFontSize") (params SetFontSize::params)(newsize #f) (size  (d-ScoreProperties "query=fontsize")))
+	(if params (if (and (string? params)  (string->number params))
+					(set! newsize params)
+					(if (number? params)
+						(set! newsize (number->string params)))))
 	(if (not newsize)
-		(set! newsize size))
-	(set! newsize  (d-GetUserInput "Overall Score Sizing"  "Give font size to use" newsize))
+		(begin
+			(set! newsize (d-DirectiveGet-score-data tag))
+			(if (not newsize)
+				(begin
+					(set! newsize (d-DirectiveGet-paper-data tag));;legacy code
+					(d-DirectiveDelete-paper tag)))
+			(if (not newsize)
+				(set! newsize size))					
+			(set! newsize  (d-GetUserInput (_ "Overall Score Sizing")  (_ "Give font size to use") newsize))))
 	(if newsize
 		(begin
-			(d-DirectivePut-paper-data tag newsize)
-			(d-DirectivePut-paper-postfix tag 
+			(d-DirectivePut-score-data tag newsize)
+			(d-DirectivePut-score-override tag DENEMO_ALT_OVERRIDE)
+			(d-DirectivePut-score-prefix tag 
 				(string-append
-				"\n} #(set-global-staff-size " newsize ") \\paper {\n" )) ;this cheats, placing the setting *outside* the paper block. It seems we don't have a score-nfix override setting to place stuff here. FIXME
+				"#(set-global-staff-size " newsize ") \n" ))
 			(d-SetSaved #f))))
