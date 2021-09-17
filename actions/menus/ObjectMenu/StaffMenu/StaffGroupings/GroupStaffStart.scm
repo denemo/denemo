2d2675733839f542091d89c4bf7863fe2510013a
Richard Shann <richard@rshann.plus.com>
2021-09-17 10:57:16 +0100

    Fix syntax of group staff brace

--- actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GroupStaffStart.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffGroupings/GroupStaffStart.scm
@@ -4,4 +4,4 @@
     (begin	
 	    (if (d-Directive-staff? tag)
 		(d-DirectiveDelete-staff tag)
-		(AttachDirective "staff" "prefix" `(,tag . "Group Staff Start") " \\new GroupStaff <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)))))
+		(AttachDirective "staff" "prefix" `(,tag . "Group Staff Start") " \\new StaffGroup <<\n" DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX DENEMO_OVERRIDE_TAGEDIT)))))
