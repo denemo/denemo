a693f9706ef8376983816111b2de13c45e20b958
Richard Shann <richard@rshann.plus.com>
2021-09-19 12:31:38 +0100

    Make Breve and Longa insert correctly when not appending

--- actions/menus/ObjectMenu/NotesRests/EditDuration/Breve.scm
+++ actions/menus/ObjectMenu/NotesRests/EditDuration/Breve.scm
@@ -1,15 +1,13 @@
 ;;Breve
-(let ((spillover (d-GetBooleanPref "spillover")) (appending (or (None?) (Appending?))))
+(let ((spillover (d-GetBooleanPref "spillover")) )
 	(d-SetPrefs "<spillover>0</spillover>")
 	(d-0)
-	(if (Appending?)
-		(d-MoveCursorLeft))
+	(d-MoveCursorLeft)
 	(d-ChangeBreve);
 	(if (and (defined? 'Snippet::Breve) (> Snippet::Breve 0))
 		(d-SelectSnippet Snippet::Breve)
 		(d-CreateSnippetFromObject "Breve"))
-	  (if appending
-		(d-MoveCursorRight))
+	(d-MoveCursorRight)
 	(if spillover 
 		(d-SetPrefs "<spillover>1</spillover>")))
 
--- actions/menus/ObjectMenu/NotesRests/EditDuration/Longa.scm
+++ actions/menus/ObjectMenu/NotesRests/EditDuration/Longa.scm
@@ -1,14 +1,13 @@
 ;;Longa
-(let ((spillover (d-GetBooleanPref "spillover")) (appending (or (None?) (Appending?))))
-(d-SetPrefs "<spillover>0</spillover>")
-(d-0)
-(if (Appending?)
-    (d-MoveCursorLeft))
-(d-ChangeLonga)
-(if (and (defined? 'Snippet::Longa) (> Snippet::Longa 0))
-    (d-SelectSnippet Snippet::Longa)
-    (d-CreateSnippetFromObject "Longa"))
-  (if appending
-    (d-MoveCursorRight))
+(let ((spillover (d-GetBooleanPref "spillover")) )
+	(d-SetPrefs "<spillover>0</spillover>")
+	(d-0)
+	(d-MoveCursorLeft)
+	(d-ChangeLonga);
+	(if (and (defined? 'Snippet::Longa) (> Snippet::Longa 0))
+		(d-SelectSnippet Snippet::Longa)
+		(d-CreateSnippetFromObject "Longa"))
+	(d-MoveCursorRight)
 	(if spillover 
 		(d-SetPrefs "<spillover>1</spillover>")))
+
