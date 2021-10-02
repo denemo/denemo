6a1fc3c6a64b57104aec84fd01715fd135fa3ceb
Richard Shann <richard@rshann.plus.com>
2021-10-02 11:27:50 +0100

    Add command to transpose movement on playback

---
+++ actions/menus/ObjectMenu/MovementMenu/Playback/TransposeOnPlayback.scm
@@ -0,0 +1,20 @@
+;TransposeOnPlayback
+(let ((params TransposeOnPlayback::params)(interval #f))
+(define (do-staff interval)
+	(d-StaffProperties (string-append "transposition="
+			(number->string 
+				(+  (string->number interval) 
+					(string->number  (d-StaffProperties "query=transposition")))))))
+
+(if (not params)
+		(set! interval (d-GetUserInput (_ "Playback Transposition") (_ "Give semitones (~/-)") "-5")))
+		
+(if (number? interval)
+	(set! interval (number->string interval)))
+(if (string? interval)
+	(begin
+		(while (d-StaffUp))
+		(do-staff interval)
+		(while (d-StaffDown)
+			(do-staff interval)))
+	(d-WarningDialog (_ "Cancelled"))))
