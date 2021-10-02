aa1eecaedfafb1c325974c7fae649e09a481dfbc
Richard Shann <richard@rshann.plus.com>
2021-10-02 12:41:57 +0100

    Allow non interactive delete

--- actions/menus/ObjectMenu/Lyrics/DeleteCurrentVerse.scm
+++ actions/menus/ObjectMenu/Lyrics/DeleteCurrentVerse.scm
@@ -1,5 +1,8 @@
-;;DeleteCurrentVerse
-(let ((delete (RadioBoxMenu (cons (_"Cancel") #f) (cons (_ "Delete Current Verse (no undo!)") 'delete))))
+;DeleteCurrentVerse
+(let ((delete DeleteCurrentVerse::params))
+	(if (not delete)
+		(set! delete (RadioBoxMenu (cons (_"Cancel") #f) (cons (_ "Delete Current Verse (no undo!)") 'delete))))
 	(if delete
 		(d-DeleteVerse)
-		(d-InfoDialog (_ "Cancelled"))))
+		(if (not DeleteCurrentVerse::params)
+			(d-InfoDialog (_ "Cancelled")))))
