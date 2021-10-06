9e2e83f7a12a7a111461f8a6937b2f0ba513a8ae
Richard Shann <richard@rshann.plus.com>
2021-10-06 14:24:51 +0100

    Offer to make short instrument name conditional

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/ShortInstrumentName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/ShortInstrumentName.scm
@@ -1,5 +1,5 @@
 ;ShortInstrumentName
-(let ((current "") (thematch ""))
+(let ((tag "ShortInstrumentName") (current "") (thematch ""))
   (if (d-Directive-staff? "DynamicsStaff")
     (begin
         (d-WarningDialog (_ "Instrument Name should not be set on a Dynamics Line"))
@@ -16,9 +16,10 @@
       (set! current (d-GetUserInput (_ "Short Instrument Name") (_ "Give Short Instrument Name:") current))
       (if current
         (let ((indent "0")( size (/ (string->number (d-ScoreProperties "query=fontsize")) 10.0))) 
-            (d-DirectivePut-staff-display "ShortInstrumentName" current)
-            (d-DirectivePut-staff-override "ShortInstrumentName"   (logior DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC))
-            (d-DirectivePut-staff-prefix "ShortInstrumentName"  (string-append " shortInstrumentName = \"" current "\""))
+            (d-DirectivePut-staff-display tag current)
+            (d-DirectivePut-staff-override tag   (logior DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC))
+            (d-DirectivePut-staff-prefix tag  (string-append " shortInstrumentName = \"" current "\""))
+            (SetDirectiveConditional #f (cons "staff" tag))
             (set! indent (max (string->number indent) (* size (string-length current))))
             (d-ScoreShortIndent indent)
             (d-SetSaved #f))))))
