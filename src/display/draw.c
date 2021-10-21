a6d859c42dd9232ca2b2966423dbbe2bb4b77bc8
Richard Shann <richard@rshann.plus.com>
2021-10-21 11:43:30 +0100

    Move playback start text out of harm's way

--- src/display/draw.c
+++ src/display/draw.c
@@ -1345,7 +1345,7 @@ draw_playback_marker (cairo_t * cr, gint color, gdouble alpha, gint pos, gint yy
   switch (color)
     {
     case GREEN:
-      drawlargetext_cr (cr, _("Ctrl+Alt-Drag Start"), pos - 100, yy-LINE_SPACE);
+      drawlargetext_cr (cr, _("Ctrl+Alt-Drag Start"), pos - 100, yy + 7*LINE_SPACE);
       break;
     case RED:
       drawlargetext_cr (cr, _("Ctrl+Alt-Drag End"), pos - 100, yy-3*LINE_SPACE);
@@ -1358,21 +1358,21 @@ static void
 draw_playback_markers (cairo_t * cr, struct infotopass *itp, gint yy, gint line_height)
 {
   //g_print ("\nMarkers at staff %d for top %d with start %d end %d:", itp->staffnum, Denemo.project->movement->top_staff, itp->startposition, itp->endposition);
-  if ((itp->startposition > 0) && (Denemo.project->movement->start_time>0.0001)) //Don't show start marker when at the start of the movement, it makes clutter and is the default
+  if ((itp->startposition > 0)) 
     {
-      if (!(Denemo.dragging_start_playback_marker && Denemo.prefs.cursor_highlight ))
-        draw_playback_marker (cr, GREEN,0.4,itp->startposition, yy, line_height);
+      if (Denemo.dragging_start_playback_marker && Denemo.prefs.cursor_highlight)
+        draw_playback_marker (cr, GREEN,0.6,itp->startposition, yy, line_height);
       else
-        draw_playback_marker (cr, GREEN, 0.2,itp->startposition, yy, line_height);
+        draw_playback_marker (cr, GREEN, (Denemo.project->movement->playingnow || (Denemo.project->movement->start_time>0.0001))?0.4:0.2,itp->startposition, yy, line_height);//Don't show strong start marker when at the start of the movement, it makes clutter and is the default
     }
   itp->startposition = -1;
 
   if (itp->endposition > 0)
       {
-      if (!(Denemo.dragging_end_playback_marker && Denemo.prefs.cursor_highlight))
-        draw_playback_marker (cr, RED, 0.4, itp->endposition, yy, line_height);
+      if (Denemo.dragging_end_playback_marker && Denemo.prefs.cursor_highlight)
+        draw_playback_marker (cr, RED, 0.6, itp->endposition, yy, line_height);
       else
-        draw_playback_marker (cr, RED, 0.2, itp->endposition, yy, line_height);
+        draw_playback_marker (cr, RED, 0.4, itp->endposition, yy, line_height);
     }
 
   itp->endposition = -1;
