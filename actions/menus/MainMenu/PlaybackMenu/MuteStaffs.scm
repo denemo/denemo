c028ecd9ed5cb9235b6c2e313c8dc3f9cba6aafd
Richard Shann <richard@rshann.plus.com>
2021-11-25 10:04:10 +0000

    Fix bug #61552 Mute staffs offers unmute

--- actions/menus/MainMenu/PlaybackMenu/MuteStaffs.scm
+++ actions/menus/MainMenu/PlaybackMenu/MuteStaffs.scm
@@ -4,17 +4,17 @@
     (d-MoveToMovementBeginning)
     (let loop ()
         (if (not (d-Directive-standalone? mute))
-                (d-MuteStaff))
-        (set! thelist (cons (cons (d-StaffProperties "query=denemo_name") (> (d-StaffMasterVolume) 0)) thelist))
+                (d-MuteStaff)(d-MuteStaff)) ;create Mute directive and make unmuted
+        (set! thelist (cons (cons (d-StaffProperties "query=denemo_name") (<= (d-StaffMasterVolume) 0)) thelist))
         (if (d-MoveToStaffDown)
             (loop)))
  (set! thelist (reverse thelist))
- (set! thelist (d-CheckBoxes thelist (_ "Choose Staffs to Play")))
+ (set! thelist (d-CheckBoxes thelist (_ "Choose Staffs to Mute")))
  (if thelist
     (begin
         (d-MoveToMovementBeginning)
         (let loop ()
-            (if (not (eq? (cdar thelist) (> (d-StaffMasterVolume) 0)))
+            (if (not (eq? (cdar thelist) (<= (d-StaffMasterVolume) 0)))
                 (d-MuteStaff))
             (set! thelist (cdr thelist))
             (if (d-MoveToStaffDown)
