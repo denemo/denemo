bbc9ad09f5abe15328b4890cd7f09d0bb51d329a
Richard Shann <richard@rshann.plus.com>
2022-01-25 11:00:12 +0000

    Allow conditional or unconditional setting/unsetting of Ambitus

--- actions/menus/ObjectMenu/Score/Typesetter/AllowAmbitus.scm
+++ actions/menus/ObjectMenu/Score/Typesetter/AllowAmbitus.scm
@@ -1,22 +1,36 @@
 ;;AllowAmbitus
-(let ((tag "Ambitus")(criterion (_ "Ambitus")))
-    (define (doAmbitus)
+(let ((tag "Ambitus")(criterion #f))
+    (define (doAmbitus choice)
       (while (d-PreviousMovement))
       (let movement ()
 		(while (d-MoveToStaffUp))
-		(let staff ()
+		(let staff () (disp "Position " (GetPosition) "\n\n")
 		  ;;skip voices? mirrored staffs?
-			 (d-DirectivePut-staff-prefix tag "\\consists \"Ambitus_engraver\"\n")
-			 (d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC))
-			 (d-DirectivePut-staff-ignore tag (cdr (d-GetIncludeCriterion)))
+			(if (eq? choice 'delete)
+				(d-DirectiveDelete-staff tag)
+				(begin
+					 (d-DirectivePut-staff-prefix tag "\\consists \"Ambitus_engraver\"\n")
+					 (d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE  DENEMO_OVERRIDE_AFFIX  DENEMO_OVERRIDE_GRAPHIC))
+					 (if criterion
+						(d-DirectivePut-staff-ignore tag (cdr (d-GetIncludeCriterion))))))
 			 (if (or (d-MoveToVoiceDown) (d-MoveToStaffDown))
 				(staff)))
 	    (if (d-NextMovement)
-		(movement))))
+			(movement))))
+			
      (d-PushPosition)
-	 (let ((saved (d-GetSaved)))
-		(d-CreateIncludeCriterion criterion)
-		(d-SetIncludeCriterion criterion)
-		(doAmbitus)
-		(d-SetSaved saved))
+	 (let ((choice (RadioBoxMenu (cons (_ "Set Conditional Ambitus") 'conditional) 
+			(cons (_ "Set Unconditional Ambitus") 'set)
+			(cons (_ "Delete Ambitus") 'delete)
+			(cons (_ "Cancel") #f))))
+		(if choice
+			(begin
+				(if (eq? choice 'conditional)
+					(begin
+						(set! criterion (_ "Ambitus"))
+						(d-CreateIncludeCriterion criterion)
+						(d-SetIncludeCriterion criterion)))
+				(doAmbitus choice))
+			(d-WarningDialog (_ "Cancelled"))))
     (d-PopPosition))
+    
