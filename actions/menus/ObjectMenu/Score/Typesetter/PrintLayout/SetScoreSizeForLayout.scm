0c699a8e7d9bf04c1558210ffe795ccacad862ed
Richard Shann <richard@rshann.plus.com>
2022-02-08 17:25:19 +0000

    Create SetScoreSizeForLayout command

---
+++ actions/menus/ObjectMenu/Score/Typesetter/PrintLayout/SetScoreSizeForLayout.scm
@@ -0,0 +1,18 @@
+;;;SetScoreSizeForLayout
+(let* ((tag (string-append "FontSize:" (d-GetLayoutName)))(size  (d-DirectiveGet-score-data tag)))
+	(if (not size)
+		(set! size  (d-ScoreProperties "query=fontsize")))
+	(set! size  (d-GetUserInput (_ "Score Size")  (_ "Give font size to use") size))
+	(if size
+		(begin
+			(d-DirectivePut-score-data tag size)
+			(d-DirectivePut-score-override tag DENEMO_ALT_OVERRIDE)
+			(d-DirectivePut-score-prefix tag 
+				(string-append
+				"#(set-global-staff-size " size ") \n" ))
+			(d-DirectivePut-score-allow tag (d-GetLayoutId))
+			(d-SetSaved #f))
+		(d-WarningDialog (_ "Cancelled"))))
+
+
+	
