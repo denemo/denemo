4c969e60ec0156812493607a694bc85dc2147f53
Richard Shann <richard@rshann.plus.com>
2022-02-12 14:15:40 +0000

    Make Export Book more transparent

--- actions/menus/MainMenu/FileMenu/Export/ExportBook.scm
+++ actions/menus/MainMenu/FileMenu/Export/ExportBook.scm
@@ -1,5 +1,5 @@
 ;;;;;CreateBook
-(define-once CreateBook::page-start 0)
+(define-once CreateBook::page-start 1)
 (define-once CreateBook::pdfs "")
 
 (let ( (tag "CreateBook") (choice 
@@ -13,24 +13,28 @@
 						(d-ExportMUDELA lilyfile)
 						(d-WarningDialog (_ "Click \"Close\" and this dialog will freeze until the typesetting is done\nSorry this is so clunky!"))
 						(d-CreatePDFFromLilyfile lilyfile basename)
-						(d-InfoDialog (_ "Now typeset further layouts and run the command again to add them to the book or to finalize the book"))
-						(set! CreateBook::page-start (+  CreateBook::page-start (d-GetNumberTypesetPages)))))						
+						(set! CreateBook::page-start (+ 0 CreateBook::page-start (d-GetNumberTypesetPages)))
+						(d-InfoDialog (string-append (_"You have a book with ") (number->string (1- CreateBook::page-start)) (_ " page(s).
+If you want to add another layout to the book typeset it and run this command again.
+Or run the command again to export the book as a PDF.")))))						
 	(case choice
 		((add)  
-				(if (zero? CreateBook::page-start)
+				(if (= 1 CreateBook::page-start)
 					(create-pdf)
 					(begin
 						(d-DirectivePut-paper-postfix tag (string-append "\nprint-first-page-number=##t\nfirst-page-number = " (number->string CreateBook::page-start) "\n"))
 						(create-pdf)
 						(d-DirectiveDelete-paper tag))))		
 		((new) 
-				(set! CreateBook::page-start 0)
+				(d-InfoDialog (_ "You have a new (empty) book. Choose a layout, typeset it (if you haven't already), and run this command again to place it as the first in the book."))
+				(set! CreateBook::page-start 1)
 				(set! CreateBook::pdfs ""))
 		((create)
 				(if (string-null? CreateBook::pdfs)
 					(d-WarningDialog (_ "You must add layouts to the Book first"))
-					(let* ((name (d-GetUserInput (_ "Create Book") (_ "Give name for PDF:") "DenemoBook")) (output (d-PathFromFilename (d-Open "query=filename")))
-							(command (if output (string-append "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=\"" output "/" name ".pdf" "\"" CreateBook::pdfs) #f)))
+					(let* ((name (d-GetUserInput (_ "Create Book") (_ "Give name for PDF:") "DenemoBook")) 
+						(output (d-ChooseDirectory (string-append (_ "Choose where to save ") name ".pdf")))
+						(command (if output (string-append "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=\"" output "/" name ".pdf" "\"" CreateBook::pdfs) #f)))
 						(if command
 							(system command)
 							(d-WarningDialog (_ "Score saved to disk"))))))))
