423a144d25fada04d9c384b793d029d93d30c0f6
Richard Shann <richard@rshann.plus.com>
2022-02-16 16:47:48 +0000

    More adaptable script for ExportBook

--- actions/menus/MainMenu/FileMenu/Export/ExportBook.scm
+++ actions/menus/MainMenu/FileMenu/Export/ExportBook.scm
@@ -1,16 +1,24 @@
 ;;;;;ExportBook
 (define-once ExportBook::page-start 1)
-(define-once ExportBook::pdfs "")
+(define-once ExportBook::pdfs '())
 (define-once ExportBook::layouts "")
 
 (let ( (saved (d-GetSaved))(tag "ExportBook") (choice 
 	(RadioBoxMenu (cons (_ "Add Current Layout to Book") 'add)
 					(cons (_ "Create PDF from Book") 'create)
 					(cons (_ "Start a new Book") 'new))))
+					
+	(define (GetNumberTypesetPages)
+		(let* ((command (list "gs" "-q" "-dNODISPLAY" "-dNOSAFER" "-c"
+				(string-append "\"(" (d-GetCurrentTypesetPDF) ") (r) file runpdfbegin pdfpagecount = quit\"")))
+			(result (string-trim-both (d-ExecuteExternalProgram command '()))))
+		(if result (string->number result) 0)))
+	
+					
 	(define (create-pdf)
 			(let* ((basename (tmpnam))
 							(lilyfile (string-append basename ".ly")))
-						(set! ExportBook::pdfs (string-append ExportBook::pdfs " " basename ".pdf"))
+						(set! ExportBook::pdfs (cons (string-append " " basename ".pdf") ExportBook::pdfs ))
 						(set! ExportBook::layouts (string-append ExportBook::layouts ", \"" (d-GetLayoutName) "\""))
 						(d-ExportMUDELA lilyfile)
 						
@@ -18,7 +26,7 @@
 						
 						(d-CreatePDFFromLilyfile lilyfile basename)
 						
-						(set! ExportBook::page-start (+ 0 ExportBook::page-start (d-GetNumberTypesetPages)))
+						(set! ExportBook::page-start (+ ExportBook::page-start (GetNumberTypesetPages)))
 						
 						(d-InfoDialog (string-append (_"You have a book with ") (number->string (1- ExportBook::page-start)) (_ " page(s); it includes the layout(s)") ExportBook::layouts (_ " so far.
 If you want to add another layout to the book typeset it and run this command again.
@@ -38,14 +46,17 @@ Or run the command again to export the book as a PDF.")))))
 				(d-InfoDialog (_ "You have a new (empty) book. Choose a layout, typeset it (if you haven't already), and run this command again to place it as the first in the book."))
 				(set! ExportBook::page-start 1)
 				(set! ExportBook::layouts "")
-				(set! ExportBook::pdfs ""))
+				(set! ExportBook::pdfs '()))
 		((create)
-				(if (string-null? ExportBook::pdfs)
+				(if (null? ExportBook::pdfs)
 					(d-WarningDialog (_ "You must add layouts to the Book first"))
 					(let* ((gs (if (equal? DENEMO_OS "Windows") (string-append DENEMO_BIN_DIR "/gs.exe") "gs"))(name (d-GetUserInput (_ "Create Book") (_ "Give name for PDF:") "DenemoBook")) 
 						(output (d-ChooseDirectory (string-append (_ "Choose where to save ") name ".pdf")))
-						(command (if output (string-append gs " -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=\"" output "/" name ".pdf" "\"" ExportBook::pdfs) #f)))
-						(if command
-							(system command)
+						(command #f))
+						(if output
+							(begin
+								(set! command (append (list "gs" "-dBATCH" "-dNOPAUSE"  "-q" 
+										"-sDEVICE=pdfwrite" (string-append "-sOutputFile=\"" output "/" name ".pdf" "\"")) (reverse ExportBook::pdfs)))
+								(d-ExecuteExternalProgram command '()))
 							(d-WarningDialog (_ "Score saved to disk")))))))
 		(d-SetSaved saved))
