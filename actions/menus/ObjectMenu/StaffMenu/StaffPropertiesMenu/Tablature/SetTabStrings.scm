9e212540bd55bf0c7f1485aa8b397133f9890893
Richard Shann <richard@rshann.plus.com>
2022-03-16 16:07:11 +0000

    Improve TAB commands

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/SetTabStrings.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/SetTabStrings.scm
@@ -1,25 +1,25 @@
-;;;SetTablatureTunings
-(let ((tunings ""))
-(if (Chord?)
-	(begin
-  		 (d-DuplicateChord) ;;; to ensure chord has no duration in LilyPond
-  		 (set! tunings (d-GetLilyPond))
-  		 (d-DeleteObject)
-  		 (d-MoveCursorLeft)
-  		 (StandAloneDirectiveProto (cons "SetTabStrings" (string-append "\\set Staff.stringTunings = \\stringTuning " tunings "\n")) #t "\nh"   )
-  		  (d-MoveCursorLeft)
-  		 (d-DirectivePut-standalone-display "SetTabStrings" tunings)
-   		  (d-TabStaff))
-	(begin
-		(if (d-Directive-standalone "SetTabStrings")
+;;;SetTabStrings
+(let ((tag "SetTabStrings")(tunings ""))
+	(if (equal? (d-StaffType) "TabStaff")
+		(if (Chord?)
 			(begin
-				
-				(set! tunings (d-GetUserInput "Re-Entrant Tunings" "Re-order the strings" (d-DirectiveGet-standalone-display "SetTabStrings")))
-				(if tunings
-					 (begin
-						(StandAloneDirectiveProto (cons "SetTabStrings" (string-append "\\set Staff.stringTunings = \\stringTuning " tunings "\n")) #t "\nh"   )
-						(d-MoveCursorLeft)
-						(d-DirectivePut-standalone-display "SetTabStrings" tunings))))
-				
-				
-					(d-WarningDialog "To use this command create a chord\nwith one note for each open string on your instrument\nand position the cursor on it")))))
+		  		 (d-DuplicateChord) ;;; to ensure chord has no duration in the LilyPond syntax got by (d-GetLilyPond)
+		  		 (set! tunings (d-GetLilyPond))
+		  		 (d-MoveCursorLeft)
+		  		 (d-DeleteObject)
+		  		 (d-DeleteObject)
+		  		 (StandAloneDirectiveProto (cons tag (string-append "\\set Staff.stringTunings = \\stringTuning " tunings "\n")) #t "\nh"   )
+		  		  (d-MoveCursorLeft)
+		  		 (d-DirectivePut-standalone-display tag (string-append (_ "Open Strings: ") tunings)))
+			(begin
+				(if (d-Directive-standalone? tag)
+					(begin
+						
+						(set! tunings (d-GetUserInput "Re-Entrant Tunings" "Re-order the strings" (d-DirectiveGet-standalone-display "SetTabStrings")))
+						(if tunings
+							 (begin
+								(StandAloneDirectiveProto (cons tag (string-append "\\set Staff.stringTunings = \\stringTuning " tunings "\n")) #t "\nh"   )
+								(d-MoveCursorLeft)
+								(d-DirectivePut-standalone-display tag tunings))))
+							(d-WarningDialog (_ "To use this command create a chord\nwith one note for each open string on your instrument\nand position the cursor on it")))))
+		(d-WarningDialog (_ "Only useful in a TAB staff"))))
