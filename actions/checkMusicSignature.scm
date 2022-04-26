9be9fc593b41f8b336c71ad2d563f0bac1520901
Richard Shann <richard@rshann.plus.com>
2022-04-26 12:22:49 +0100

    Allow searching all scores and skipping some

--- actions/checkMusicSignature.scm
+++ actions/checkMusicSignature.scm
@@ -1,35 +1,46 @@
 ;;;This script requires a Scheme variable DenemoMusicSignature to be set to a list of semitone steps
 ;;;It will test the current denemo score to see if it matches at the opening bars
-(d-GoToPosition DenemoSearchMovement 1 1 1) ; this could check for the opening bars of each movement by looping with d-NextMovement from here
-(disp "\nChecking score " (d-GetFilename))
-(let ((current (d-GetNoteFromTopAsMidi))
-        (sig DenemoMusicSignature)
-        (test #f)
-        (next #f))
-    (if (not current)
-        (d-NextNote))
-    (set! current (d-GetNoteFromTopAsMidi))
-(disp "starting with current " current "\n\n\n\n")
-(if current
-    (begin
-        (while (and (d-NextNote) (not (null? sig)))
-            (set! next (d-GetNoteFromTopAsMidi))
-            (set! test (- next current))
-            (set! current next)
-            (if (= test (car sig))
-                (begin  
-                    (set! sig (cdr sig)))
-                (begin
-                    (disp "No match at current" DenemoMusicSignature " test " test "\n\n")
-                    (d-Quit "1"))))
-        (if (null? sig)
-            (begin
-                (disp  "\nThe file " (d-GetFilename) " matches the given Denemo Music Signature\n")
-                (d-Quit "0"))))
-    (begin
-        (disp "Returning status 2 (target score has too few notes)\n")
-        (d-Quit "2"))))
-(d-Quit "3") ;;just one note in target
+;;; DenemoSearchMovement should be set to the movement to search in or 0 to search all movements
+(disp "\nChecking score " (d-GetFilename) " movements " (if (zero? DenemoSearchMovement) "All " (number->string DenemoSearchMovement)) "\n")
+(let loop ((movement_number 1))
+	;(disp "Checking movement " movement_number " against " (d-GetMovementsInScore) "\n")
+	(if (<= movement_number (d-GetMovementsInScore))
+		(begin
+		 ;(disp "testing " (zero? DenemoSearchMovement) " or " (= movement_number DenemoSearchMovement) " giving " (or (zero? DenemoSearchMovement)(= movement_number DenemoSearchMovement)) "\n\n")
+			(if (or (zero? DenemoSearchMovement)(= movement_number DenemoSearchMovement))
+				(begin
+					(d-GoToPosition movement_number 1 1 1)
+					;(disp "Went to " (GetPosition) " for " DenemoSearchMovement " at " movement_number "\n\n")
+					(let ((current (d-GetNoteFromTopAsMidi))
+							(sig DenemoMusicSignature)
+							(test #f)
+							(next #f))
+						(while (and (not current) 
+							(d-NextNote))
+							(set! current (d-GetNoteFromTopAsMidi)))
+						;(disp "starting with midi note " current " at movement " movement_number " location" (GetPosition) "\n")
+						(if current
+							(begin
+								(while (and (d-NextNote) (not (null? sig)))
+									(set! next (d-GetNoteFromTopAsMidi))
+									(set! test (- next current))
+									(set! current next)
+									(if (= test (car sig))
+										(begin  
+											(set! sig (cdr sig))
+											(if (null? sig)
+												(begin
+													;(disp  "\nThe file " (d-GetFilename) " matches the given Denemo Music Signature\n")
+													(d-Quit "0"))))
+										(begin
+											(set! sig '())
+											(disp "No match at location " (GetPosition) " with pattern" DenemoMusicSignature " note discrep: " test "\n\n")
+											))))))))
+			;(disp "Reached "	movement_number " at location " (GetPosition) "\n")			
+         (loop (1+ movement_number)))
+     (disp "Checked all movements")))						
+(d-Quit "4")
+		
             
                 
         
--- actions/menus/ObjectMenu/Score/GenerateMusicSignatureSearchScript.scm
+++ actions/menus/ObjectMenu/Score/GenerateMusicSignatureSearchScript.scm
@@ -1,10 +1,12 @@
 ;;;GenerateMusicSignatureSearchScript with movement number to search in
 (use-modules (ice-9 ftw))
-(d-GoToPosition 1 1 1 1)
+(d-GoToPosition #f 1 1 1)
 (let ((current (d-GetNoteFromTopAsMidi))
 	(source (d-GetFilename))
-	(movement-number "1")
-        (count (string->number (d-GetUserInput "Create Music Signature" "Give number of notes to match: " "5")))
+	(movement-number "1") (skips (d-GetUserInput (_"Create Music Signature")
+				(_ "Give number of matching scores to skip: ") "0"))
+        (count (string->number (d-GetUserInput (_"Create Music Signature") 
+												(_ "Give number of notes to match: ") "5")))
         (startdir #f)
         (theproc #f)
         (sig "")
@@ -21,14 +23,22 @@
                     " -n " 
                     " -a \"" (string-append "(define DenemoSearchMovement " movement-number ") (define DenemoMusicSignature '(" sig "))")
                     "\" -i " (string-append "\"" DENEMO_ACTIONS_DIR "checkMusicSignature.scm" "\"") 
-                    " \"" filename "\""))) ;(disp "Returned " status "\n")
+                    " \"" filename "\""))) (disp "Returned " status "\n")
                 (if (zero? status)
                     (begin
-                        (d-OpenNewWindow filename)
-                        (d-GoToPosition (string->number movement-number) 1 1 1)
-                        (set! found #t)
-                        #f)
-                        #t)) ;not a match, continue traversal
+						(if (zero? skips)
+							(begin
+								(d-OpenNewWindow filename)
+								(d-GoToPosition (string->number movement-number) 1 1 1)
+								(set! found #t)
+								#f)
+							(begin
+								(set! skips (1- skips))
+								#t)))
+                    (begin
+						;(disp "File " filename " is not a match - continue\n")
+						#t)) ;not a match, continue traversal
+			)
             (begin
             #t))) ;not a file, continue traversal
 ;;;;actual procedure        
@@ -42,11 +52,19 @@
         (set! current next)
         (set! count (1- count)))
   (set! startdir (d-ChooseDirectory "Where to search" DENEMO_HOME_DIR '() ))
-  (set! movement-number (d-GetUserInput (_ "Search for Similar") (_"Which movement to search in?") "1"))
+  (set! movement-number (d-GetUserInput (_ "Search for Similar") (_"Which movement to search in (0 for all movements)?") "1"))
   (if (not movement-number)
     (set! movement-number "1"))
+  (if (string? skips)
+		(begin
+			(set! skips (string->number skips))
+			(if (and skips (negative? skips))
+				(set! skips 0)))
+		(set! skips 0))
   (d-InfoDialog "Searching ... the display will be very sluggish!")
   (ftw startdir theproc)
   (if found
-    (d-InfoDialog "This score starts with the same intervals")
-    (d-InfoDialog (string-append "No score in or below " startdir " starts with the same intervals"))))
+    (if (equal? "0" movement-number)
+		(d-InfoDialog (_ "This score has a movement that starts with the same intervals"))
+		(d-InfoDialog (_ "This score starts with the same intervals at the given movement number")))
+    (d-InfoDialog (string-append "No (other) score in or below " startdir " starts with the same intervals"))))
